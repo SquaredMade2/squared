a9e09bf575c41aa4e6dc573065406646743b873a
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-07-24 13:35:31 -0400

    feat: implemented accordion ui component and removed code no longer needed

--- apps/web/src/components/NavBar/index.tsx
+++ apps/web/src/components/NavBar/index.tsx
@@ -2,49 +2,59 @@
 import React from 'react';
 import { useAppSelector, useAppDispatch } from '@/hooks/typeScriptReduxHooks';
 import { usePathname, useRouter } from 'next/navigation';
-import { motion, useAnimation } from 'framer-motion';
 import { setCurrentTeam } from '@/store/taskData';
 import NewIssueModal from '@/components/NewIssueModal';
 import WorkSpaceDropDown from '@/components/WorkSpaceDropdown';
 import NewIssueButton from '@/components/NewIssueButton';
 import NavBarTeams from '@/components/NavBarTeams';
-import { NavSearchIcon } from '@/components/Svg';
-import type { Team } from '@/store/taskData/taskData.interfaces';
-import Link from 'next/link';
+import { NavSearchIcon, TeamIcon } from "@/components/Svg";
+import type { Team } from "@/store/taskData/taskData.interfaces";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
 
 const styles = {
-  header: 'flex items-center justify-center w-full lg:justify-center',
-  main: 'flex flex h-full justify-center bg-popover border-r   w-[296px]',
-  second: 'flex flex-col gap-4 lg:pt-1.5 pt-6 items-center text-nav w-full',
-  projectTasklist: 'flex flex-row items-center cursor-pointer relative w-full',
-  newIssueDiv: 'flex flex-row w-full justify-around ml-2',
-  newIssueModalContainer: 'absolute top-[100px] left-full',
-  newTaskButton: 'pr-36 pl-2 border shadow-lg rounded-md focus:outline-none',
-  newTaskText: 'text-nav',
+  header: "flex items-center justify-center w-full lg:justify-center",
+  main: "flex flex h-full justify-center bg-popover border-r   w-[296px]",
+  second: "flex flex-col gap-4 lg:pt-1.5 pt-6 items-center text-nav w-full",
+  projectTasklist: "flex flex-row items-center cursor-pointer relative w-full",
+  newIssueDiv: "flex flex-row w-full justify-around ml-2",
+  newIssueModalContainer: "absolute top-[100px] left-full",
+  newTaskButton: "pr-36 pl-2 border shadow-lg rounded-md focus:outline-none",
+  newTaskText: "text-nav",
   searchButton:
-    'rounded-md border border-border w-10 align-center flex justify-center bg-secondary shadow-lg focus:outline-none flex flex-row items-center cursor-pointer hover:bg-popover h-10 items-center',
-  searchIconSVG: 'stroke-current fill-transparent cursor-pointer h-5 w-5',
-  createWorkSpaceDiv: 'items-center',
-  inviteDiv: 'flex flex-col gap-4 items-center pb-6 text-foreground h-full justify-end',
-  userImg: 'text-xs',
-  dropdownPostion: 'relative',
-  teamsWrapper: 'w-full h-full left-5 mt-10 cursor-default text-foreground',
-  themeButton: 'mr-3',
-  stackIconButton: 'flex items-center cursor-pointer hover:bg-background',
-  stackIconSVG: 'mr-2',
-  row: 'w-full flex items-center my-1.5 rounded-md mr-3',
-  innerDivSpan: 'text-sm m-2 text-popover-foreground font-semibold',
-  triangle: 'transition-all 0.2s ease-in-out ml-1 z-30',
-  fullWidth: 'w-full ml-1.5',
-  ul: 'overflow-hidden',
-  searchButtonDiv: 'ml-3.5',
-  mainContainer: 'w-11/12 flex flex-col',
-  inboxButton: 'w-full flex items-center h-9 hover:bg-secondary rounded-md cursor-pointer',
-  hover: 'bg-secondary',
+    "rounded-md border border-border w-10 align-center flex justify-center bg-secondary shadow-lg focus:outline-none flex flex-row items-center cursor-pointer hover:bg-popover h-10 items-center",
+  searchIconSVG: "stroke-current fill-transparent cursor-pointer h-5 w-5",
+  createWorkSpaceDiv: "items-center",
+  inviteDiv:
+    "flex flex-col gap-4 items-center pb-6 text-foreground h-full justify-end",
+  userImg: "text-xs",
+  dropdownPostion: "relative",
+  teamsWrapper: "w-full h-full left-5 mt-10 cursor-default text-foreground",
+  themeButton: "mr-3",
+  stackIconButton: "flex items-center cursor-pointer hover:bg-background",
+  stackIconSVG: "mr-2",
+  row: "w-full flex items-center my-1.5 rounded-md mr-3",
+  innerDivSpan: "text-sm m-2 text-popover-foreground font-semibold",
+  triangle: "transition-all 0.2s ease-in-out ml-1 z-30",
+  fullWidth: "w-full ml-1.5",
+  ul: "overflow-hidden",
+  searchButtonDiv: "ml-3.5",
+  mainContainer: "w-11/12 flex flex-col",
+  inboxButton:
+    "w-full flex items-center h-9 hover:bg-secondary rounded-md cursor-pointer",
+  hover: "bg-secondary",
+  teamRow:
+    "w-full flex items-center my-1.5 hover:bg-secondary rounded-md pl-0.5",
+  teamSVG: "mr-2 p-0.5 rounded",
+  teamButton: "flex items-center cursor-pointer",
+  listWrapper: "w-full bg-accent z-10",
 };
 
 const Navbar = () => {
-  const controls = useAnimation();
   const dispatch = useAppDispatch();
   const router = useRouter();
   const pathname = usePathname();
@@ -98,24 +108,28 @@ const Navbar = () => {
           <div className={styles.row}>
             <span className={styles.innerDivSpan}>Your teams</span>
           </div>
-          <motion.ul
-            className={styles.ul}
-            initial={{ height: 'auto' }}
-            animate={controls}
-            transition={{ duration: 0.1 }}
-          >
+          <Accordion type="single" collapsible className={styles.ul}>
             {workspace?.teams.map((team: Team) => {
               return (
-                <NavBarTeams
-                  key={team._id}
-                  id={team._id}
-                  teamName={team.name}
-                  onDropdownClick={() => handleTeamClick(team)}
-                  teamIdentifier={team.identifier}
-                />
+                <AccordionItem key={team._id} value={team._id} className={styles.listWrapper}>
+                  <AccordionTrigger className={styles.teamRow}>
+                    <div className={styles.teamSVG}>
+                      <TeamIcon />
+                    </div>
+                    <span className={styles.teamButton}>{team.name}</span>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <NavBarTeams
+                      id={team._id}
+                      teamName={team.name}
+                      onDropdownClick={() => handleTeamClick(team)}
+                      teamIdentifier={team.identifier}
+                    />
+                  </AccordionContent>
+                </AccordionItem>
               );
             })}
-          </motion.ul>
+          </Accordion>
         </div>
         <div className={styles.newIssueModalContainer}>
           <NewIssueModal />
--- apps/web/src/components/NavBarTeams/index.tsx
+++ apps/web/src/components/NavBarTeams/index.tsx
@@ -2,12 +2,9 @@ import type React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAppDispatch, useAppSelector } from '@/hooks/typeScriptReduxHooks';
-import { motion, useAnimation } from 'framer-motion';
 import type { getTeamInfoType, handleActiveParamsType } from '@/app/interfaces/Navbars.interfaces';
-
 import type { Team as TaskDataTeam } from '@/store/taskData/taskData.interfaces';
-
-import { DropdownTriangle, TeamIcon, IssuesIcon, ViewsStackIcon } from '@/components/Svg';
+import { IssuesIcon, ViewsStackIcon } from '@/components/Svg';
 import { useRouter } from 'next/navigation';
 import { getTeam } from '@/store/taskData/thunks';
 import type { NavBarTeamProps } from './NavBarTeams.interfaces';
@@ -33,8 +30,6 @@ const NavBarTeams = ({
   teamIdentifier,
 }: NavBarTeamProps): React.ReactElement => {
   const dispatch = useAppDispatch();
-  const controls = useAnimation();
-  const [tasklistToggle, setTasklistToggle] = useState(false);
   const [isHovered, setIsHovered] = useState<string>('#858699');
   const theme = useAppSelector((state) => state.userSettings.theme);
   const currentWorkspace = useAppSelector((state) => state.taskData.currentWorkspace);
@@ -46,15 +41,6 @@ const NavBarTeams = ({
     } else {
       console.error('Team identifier not found');
     }
-  };
-
-  const handleToggle = (): void => {
-    if (tasklistToggle) {
-      controls.start({ height: 0 });
-    } else {
-      controls.start({ height: 'auto' });
-    }
-    setTasklistToggle(!tasklistToggle);
   };
 
   const handleMouseEnter = () => {
@@ -92,79 +78,73 @@ const NavBarTeams = ({
   }, []);
 
   return (
-    <li key={id} className={styles.listWrapper}>
-      <div className={styles.row} onClick={() => handleToggle()}>
-        <div className={styles.row}>
-          <div className={styles.svg}>{<TeamIcon />}</div>
-          <span className={styles.button}>{teamName}</span>
-          <DropdownTriangle
-            className={tasklistToggle ? `rotate-90 ${styles.triangle}` : styles.triangle}
-          />
+    <div className={styles.listWrapper}>
+      <div
+        className={`${styles.row} group`}
+        onClick={onDropdownClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <button className={styles.button} type="button">
+          <div className={`${styles.svg}`}>
+            {<IssuesIcon fill={isHovered} />}
+          </div>
+          <p>Issues</p>
+        </button>
+      </div>
+
+      <div className="ml-2">
+        <div className={styles.innerDiv}>
+          <div
+            className={styles.row}
+            onClick={() => handleActiveParams("active")}
+          >
+            <div>
+              <span className={styles.innerDivSpan}>Active</span>
+            </div>
+          </div>
+          <div
+            className={styles.row}
+            onClick={() => handleActiveParams("backlog")}
+          >
+            <div>
+              <span className={styles.innerDivSpan}>Backlog</span>
+            </div>
+          </div>
         </div>
       </div>
-      <motion.div
-        className={styles.container}
-        initial={{ height: 0 }}
-        animate={controls}
-        transition={{ duration: 0.2 }}
+
+      {/*
+    			Commented out broken links until all bugs are fixed and app is launched. Implementing
+    			these links will take a while. ---> Pinak
+    			<div className={`${styles.row} group`}>
+    				<Link href={`/projects`}>
+    				<button className={styles.button}>
+    					<div className={styles.svg}>
+    						{<ProjectSquaresSub className={`group-hover:fill-white`} />}
+    					</div>
+    					<p>Projects</p>
+    				</button>
+    				</Link>
+    			</div> */}
+
+      <button
+        className={styles.viewsButton}
+        onClick={handleViewsButtonClick}
+        type="button"
       >
-        <div
-          className={`${styles.row} group`}
-          onClick={onDropdownClick}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <button className={styles.button} type="button">
-            <div className={`${styles.svg}`}>{<IssuesIcon fill={isHovered} />}</div>
-            <p>Issues</p>
-          </button>
-          {/* </Link> */}
-        </div>
-
-        {/* Commented out unused links until all bugs are fixed and app is launched. Implementing 
-					these links will take a while. ---> Pinak */}
-        <div className="ml-2">
-          <div className={styles.innerDiv}>
-            <div className={styles.row} onClick={() => handleActiveParams('active')}>
-              <div>
-                <span className={styles.innerDivSpan}>Active</span>
-              </div>
+        <div className={`${styles.row} group`}>
+          <div className={styles.button}>
+            <div className={styles.svg}>
+              <ViewsStackIcon
+                className={`group-hover:${theme === "light" ? "fill-black" : "fill-white"}`}
+              />
             </div>
-            <div className={styles.row} onClick={() => handleActiveParams('backlog')}>
-              <div>
-                <span className={styles.innerDivSpan}>Backlog</span>
-              </div>
-            </div>
+            <p>Views</p>
           </div>
         </div>
-
-        {/*
-					Commented out broken links until all bugs are fixed and app is launched. Implementing 
-					these links will take a while. ---> Pinak
-					<div className={`${styles.row} group`}>
-						<Link href={`/projects`}>
-						<button className={styles.button}>
-							<div className={styles.svg}>
-								{<ProjectSquaresSub className={`group-hover:fill-white`} />}
-							</div>
-							<p>Projects</p>
-						</button>
-						</Link>
-					</div> */}
-        <button className={styles.viewsButton} onClick={handleViewsButtonClick} type="button">
-          <div className={`${styles.row} group`}>
-            <div className={styles.button}>
-              <div className={styles.svg}>
-                <ViewsStackIcon
-                  className={`group-hover:${theme === 'light' ? 'fill-black' : 'fill-white'}`}
-                />
-              </div>
-              <p>Views</p>
-            </div>
-          </div>
-        </button>
-      </motion.div>
-    </li>
+      </button>
+    </div>
   );
 };
 
