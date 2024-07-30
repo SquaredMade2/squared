7d4bf3ad2422cea06c4bf19724d16dd61fd79a2b
THEjacob1000 <jacob.s.sanderson@gmail.com>
2024-07-30 18:39:08 +1000

    fix: fixed all remaining tests

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,13 @@ module.exports = {
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transformIgnorePatterns: [
-    "node_modules/(?!(module-that-needs-transformation)/)",
+    "node_modules/(?!(module-that-needs-transformation|mongoose|other-package-name)/)",
+    "<rootDir>/dist/",
   ],
+  testPathIgnorePatterns: ["<rootDir>/dist/"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleDirectories: ["node_modules", "src"],
