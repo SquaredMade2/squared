ed9d2dfb2d24c8f23b08c6eae8347374cf092cc9
klexup <jameshensley37@gmail.com>
2024-08-04 12:32:44 -0600

    fix: fixed primary bug

--- apps/web/src/components/TaskStatusSection/index.tsx
+++ apps/web/src/components/TaskStatusSection/index.tsx
@@ -4,35 +4,42 @@ import { Button } from "../ui/button";
 import { GridColumnNewIssueButton } from "../NewIssueButton";
 
 const style = {
-	subTitleBgGrid:
-		"flex flex-col gap-2 z-30 w-full min-h-[135px] h-full overflow-x-hidden",
-	subTitleBgList: "grid grid-rows-[1fr 9fr] rounded-lg bg-card w-full",
+  subTitleBgGrid:
+    "flex flex-col gap-2 z-30 w-full min-h-[135px] h-full overflow-x-hidden",
+  subTitleBgList: "grid grid-rows-[1fr 9fr] rounded-lg bg-card w-full",
 };
 
 const TaskStatusSection = ({
-	isListView,
-	filteredTasks,
-	setShowRenameModal,
-	setTaskData,
-	showTasks,
-	title,
+  isListView,
+  filteredTasks,
+  setShowRenameModal,
+  setTaskData,
+  showTasks,
+  title,
 }: TaskStatusSectionProps) => {
-	const location = "dashboard";
-	const highlightText = (taskTitle: string) => taskTitle;
-
-	return (
-		<div className={isListView ? style.subTitleBgList : style.subTitleBgGrid}>
-			{showTasks && (
-				<TaskCard
-					filteredTasks={filteredTasks}
-					setShowRenameModal={setShowRenameModal}
-					setTaskData={setTaskData}
-					location={location}
-					highlightText={highlightText}
-				/>
-			)}
-			{!isListView && <GridColumnNewIssueButton status={title} />}
-		</div>
-	);
+  const location = "dashboard";
+  const highlightText = (taskTitle: string) => taskTitle;
+  return (
+    <div
+      className={
+        (isListView ? style.subTitleBgList : style.subTitleBgGrid) +
+          //If column is empty, add padding to the bottom allowing new issues to be dragged within.
+          isListView && filteredTasks.length < 1
+          ? " pb-1"
+          : ""
+      }
+    >
+      {showTasks && (
+        <TaskCard
+          filteredTasks={filteredTasks}
+          setShowRenameModal={setShowRenameModal}
+          setTaskData={setTaskData}
+          location={location}
+          highlightText={highlightText}
+        />
+      )}
+      {!isListView && <GridColumnNewIssueButton status={title} />}
+    </div>
+  );
 };
 export default TaskStatusSection;
