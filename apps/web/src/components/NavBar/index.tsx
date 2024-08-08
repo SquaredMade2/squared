4d81f04301dea303afde9b939b6b92c233389a66
Sajin Rana <132703827+sajin-rana@users.noreply.github.com>
2024-08-08 10:52:59 +0800

    refactor: resolve the comment

--- apps/web/src/components/NavBar/index.tsx
+++ apps/web/src/components/NavBar/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useAppSelector, useAppDispatch } from "@/hooks/typeScriptReduxHooks";
 import { usePathname, useRouter } from "next/navigation";
 import { setCurrentTeam } from "@/store/taskData";
