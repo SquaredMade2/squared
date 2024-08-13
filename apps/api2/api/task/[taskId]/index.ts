c5cbfd753d8f11b982e464f092911099539c0e7a
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-13 23:09:25 +1000

    feat: added updateTask capability

--- apps/api2/api/task/[taskId]/index.ts
+++ apps/api2/api/task/[taskId]/index.ts
@@ -28,5 +28,23 @@ export function createRoute() {
         res.status(500).json({ message: "Internal server error" });
       }
     },
+    PUT: async (req: Request<Params>, res: Response, body: Task) => {
+      const { taskId } = req.params;
+      try {
+        const task: Task | null = await prisma.task.update({
+          where: { id: taskId },
+          data: body,
+        });
+        if (!task) {
+          return res.status(404).json({ message: "Task not found" });
+        }
+
+        // Return the updated task
+        res.json(task);
+      } catch (error) {
+        console.error("Error updating task:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    },
   };
 }
