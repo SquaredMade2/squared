ebe5020ed681d06925b132429c144e0029f3f055
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-15 06:31:10 +1000

    ci: deleted eb hooks

--- .platform/confighooks/prebuild/01login.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-USER=$(/opt/elasticbeanstalk/bin/get-config env -k USER)
-PASSWD=$(/opt/elasticbeanstalk/bin/get-config env -k PASSWD)
-
-echo $PASSWD | docker login -u $USER --password-stdin
