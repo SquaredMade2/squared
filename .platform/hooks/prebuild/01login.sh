427a1feab29f35c8eefd36390b5e501883c8833e
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-14 23:11:29 +1000

    ci: removed .platform hooks

--- .platform/hooks/prebuild/01login.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-USER=$(/opt/elasticbeanstalk/bin/get-config environment -k USER)
-PASSWD=$(/opt/elasticbeanstalk/bin/get-config environment -k PASSWD)
-
-echo $PASSWD | docker login -u $USER --password-stdin
