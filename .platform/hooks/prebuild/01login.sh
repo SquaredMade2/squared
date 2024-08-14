33558adafef719860743e963611f6ac573564b24
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-14 22:53:02 +1000

    ci: adjusted login scripts

--- .platform/hooks/prebuild/01login.sh
+++ .platform/hooks/prebuild/01login.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-USER=/opt/elasticbeanstalk/bin/get-config environment -k USER
-/opt/elasticbeanstalk/bin/get-config environment -k PASSWD | docker login -u $USER --password-stdin
+USER=$(/opt/elasticbeanstalk/bin/get-config environment -k USER)
+PASSWD=$(/opt/elasticbeanstalk/bin/get-config environment -k PASSWD)
+
+echo $PASSWD | docker login -u $USER --password-stdin
