5f14ebc828074a54ef3b40d53e975b03820603a2
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-14 21:39:52 +1000

    fix: fixed it to stage the changes after formatting

--- apps/web/src/components/AssigneeButton/AssigneeButton.interfaces.ts
+++ apps/web/src/components/AssigneeButton/AssigneeButton.interfaces.ts
@@ -1,7 +1,7 @@
 import type { Dispatch, SetStateAction } from "react";
 
 export interface AssigneeButtonProps {
-  location: string;
-  showAssigneeDropdown: boolean;
-  setShowAssigneeDropdown: Dispatch<SetStateAction<boolean>>;
+	location: string;
+	showAssigneeDropdown: boolean;
+	setShowAssigneeDropdown: Dispatch<SetStateAction<boolean>>;
 }
