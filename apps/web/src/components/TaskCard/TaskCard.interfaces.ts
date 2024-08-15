70833e6e7cf19788732e84031bdc6b35e7d33025
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-14 23:50:12 -0700

    fix: taskcardprops

--- apps/web/src/components/TaskCard/TaskCard.interfaces.ts
+++ apps/web/src/components/TaskCard/TaskCard.interfaces.ts
@@ -3,7 +3,7 @@ import type { Task } from "@/store/taskData/taskData.interfaces";
 
 export interface TaskCardProps {
 	filteredTasks: Task[];
-	setTaskData: (task: Task) => void | undefined;
+	setTaskData: ((task: Task) => void) | undefined;
 	setShowRenameModal?: (show: boolean) => void;
 	highlightText: (text: string) => string | React.ReactNode;
 	location: string;
