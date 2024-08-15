7672b199811f238cab9a8ca83525f371a4c30749
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-14 22:33:46 -0700

    fix: merging bug

--- apps/web/src/components/TaskCard/index.tsx
+++ apps/web/src/components/TaskCard/index.tsx
@@ -21,9 +21,9 @@ import type { TaskCardProps } from "./TaskCard.interfaces";
 import type { AppDispatch, RootState } from "@/store";
 import type { Task } from "@/store/taskData/taskData.interfaces";
 import { deleteTaskCard } from "@/api/taskApi";
+import { formatUrl } from "@/utils/formatting";
 import { ContextMenu, ContextMenuTrigger } from "../ui/context-menu";
 import TaskContextMenu from "../TaskContextMenu";
-import { formatUrl } from "@/utils/formatting";
 
 const TaskCard = ({
 	filteredTasks,
@@ -82,289 +82,284 @@ const TaskCard = ({
 		setDeleteFade(false);
 	};
 
+	const handleContextMenu = (
+		e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+		task: Task,
+	) => {
+		e.preventDefault();
+		setTaskData?.(task);
+		setSelectedTask(task);
+		setMenuPosition({ x: e.clientX, y: e.clientY });
+	};
+
+	const navigateToTask = async (task: Task) => {
+		dispatch(setTaskPage(task));
+		router.push(
+			`/${currentTeam.name}/task/${currentTeam.identifier}/${formatUrl(task.title)}`,
+		);
+	};
+
 	const copyToClipboard = (taskId: string) => {
 		navigator.clipboard.writeText(`${window.location.origin}/tasks/${taskId}`);
 		setIsCopied(true);
 		setTimeout(() => {
 			setIsCopied(false);
 		}, 3000);
+	};
 
-		const handleContextMenu = (
-			e: React.MouseEvent<HTMLDivElement, MouseEvent>,
-			task: Task,
-		) => {
-			e.preventDefault();
-			setTaskData?.(task);
-			setSelectedTask(task);
-			setMenuPosition({ x: e.clientX, y: e.clientY });
-		};
+	const handleGlobalClick = () => {
+		setMenuPosition(null);
+	};
 
-		const navigateToTask = async (task: Task) => {
-			dispatch(setTaskPage(task));
-			router.push(
-				`/${currentTeam.name}/task/${currentTeam.identifier}/${formatUrl(task.title)}`,
-			);
-		};
+	useEffect(() => {
+		dispatch(getAllUsers(currentWorkspace._id));
+	}, [currentWorkspace._id, dispatch]);
 
-		const handleGlobalClick = () => {
-			setMenuPosition(null);
-		};
-
-		useEffect(() => {
-			dispatch(getAllUsers(currentWorkspace._id));
-		}, [currentWorkspace._id, dispatch]);
-
-		useEffect(() => {
-			function handleClickAway(e: MouseEvent) {
-				if (
-					!Object.values(taskRefs.current).some((taskEl) =>
-						taskEl?.contains(e.target as Node),
-					)
-				) {
-					setMenuPosition(null);
-				}
+	useEffect(() => {
+		function handleClickAway(e: MouseEvent) {
+			if (
+				!Object.values(taskRefs.current).some((taskEl) =>
+					taskEl?.contains(e.target as Node),
+				)
+			) {
+				setMenuPosition(null);
 			}
+		}
 
-			document.addEventListener("mousedown", handleClickAway);
-			return () => {
-				document.removeEventListener("mousedown", handleClickAway);
-			};
-		}, []);
-		return (
-			<>
-				{view === "list" &&
-					location === "dashboard" &&
-					filteredTasks?.map((task, index) => (
-						<Draggable draggableId={task._id} index={index} key={task._id}>
-							{(provided) => (
-								<div
-									id={"this"}
-									{...provided.draggableProps}
-									{...provided.dragHandleProps}
-									ref={provided.innerRef}
-									onClick={handleGlobalClick}
-									onContextMenu={(e) => handleContextMenu(e, task)}
-								>
-									<ContextMenu>
-										<ContextMenuTrigger>
-											<div
-												ref={(el: HTMLDivElement | null) => {
-													taskRefs.current[task._id] = el;
-												}}
-											>
-												<TaskContextMenu
-													task={task}
-													setIsCopied={setIsCopied}
-													copyToClipboard={copyToClipboard}
-												/>
-											</div>
-
-											<div
-												className={`relative group/main grid grid-cols-24 items-center w-full py-2 text-blue bg-card border-t border-solid border-border hover:bg-accent ${
-													index === filteredTasks.length - 1 && "rounded-b-lg"
-												}`}
-											>
-												<div className="group/select w-10 col-span-1 flex justify-end items-center pl-2 ml-3.5">
-													<div className="hidden transition ease-in-out duration-200 sm:group-hover/main:hidden xs:group-hover/main:hidden md:group-hover/main:block md:group-hover/select:-translate-x-2">
-														<GripVertical className="size-5" />
-													</div>
-													<div className="xs:mr-5 sm:mr-5 md:mr-4">
-														<input
-															title="input"
-															className="appearance-none checked:bg-primary/80 form-checkbox border border-checkbox md:hidden rounded group-hover/select:block sm:block xs:block w-[13px] h-[13px]"
-															type="checkbox"
-														/>
-													</div>
-												</div>
-												<div
-													onClick={() => navigateToTask(task)}
-													className="grid grid-cols-10 col-span-23 pl-2 pr-6 lg:pl-0"
-												>
-													<div className="col-span-10 text-foreground">
-														<TaskCardTitle
-															key={task._id}
-															task={task}
-															isShown={showPriority}
-															taskTitle={task.title}
-															location={location}
-															highlightText={highlightText}
-														/>
-													</div>
-												</div>
-											</div>
-										</ContextMenuTrigger>
-									</ContextMenu>
-								</div>
-							)}
-						</Draggable>
-					))}
-				{location === "dashboard" &&
-					view === "grid" &&
-					filteredTasks
-						?.filter((task) => {
-							if (!uniqueTasks.includes(task)) {
-								uniqueTasks.push(task);
-								return task;
-							}
-						})
-						.map((task, index) => {
-							return (
-								<Draggable draggableId={task._id} index={index} key={task._id}>
-									{(provided) => (
+		document.addEventListener("mousedown", handleClickAway);
+		return () => {
+			document.removeEventListener("mousedown", handleClickAway);
+		};
+	}, []);
+	return (
+		<>
+			{view === "list" &&
+				location === "dashboard" &&
+				filteredTasks?.map((task, index) => (
+					<Draggable draggableId={task._id} index={index} key={task._id}>
+						{(provided) => (
+							<div
+								id={"this"}
+								{...provided.draggableProps}
+								{...provided.dragHandleProps}
+								ref={provided.innerRef}
+								onClick={handleGlobalClick}
+								onContextMenu={(e) => handleContextMenu(e, task)}
+							>
+								<ContextMenu>
+									<ContextMenuTrigger>
 										<div
-											{...provided.draggableProps}
-											{...provided.dragHandleProps}
-											ref={provided.innerRef}
-											onClick={handleGlobalClick}
-											onContextMenu={(e) => handleContextMenu(e, task)}
+											ref={(el: HTMLDivElement | null) => {
+												taskRefs.current[task._id] = el;
+											}}
 										>
-											<ContextMenu>
-												<ContextMenuTrigger>
-													<div
-														ref={(el: HTMLDivElement | null) => {
-															taskRefs.current[task._id] = el;
-														}}
-													>
-														<TaskContextMenu
-															task={task}
-															setIsCopied={setIsCopied}
-															copyToClipboard={copyToClipboard}
-														/>
-													</div>
-													<Link
-														href={`/${currentTeam.name}/task/${currentTeam.identifier}/${formatUrl(task.title)}`}
-														onClick={() => dispatch(setTaskPage(task))}
-													>
-														<div className="relative w-[325px]">
-															<div
-																key={task._id}
-																className={`cursor-pointer flex flex-col justify-center w-full p-4 text-blue text-foreground bg-card rounded-lg shadow border dark:border-none hover:bg-accent space-y-4 ${
-																	theme === "light"
-																		? "bg-card"
-																		: "bg-background"
-																}`}
-															>
-																<TaskCardTitle
-																	task={task}
-																	taskTitle={task.title}
-																	location={location}
-																	highlightText={highlightText}
-																	isShown={showPriority}
-																/>
-																{showDateTime && (
-																	<TaskCardDate
-																		icon={
-																			<Calendar className="cursor-pointer size-4" />
-																		}
-																	>
-																		Due Date:{" "}
-																		{task.dueDate
-																			? format(
-																					new Date(task.dueDate),
-																					"M/d/yy, h:mm a",
-																				)
-																			: "No Date Set"}
-																	</TaskCardDate>
+											<TaskContextMenu
+												task={task}
+												setIsCopied={setIsCopied}
+												copyToClipboard={copyToClipboard}
+											/>
+										</div>
+
+										<div
+											className={`relative group/main grid grid-cols-24 items-center w-full py-2 text-blue bg-card border-t border-solid border-border hover:bg-accent ${
+												index === filteredTasks.length - 1 && "rounded-b-lg"
+											}`}
+										>
+											<div className="group/select w-10 col-span-1 flex justify-end items-center pl-2 ml-3.5">
+												<div className="hidden transition ease-in-out duration-200 sm:group-hover/main:hidden xs:group-hover/main:hidden md:group-hover/main:block md:group-hover/select:-translate-x-2">
+													<GripVertical className="size-5" />
+												</div>
+												<div className="xs:mr-5 sm:mr-5 md:mr-4">
+													<input
+														title="input"
+														className="appearance-none checked:bg-primary/80 form-checkbox border border-checkbox md:hidden rounded group-hover/select:block sm:block xs:block w-[13px] h-[13px]"
+														type="checkbox"
+													/>
+												</div>
+											</div>
+											<div
+												onClick={() => navigateToTask(task)}
+												className="grid grid-cols-10 col-span-23 pl-2 pr-6 lg:pl-0"
+											>
+												<div className="col-span-10 text-foreground">
+													<TaskCardTitle
+														key={task._id}
+														task={task}
+														isShown={showPriority}
+														taskTitle={task.title}
+														location={location}
+														highlightText={highlightText}
+													/>
+												</div>
+											</div>
+										</div>
+									</ContextMenuTrigger>
+								</ContextMenu>
+							</div>
+						)}
+					</Draggable>
+				))}
+			{location === "dashboard" &&
+				view === "grid" &&
+				filteredTasks
+					?.filter((task) => {
+						if (!uniqueTasks.includes(task)) {
+							uniqueTasks.push(task);
+							return task;
+						}
+					})
+					.map((task, index) => {
+						return (
+							<Draggable draggableId={task._id} index={index} key={task._id}>
+								{(provided) => (
+									<div
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										ref={provided.innerRef}
+										onClick={handleGlobalClick}
+										onContextMenu={(e) => handleContextMenu(e, task)}
+									>
+										<ContextMenu>
+											<ContextMenuTrigger>
+												<div
+													ref={(el: HTMLDivElement | null) => {
+														taskRefs.current[task._id] = el;
+													}}
+												>
+													<TaskContextMenu
+														task={task}
+														setIsCopied={setIsCopied}
+														copyToClipboard={copyToClipboard}
+													/>
+												</div>
+												<Link
+													href={`/${currentTeam.name}/task/${currentTeam.identifier}/${formatUrl(task.title)}`}
+													onClick={() => dispatch(setTaskPage(task))}
+												>
+													<div className="relative w-[325px]">
+														<div
+															key={task._id}
+															className={`cursor-pointer flex flex-col justify-center w-full p-4 text-blue text-foreground bg-card rounded-lg shadow border dark:border-none hover:bg-accent space-y-4 ${
+																theme === "light" ? "bg-card" : "bg-background"
+															}`}
+														>
+															<TaskCardTitle
+																task={task}
+																taskTitle={task.title}
+																location={location}
+																highlightText={highlightText}
+																isShown={showPriority}
+															/>
+															{showDateTime && (
+																<TaskCardDate
+																	icon={
+																		<Calendar className="cursor-pointer size-4" />
+																	}
+																>
+																	Due Date:{" "}
+																	{task.dueDate
+																		? format(
+																				new Date(task.dueDate),
+																				"M/d/yy, h:mm a",
+																			)
+																		: "No Date Set"}
+																</TaskCardDate>
+															)}
+															<div className="flex flex-row items-center space-x-4">
+																{showPriority && (
+																	<TaskCardPriority border={true} task={task} />
 																)}
-																<div className="flex flex-row items-center space-x-4">
-																	{showPriority && (
-																		<TaskCardPriority
-																			border={true}
-																			task={task}
-																		/>
-																	)}
-																	{showLabels && (
-																		<TaskCardLabels task={task} view="grid" />
-																	)}
-																</div>
+																{showLabels && (
+																	<TaskCardLabels task={task} view="grid" />
+																)}
 															</div>
 														</div>
-													</Link>
-												</ContextMenuTrigger>
-											</ContextMenu>
+													</div>
+												</Link>
+											</ContextMenuTrigger>
+										</ContextMenu>
+									</div>
+								)}
+							</Draggable>
+						);
+					})}
+			{location === "search" &&
+				filteredTasks?.map((task) => (
+					<div
+						id={"this"}
+						key={task._id}
+						onClick={handleGlobalClick}
+						onContextMenu={(e) => handleContextMenu(e, task)}
+					>
+						<ContextMenu>
+							<ContextMenuTrigger>
+								<div
+									ref={(el: HTMLDivElement | null) => {
+										taskRefs.current[task._id] = el;
+									}}
+								>
+									<TaskContextMenu
+										task={task}
+										setIsCopied={setIsCopied}
+										copyToClipboard={copyToClipboard}
+									/>
+								</div>
+								<div className="relative group/main grid grid-cols-24 items-center w-full py-2 text-blue bg-card border-t border-solid border-border hover:bg-accent">
+									<div className="group/select w-10 col-span-1 flex justify-end items-center pl-2 ml-3.5">
+										<div className="hidden transition ease-in-out duration-200 sm:group-hover/main:hidden xs:group-hover/main:hidden md:group-hover/main:block md:group-hover/select:-translate-x-2">
+											<GripVertical className="size-5" />
 										</div>
-									)}
-								</Draggable>
-							);
-						})}
-				{location === "search" &&
-					filteredTasks?.map((task) => (
-						<div
-							id={"this"}
-							key={task._id}
-							onClick={handleGlobalClick}
-							onContextMenu={(e) => handleContextMenu(e, task)}
-						>
-							<ContextMenu>
-								<ContextMenuTrigger>
-									<div
-										ref={(el: HTMLDivElement | null) => {
-											taskRefs.current[task._id] = el;
-										}}
+										<div className="xs:mr-5 sm:mr-5 md:mr-4">
+											<input
+												title="input"
+												className="appearance-none checked:bg-primary/80 form-checkbox border border-checkbox md:hidden rounded group-hover/select:block sm:block xs:block w-[13px] h-[13px]"
+												type="checkbox"
+											/>
+										</div>
+									</div>
+									<Link
+										href={`/${currentTeam.name}/task/${currentTeam.identifier}/${formatUrl(task.title)}`}
+										onClick={() => setTaskPage(task)}
+										className="grid grid-cols-10 col-span-23 pl-2 pr-6 lg:pl-0"
 									>
-										<TaskContextMenu
-											task={task}
-											setIsCopied={setIsCopied}
-											copyToClipboard={copyToClipboard}
-										/>
-									</div>
-									<div className="relative group/main grid grid-cols-24 items-center w-full py-2 text-blue bg-card border-t border-solid border-border hover:bg-accent">
-										<div className="group/select w-10 col-span-1 flex justify-end items-center pl-2 ml-3.5">
-											<div className="hidden transition ease-in-out duration-200 sm:group-hover/main:hidden xs:group-hover/main:hidden md:group-hover/main:block md:group-hover/select:-translate-x-2">
-												<GripVertical className="size-5" />
-											</div>
-											<div className="xs:mr-5 sm:mr-5 md:mr-4">
-												<input
-													title="input"
-													className="appearance-none checked:bg-primary/80 form-checkbox border border-checkbox md:hidden rounded group-hover/select:block sm:block xs:block w-[13px] h-[13px]"
-													type="checkbox"
-												/>
+										<div className="col-span-10 text-foreground">
+											<TaskCardTitle
+												key={task._id}
+												task={task}
+												isShown={showPriority}
+												taskTitle={task.title}
+												highlightText={highlightText}
+												location={location}
+											/>
+										</div>
+										<div className="flex justify-end col-span-4 items-center lg:pr-5">
+											<div className="flex justify-end col-span-3 items-center pl-3.5">
+												{user && (
+													<ProfileImage
+														profileName={user.name}
+														location="taskCard"
+													/>
+												)}
 											</div>
 										</div>
-										<Link
-											href={`/${currentTeam.name}/task/${currentTeam.identifier}/${formatUrl(task.title)}`}
-											onClick={() => setTaskPage(task)}
-											className="grid grid-cols-10 col-span-23 pl-2 pr-6 lg:pl-0"
-										>
-											<div className="col-span-10 text-foreground">
-												<TaskCardTitle
-													key={task._id}
-													task={task}
-													isShown={showPriority}
-													taskTitle={task.title}
-													highlightText={highlightText}
-													location={location}
-												/>
-											</div>
-											<div className="flex justify-end col-span-4 items-center lg:pr-5">
-												<div className="flex justify-end col-span-3 items-center pl-3.5">
-													{user && (
-														<ProfileImage
-															profileName={user.name}
-															location="taskCard"
-														/>
-													)}
-												</div>
-											</div>
-										</Link>
-									</div>
-								</ContextMenuTrigger>
-							</ContextMenu>
-						</div>
-					))}
+									</Link>
+								</div>
+							</ContextMenuTrigger>
+						</ContextMenu>
+					</div>
+				))}
 
-				{showDeleteCard && selectedTask && (
-					// Not sure if this Component is being used, because there is no confirmation before delete
-					<DeleteConfirmCard
-						task={selectedTask}
-						handleDeleteTaskCard={handleDeleteTaskCard}
-						onClose={handleCloseDeleteCard}
-						deleteFade={deleteFade}
-					/>
-				)}
-			</>
-		);
-	};
+			{showDeleteCard && selectedTask && (
+				// Not sure if this Component is being used, because there is no confirmation before delete
+				<DeleteConfirmCard
+					task={selectedTask}
+					handleDeleteTaskCard={handleDeleteTaskCard}
+					onClose={handleCloseDeleteCard}
+					deleteFade={deleteFade}
+				/>
+			)}
+		</>
+	);
 };
 
 export default TaskCard;
