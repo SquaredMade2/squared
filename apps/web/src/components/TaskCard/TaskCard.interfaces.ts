84816d62376a7e7f0bea9e493ad0149ceaec4129
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-14 23:42:46 -0700

    fix: type error fix

--- apps/web/src/components/TaskCard/TaskCard.interfaces.ts
+++ apps/web/src/components/TaskCard/TaskCard.interfaces.ts
@@ -3,7 +3,7 @@ import type { Task } from "@/store/taskData/taskData.interfaces";
 
 export interface TaskCardProps {
 	filteredTasks: Task[];
-	setTaskData: (taskData: Task) => void;
+	setTaskData: (task: Task) => void | undefined;
 	setShowRenameModal?: (show: boolean) => void;
 	highlightText: (text: string) => string | React.ReactNode;
 	location: string;
