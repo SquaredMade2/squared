ff265f14937d2d31794f448e55912ae59c18209c
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-19 19:27:13 +1000

    feat: implemented teams functionality in new store/backend

--- apps/web/src/storeZ/teams/interfaces.ts
+++ apps/web/src/storeZ/teams/interfaces.ts
@@ -9,6 +9,12 @@ export type TeamActions = {
 	getTeam: (
 		teamId: string,
 	) => (state: TeamState) => Promise<Team | undefined> | Team;
+	updateTeam: (
+		teamId: string,
+		team: Partial<Team>,
+	) => (state: TeamState) => Promise<Team>;
+	deleteTeam: (teamId: string) => (state: TeamState) => void;
+	getAllTeams: (workspaceId: string) => (state: TeamState) => Promise<Team[]>;
 };
 
 export type TeamStore = TeamState & TeamActions;
