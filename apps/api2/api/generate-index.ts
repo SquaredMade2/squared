7fcad39929b76b4934d746850b380c8d881dcea7
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-20 17:42:35 +1000

    fix: adjusted default port

--- apps/api2/api/generate-index.ts
+++ apps/api2/api/generate-index.ts
@@ -89,7 +89,7 @@ export function createApiRouter(router: Router, deps: AllRouteDeps) {`);
   // Adding the server setup to the generated index file
   writeLn(`
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5173;
 
 // Initialize the router
 const router = express.Router();
--- apps/api2/api/index.ts
+++ apps/api2/api/index.ts
@@ -68,7 +68,7 @@ export function createApiRouter(router: Router, deps: AllRouteDeps) {
 }
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5173;
 
 // Initialize the router
 const router = express.Router();
