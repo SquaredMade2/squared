b7f79d10c52aa6e4afb4d592e48ed3599c23b479
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-23 09:44:36 +1000

    ci: adjusted branch name in version workflow

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -93,15 +93,15 @@ mongoose
 		console.log("Database Connection Error", err);
 	});
 
-  const vercelAccess: StaticOrigin = /[\w-\/:]*squared-52c50d26\.vercel\.app/
+const vercelAccess: StaticOrigin = /[\w-\/:]*squared-52c50d26\.vercel\.app/;
 
 const whitelist = [
-  "http://localhost:3000",
-  `http://localhost:${PORT}`,
-  "https://app.squaredmade.com",
-  "https://develop.squaredmade.com",
-  "https://squared-web.vercel.app",
-  vercelAccess
+	"http://localhost:3000",
+	`http://localhost:${PORT}`,
+	"https://app.squaredmade.com",
+	"https://develop.squaredmade.com",
+	"https://squared-web.vercel.app",
+	vercelAccess,
 ];
 
 type StaticOrigin =
