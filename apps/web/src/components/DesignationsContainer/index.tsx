7ff7a24e55436c7bb1b2341f618d658f749f6da3
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-29 21:40:42 -0500

    feat: refactored sidebar status button

--- apps/web/src/components/DesignationsContainer/index.tsx
+++ apps/web/src/components/DesignationsContainer/index.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import LabelButton from "@/components/LabelButton";
 import PriorityButton from "@/components/PriorityButton";
-import StatusButton from "@/components/StatusButton";
-import { StatusDropdownRefactored } from "../StatusDropdownRefactored/intex";
+import { StatusDropdownButton } from "@/components/StatusDropdownButton/intex";
 import DateButton from "@/components/DateButton";
 import EffortEstimateButton from "@/components/EffortEstimateButton";
 import HelpButton from "@/components/HelpButton";
@@ -120,17 +119,17 @@ const DesignationsContainer = ({ location }: DesignationsContainerProps) => {
 		<>
 			{location === "newIssue" && (
 				<div className="flex flex-row flex-start items-center h-9">
-					<StatusDropdownRefactored location={location} />
-					{/* <PriorityButton location={location} />
+					<StatusDropdownButton location={location} />
+					<PriorityButton location={location} />
 					<LabelButton location={location} />
 					<DateButton location={location} />
-					<EffortEstimateButton location={location} /> */}
+					<EffortEstimateButton location={location} />
 				</div>
 			)}
 
 			{location === "issueSidebar" && (
 				<div className="flex flex-col relative w-full z-[1] rounded-lg p-5 gap-5 bg-card">
-					{generateItemContainer("Status", StatusButton, location)}
+					{generateItemContainer("Status", StatusDropdownButton, location)}
 					{generateItemContainer("Priority", PriorityButton, location)}
 					{generateItemContainer("Labels", LabelButton, location)}
 					{generateItemContainer("Due Date", DateButton, location)}
