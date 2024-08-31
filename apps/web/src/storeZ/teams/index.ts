eabfd92a4c67b8ff0122a1efefaf268576ba6b3d
THEjacob1000 <jacob@jacobdevelops.com>
2024-08-31 17:09:17 +1000

    feat: update team store

--- apps/web/src/storeZ/teams/index.ts
+++ apps/web/src/storeZ/teams/index.ts
@@ -4,6 +4,7 @@ import type { TeamState, TeamStore } from "./interfaces";
 import axios from "axios";
 import type { Team } from "@repo/db";
 import { persist } from "zustand/middleware";
+import { useTeamStore } from "../provider";
 
 const apiString = (path: string) =>
 	`${process.env.NEXT_PUBLIC_SERVERZ}/api/team/${path}`;
@@ -13,36 +14,31 @@ export const createTeamStore = (initState: TeamState = { teams: [] }) => {
 		persist(
 			(set) => ({
 				...initState,
-				addTeam: (team) => async (state) => {
+				addTeam: async (team) => {
 					const newTeam: Team = await axios.post(apiString(team.id), team);
-					newTeam &&
-						set({
-							teams: [...state.teams, newTeam],
-						});
+					const { teams } = useTeamStore();
+					set({ teams: [...teams, newTeam] });
 					return team;
 				},
-				getTeam: (teamId) => async (state) => {
-					const stateTeam = state.teams.find((t) => t.id === teamId);
+				getTeam: async (teamId) => {
+					const { teams } = useTeamStore();
+					const stateTeam = teams.find((t) => t.id === teamId);
 					return stateTeam || (await axios.get(apiString(teamId)));
 				},
-				updateTeam: (teamId, team) => async (state) => {
+				updateTeam: async (teamId, team) => {
 					const updatedTeam: Team = await axios.put(apiString(teamId), team);
-					set({
-						teams: state.teams.map((t) => (t.id === teamId ? updatedTeam : t)),
-					});
+					const { teams } = useTeamStore();
+					set({ teams: teams.map((t) => (t.id === teamId ? updatedTeam : t)) });
 					return updatedTeam;
 				},
-				deleteTeam: (teamId) => (state) => {
-					axios.delete(apiString(teamId));
-					set({
-						teams: state.teams.filter((t) => t.id !== teamId),
-					});
+				deleteTeam: async (teamId) => {
+					await axios.delete(apiString(teamId));
+					const { teams } = useTeamStore();
+					set({ teams: teams.filter((t) => t.id !== teamId) });
 				},
-				getAllTeams: (workspaceId) => async () => {
+				getAllTeams: async (workspaceId) => {
 					const teams: Team[] = await axios.get(apiString(workspaceId));
-					set({
-						teams: teams,
-					});
+					set({ teams: teams });
 					return teams;
 				},
 			}),
