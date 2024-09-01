aa71c78d28d38779d8f882249352cd963b49a963
klexup <jameshensley37@gmail.com>
2024-09-01 02:44:45 -0400

    refactor: changed to used zustand

--- apps/web/src/app/[workspace]/WorkspaceNotFoundPage/index.tsx
+++ apps/web/src/app/[workspace]/WorkspaceNotFoundPage/index.tsx
@@ -1,28 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { useAppSelector } from "@/hooks/typeScriptReduxHooks";
 import { handleWorkspaceNameOverflow } from "@/utils/formatting";
 import WorkspaceInitials from "@/components/WorkspaceImage";
 import { Check, FileSearch } from "lucide-react";
+import { useAuthStore, useWorkspaceStore } from "@/storeZ/provider";
+import type { Workspace } from "@repo/db";
 
 const WorkspaceNotFoundPage = (): React.ReactElement => {
 	const router = useRouter();
-
 	const [menuOpen, setMenuOpen] = useState(false);
-
-	const user = useAppSelector((state) => state.userSettings.user);
-	const allWorkspaces = useAppSelector((state) => state.taskData.workspaces);
+	const [allWorkspaces, setAllWorkspaces] = useState<Workspace[]>([]);
+	const { user } = useAuthStore();
+	const { getAllWorkspaces } = useWorkspaceStore();
 	const { theme } = useAppSelector((state) => state.userSettings);
-	const currentWorkspace = useAppSelector(
-		(state) => state.taskData.currentWorkspace,
-	);
-
 	const handleOffClick: () => void = () => {
 		if (menuOpen) {
 			setMenuOpen(false);
 		}
 	};
+
+	useEffect(() => {
+		const handleGetAllWorkspaces = async () => {
+			if (user) {
+				const response = await getAllWorkspaces(user.id);
+				setAllWorkspaces(response);
+			}
+		};
+		handleGetAllWorkspaces();
+	}, []);
 
 	return (
 		<div
@@ -62,22 +69,19 @@ const WorkspaceNotFoundPage = (): React.ReactElement => {
 										legacyBehavior
 										href={`workspace/${workspace.url}`}
 										className="px-3 py-1.5 flex items-center hover:bg-popoverHover rounded text-sm font-medium cursor-default justify-between"
-										key={workspace._id}
+										key={workspace.id}
 									>
 										<div>
 											<div className="flex">
 												<WorkspaceInitials
-													workspaceName={workspace.name}
+													workspaceName={workspace.name ?? ""}
 													backgroundColor={index}
 													location="workspaceList"
 												/>
-												<li>{handleWorkspaceNameOverflow(workspace.name)}</li>
+												<li>
+													{handleWorkspaceNameOverflow(workspace.name ?? "")}
+												</li>
 											</div>
-											{workspace.name === currentWorkspace.name && (
-												<div>
-													<Check className="text-[#575BC7] size-5" />
-												</div>
-											)}
 										</div>
 									</Link>
 								))}
