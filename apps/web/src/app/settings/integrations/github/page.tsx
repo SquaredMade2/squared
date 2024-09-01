62add3a72da0d955afb206925c8e9f46062e2f46
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-08-31 23:22:18 -0400

    style: converted to card ui component

--- apps/web/src/app/settings/integrations/github/page.tsx
+++ apps/web/src/app/settings/integrations/github/page.tsx
@@ -4,6 +4,13 @@ import SettingsTopNavBar from "@/components/SettingsTopNavBar";
 import { useAppDispatch, useAppSelector } from "@/hooks/typeScriptReduxHooks";
 import { navBarToggle } from "@/store/userSettings";
 import { GithubIcon } from "@/components/Svg";
+import {
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const GithubSettings: React.FC = () => {
 	const dispatch = useAppDispatch();
@@ -34,7 +41,7 @@ const GithubSettings: React.FC = () => {
 				<SettingsTopNavBar setShowNavBar={handleNavToggle} />
 			</div>
 			<div className="flex flex-col h-full w-full items-center bg-background pt-20 md:items-center sm:items-start sm:px-4 xs:pt-10 xs:px-4">
-				<div className="w-full md:px-20 lg:px-80">
+				<div className="w-full md:px-20 lg:px-40 xl:px-80">
 					<div className="flex flex-row items-center mb-8 space-x-6">
 						<div className="flex flex-row justify-center items-center w-16 h-16 bg-white rounded-lg">
 							<div className="w-[50px]">
@@ -45,25 +52,23 @@ const GithubSettings: React.FC = () => {
 							Github
 						</header>
 					</div>
-					<span className="block w-full border-t border-border" />
-					<div className="flex items-center justify-between space-x-60">
-						<div className="flex flex-col justify-center h-24">
-							<h3 className="text-2xl text-foreground font-medium">
-								Connect Personal Account
-							</h3>
-							<header className="text-muted-foreground text-sm">
+					<Card className="flex justify-center items-center p-2">
+						<CardHeader>
+							<CardTitle>Connect Personal Account</CardTitle>
+							<CardDescription>
 								Connect your personal account to use the integration feature
-							</header>
+							</CardDescription>
+						</CardHeader>
+						<div className="flex justify-center items-center p-6">
+							<Button
+								onClick={handleClick}
+								className="w-24 h-16 rounded-lg bg-secondary hover:bg-primary hover:text-white"
+								variant="outline"
+							>
+								Connect
+							</Button>
 						</div>
-						<button
-							type="button"
-							onClick={handleClick}
-							className="w-24 h-16 rounded-lg bg-secondary hover:bg-primary"
-						>
-							Connect
-						</button>
-					</div>
-					<span className="block w-full border-t border-border" />
+					</Card>
 				</div>
 			</div>
 		</div>
--- apps/web/src/app/settings/integrations/page.tsx
+++ apps/web/src/app/settings/integrations/page.tsx
@@ -3,6 +3,12 @@ import SettingsTopNavBar from "@/components/SettingsTopNavBar";
 import { useAppDispatch, useAppSelector } from "@/hooks/typeScriptReduxHooks";
 import { navBarToggle } from "@/store/userSettings";
 import { GithubIcon } from "@/components/Svg";
+import {
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import Link from "next/link";
 
 const IntegrationSettings: React.FC = () => {
@@ -20,7 +26,7 @@ const IntegrationSettings: React.FC = () => {
 				<SettingsTopNavBar setShowNavBar={handleNavToggle} />
 			</div>
 			<div className="flex flex-col h-full w-full items-center bg-background pt-20 md:items-center sm:items-start sm:px-4 xs:pt-10 xs:px-10">
-				<div className="w-full md:px-20 lg:px-80">
+				<div className="w-full md:px-20 lg:px-40 xl:px-80">
 					<div className="flex-col mb-8">
 						<h3 className="text-2xl text-foreground mb-3 font-medium">
 							Integrations
@@ -29,42 +35,24 @@ const IntegrationSettings: React.FC = () => {
 							Enhance Squared experience by integrating add-ons
 						</header>
 					</div>
-					<span className="block w-full border-t border-border" />
-
 					<Link href={"/settings/integrations/github"}>
-						<div className="flex flex-row items-center h-36 hover:bg-secondary">
-							<div className="flex flex-row justify-center items-center w-20 h-16 bg-white rounded-lg ml-4">
-								<div className="w-[50px]">
-									<GithubIcon />
+						<Card className="hover:bg-secondary">
+							<CardHeader className="flex flex-row items-center justify-center">
+								<div className="flex justify-center items-center w-16 h-16 p-2 bg-white rounded-lg ml-4">
+									<div className="w-[50px]">
+										<GithubIcon />
+									</div>
 								</div>
-							</div>
-							<div className="flex flex-col mx-5 h-20">
-								<h3 className="text-2xl text-foreground mb-3 font-medium">
-									Github
-								</h3>
-								<header className="text-muted-foreground text-sm">
-									Automate your pull request and commit workflows and keep
-									issues synced both ways
-								</header>
-							</div>
-						</div>
+								<div className="flex flex-col mx-5 space-y-1">
+									<CardTitle>Github</CardTitle>
+									<CardDescription>
+										Automate your pull request and commit workflows and keep
+										issues synced both ways
+									</CardDescription>
+								</div>
+							</CardHeader>
+						</Card>
 					</Link>
-
-					<span className="block w-full border-t border-border" />
-					{/* Use format below for additional integration options */}
-					{/* <div className={styles.headerWrapper}>
-                        <div className={styles.githubLogoWrapper}>
-                            <div className={styles.githubIcon}>
-                                <GithubIcon />
-                            </div>
-                        </div>
-                        <div className={styles.headerText}>
-                            <h3 className={styles.title}>Other Integrations</h3>
-                            <header className={styles.subtitle}>
-                                For other integrations
-                            </header>
-                        </div>
-                    </div> */}
 				</div>
 			</div>
 		</div>
