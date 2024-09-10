c0748921a2173e4d3f359b41342f2f3ab4de2895
THEjacob1000 <jacob@jacobdevelops.com>
2024-09-10 17:22:39 +1000

    fix: fixed type error on task card

--- apps/web/src/components/TaskCardStatus/index.tsx
+++ apps/web/src/components/TaskCardStatus/index.tsx
@@ -1,27 +1,33 @@
 import React, { useState, useEffect, type ReactNode } from "react";
 import { filterInProgress } from "@/components/Svg";
 import type { TaskCardStatusProps } from "@/app/interfaces/Tasks.interfaces";
-import { Circle, CircleCheckBig, CircleDashed, CircleX } from "lucide-react";
+import {
+	Circle,
+	CircleCheckBig,
+	CircleDashed,
+	CircleFadingPlus,
+	CircleX,
+} from "lucide-react";
 
 const TaskCardStatus = ({ task }: TaskCardStatusProps) => {
 	const [svg, setSvg] = useState<ReactNode>();
 
 	useEffect(() => {
 		switch (task.status) {
+			case "backlog":
+				setSvg(<CircleDashed className="size-4" />);
+				break;
 			case "todo":
 				setSvg(<Circle className="size-4" />);
 				break;
 			case "inProgress":
 				setSvg(filterInProgress);
 				break;
-			case "backlog":
-				setSvg(<CircleDashed className="size-4" />);
+			case "inReview":
+				setSvg(<CircleFadingPlus className="size-4 text-green-400" />);
 				break;
 			case "done":
 				setSvg(<CircleCheckBig className="size-4 text-[#7394FF]" />);
-				break;
-			case "canceled":
-				setSvg(<CircleX className="size-4" />);
 				break;
 			default:
 				setSvg(<Circle className="size-4" />);
