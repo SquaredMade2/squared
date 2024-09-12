114a4b7749f99fcc9e1be99bef051769e63a7bd4
THEjacob1000 <jacob@jacobdevelops.com>
2024-09-12 13:32:34 +1000

    feat: styling of label menu

--- apps/web/src/components/LabelFilterDropDown/index.tsx
+++ apps/web/src/components/LabelFilterDropDown/index.tsx
@@ -10,74 +10,75 @@ import {
 	CommandList,
 	CommandItem,
 	CommandEmpty,
+	CommandGroup,
 } from "@/components/ui/command";
 import { useFilterStore, useWorkspaceStore } from "@/storeZ";
 import type { LabelFilterDropDownProps } from "./LabelFilterDropDown.interfaces";
 import type { FilterCondition } from "@/storeZ/filters";
+import type { Label } from "@repo/db";
+import { LabelColor } from "../LabelDropdownButton";
+import { Check } from "lucide-react";
 
 const LabelFilterDropDown = ({
 	showLabelFilterDropDown,
 	setShowLabelFilterDropDown,
 }: LabelFilterDropDownProps): React.ReactElement => {
-	const [query, setQuery] = useState<string>("");
-	const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
-	const { addFilter, removeFilter } = useFilterStore((state) => state);
 	const { workspaceLabels } = useWorkspaceStore((state) => state);
+	const [selectedLabels, setSelectedLabels] = useState<Label[]>([]);
+	const { addFilter, removeFilter } = useFilterStore((state) => state);
 
-	const handleSelect = (labelName: string) => {
+	const handleLabelChange = (label: Label, checked: boolean) => {
 		setSelectedLabels((prev) =>
-			prev.includes(labelName)
-				? prev.filter((label) => label !== labelName)
-				: [...prev, labelName],
+			checked ? [...prev, label] : prev.filter((item) => item !== label),
 		);
 	};
 
 	useEffect(() => {
-		// Apply filters based on the selected labels
 		if (selectedLabels.length > 0) {
-			for (const label of selectedLabels) {
-				const filterCondition: FilterCondition = {
-					field: "labels",
-					value: label,
-					operator: "arrayIncludesAny",
-				};
-				addFilter(filterCondition);
-			}
+			addFilter({
+				field: "labels",
+				value: selectedLabels.map((label) => label.id),
+				operator: "arrayIncludesAny",
+			});
 		} else {
 			removeFilter("labels");
 		}
-		setShowLabelFilterDropDown(false);
-	}, [selectedLabels]);
+	}, [selectedLabels, addFilter, removeFilter]);
 
 	return (
 		<Popover
 			open={showLabelFilterDropDown}
 			onOpenChange={setShowLabelFilterDropDown}
 		>
-			<PopoverTrigger>
-				<div className="hidden" aria-hidden="true" />
-			</PopoverTrigger>
-			<PopoverContent className="w-72 p-0 mt-4 mr-32" sideOffset={10}>
+			<PopoverTrigger />
+			<PopoverContent className="w-72 p-0" sideOffset={20}>
 				<Command>
-					<CommandInput
-						placeholder="Search labels..."
-						value={query}
-						onValueChange={setQuery}
-					/>
+					<CommandInput placeholder="Search labels..." />
+					<CommandEmpty>No Labels Found</CommandEmpty>
 					<CommandList>
-						<CommandEmpty>No results found.</CommandEmpty>
-						{workspaceLabels.map((item) => (
-							<CommandItem
-								key={item.id}
-								value={item.name}
-								onSelect={() => handleSelect(item.name)}
-							>
-								<div className="flex items-center space-x-2">
-									<div className={`size-3 rounded-full bg-[${item.color}]`} />,
-									<span>{item.name}</span>
-								</div>
-							</CommandItem>
-						))}
+						<CommandGroup>
+							{workspaceLabels.map((label) => (
+								<CommandItem
+									key={label.id}
+									onClick={() =>
+										handleLabelChange(label, selectedLabels.includes(label))
+									}
+								>
+									<div className="flex items-center space-x-2">
+										{selectedLabels.includes(label) ? (
+											<Check className="size-3" />
+										) : (
+											<div className="size-3" />
+										)}
+										<div
+											className="w-3 h-3 rounded-lg"
+											style={{ backgroundColor: label.color }}
+										/>
+										<span>{label.name}</span>
+									</div>
+								</CommandItem>
+							))}
+						</CommandGroup>
 					</CommandList>
 				</Command>
 			</PopoverContent>
