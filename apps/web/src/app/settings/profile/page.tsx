166b76c337ad3c0aa15537c6c9606f8f289cc802
klexup <jameshensley37@gmail.com>
2024-09-12 14:15:50 -0400

    refactor: profile page to use zustand & removed styles object

--- apps/web/src/app/settings/profile/page.tsx
+++ apps/web/src/app/settings/profile/page.tsx
@@ -1,52 +1,21 @@
 "use client";
 
 import { useState, type ReactElement } from "react";
-import { useSelector } from "react-redux";
 import { useToast } from "@/components/ui/use-toast";
 import type { InputChangeEvent } from "types";
-import { updateProfile, getUser } from "@/store/userSettings/thunks";
-import { useAppDispatch } from "@/hooks/typeScriptReduxHooks";
 import ProfileImage from "@/components/ProfileImage";
 import BlueButton from "@/components/BlueButton";
-import type { RootState } from "@/store";
-
-const styles = {
-	mainContainer: "flex mdsm:flex-col bg-card h-screen min-h-screen w-full",
-	pageContainer: "flex flex-col h-full w-full items-center bg-background pt-20",
-	pageWrapper: "w-1/3 mdsm:w-3/4",
-	title: "text-2xl text-foreground mb-1 font-medium",
-	profileSubTitle: "text-muted-foreground text-sm",
-	line: "block w-full border-t border-border my-6",
-	pictureTitle: "text-foreground text-sm mb-1.5",
-	emailTitle: "text-foreground text-sm",
-	email: "text-muted-foreground text-sm",
-	marginBottomSix: "mb-6",
-	fullNameTitle: "text-foreground text-sm",
-	input:
-		"w-full border border-border rounded focus:outline-none focus:ring-1 focus:ring-indigo-400 text-foreground py-1.5 px-3 text-sm mt-1.5 bg-textField",
-	usernameTitleWrapper: "flex items-center",
-	usernameTitle: "text-foreground text-sm",
-	usernameSubTitle: "text-muted-foreground font-normal text-xs ml-1",
-	updateButtonLight:
-		"mb-20 px-4 py-1 rounded text-foreground bg-purpleButtonHover hover:bg-purpleButton transition ease-out duration-100 box-content",
-	updateButtonDark:
-		"mb-20 px-4 py-1 rounded text-foreground bg-purpleButton hover:bg-purpleButtonHover transition ease-out duration-100 box-content",
-	navbarWrapper:
-		"relative mdsm:absolute -left-0 transition-all duration-300 ease-in-out",
-	TopNavbar: "lg:hidden mdsm:visible bg-background",
-	visible: "opacity-0 transition-all duration-300 ease-in-out",
-	notVisible: "opacity-100 transition-all duration-300 ease-in-out",
-};
+import { useAuthStore, useUserStore } from "@/storeZ";
 
 export default function Profile(): ReactElement {
 	const { toast } = useToast();
-	const dispatch = useAppDispatch();
-
-	const user = useSelector((state: RootState) => state.userSettings.user);
-	const [fullName, setFullName] = useState<string>(user.name);
-	const [username, setUsername] = useState<string>(user.username);
-	const prevFullname = user.name;
-	const prevUsername = user.username;
+	const user = useAuthStore((state) => state.user);
+	const updateUser = useUserStore((state) => state.updateUser);
+	const getUser = useUserStore((state) => state.getUser);
+	const [fullName, setFullName] = useState<string>(user?.name as string);
+	const [username, setUsername] = useState<string>(user?.username as string);
+	const prevFullname = user?.name;
+	const prevUsername = user?.username;
 	const valueChanged = fullName !== prevFullname || username !== prevUsername;
 
 	const handleUpdate = async () => {
@@ -56,10 +25,12 @@ export default function Profile(): ReactElement {
 				variant: "destructive",
 			});
 		}
-		if (valueChanged) {
-			const data = { name: fullName, username, id: user._id };
-			await dispatch(updateProfile(data));
-			dispatch(getUser());
+		if (user) {
+			if (valueChanged) {
+				const data = { name: fullName, username, id: user?.id };
+				await updateUser(user.id, data);
+				getUser(user.id);
+			}
 		}
 	};
 
@@ -74,48 +45,54 @@ export default function Profile(): ReactElement {
 	};
 
 	return (
-		<div className={styles.mainContainer}>
-			<div className={styles.pageContainer}>
-				<div className={styles.pageWrapper}>
+		<div className="flex mdsm:flex-col bg-card h-screen min-h-screen w-full">
+			<div className="flex flex-col h-full w-full items-center bg-background pt-20">
+				<div className="w-1/3 mdsm:w-3/4">
 					<div>
-						<h1 className={styles.title}>Profile</h1>
-						<p className={styles.profileSubTitle}>
+						<h1 className="text-2xl text-foreground mb-1 font-medium">
+							Profile
+						</h1>
+						<p className="text-muted-foreground text-sm">
 							Manage your Squared profile
 						</p>
 					</div>
-					<span className={styles.line} />
-					<p className={styles.pictureTitle}>Profile picture</p>
+					<span className="block w-full border-t border-border my-6" />
+					<p className="text-foreground text-sm mb-1.5">Profile picture</p>
 					<ProfileImage profileName={fullName} location="settings" />
-					<div className={styles.marginBottomSix}>
-						<p className={styles.emailTitle}>Email</p>
-						<p className={styles.email}>{user.email}</p>
+					<div className="mb-6">
+						<p className="text-foreground text-sm">Email</p>
+						<p className="text-muted-foreground text-sm">{user?.email}</p>
 					</div>
-					<div className={styles.marginBottomSix}>
-						<p className={styles.fullNameTitle}>Full name</p>
+					<div className="mb-6">
+						<p className="text-foreground text-sm">Full name</p>
 						<input
 							type="text"
-							className={`${styles.input} bg-background`}
+							className="w-full border border-border rounded focus:outline-none focus:ring-1 focus:ring-indigo-400 text-foreground py-1.5 px-3 text-sm mt-1.5 bg-background"
 							value={fullName}
 							onChange={handlefullNameChange}
 						/>
 					</div>
-					<div className={styles.marginBottomSix}>
-						<div className={styles.usernameTitleWrapper}>
-							<p className={styles.usernameTitle}>Username</p>
-							<p className={styles.usernameSubTitle}>
+					<div className="mb-6">
+						<div className="flex items-center">
+							<p className="text-foreground text-sm">Username</p>
+							<p className="text-muted-foreground font-normal text-xs ml-1">
 								- Nickname or first name, however you want to be called in
 								Squared
 							</p>
 						</div>
 						<input
 							type="text"
-							className={`${styles.input} bg-background`}
+							className="w-full border border-border rounded focus:outline-none focus:ring-1 focus:ring-indigo-400 text-foreground py-1.5 px-3 text-sm mt-1.5 bg-background"
 							value={username}
 							onChange={handleUsernameChange}
 						/>
 					</div>
 					<div
-						className={`${valueChanged ? styles.notVisible : styles.visible}`}
+						className={
+							valueChanged
+								? "opacity-100 transition-all duration-300 ease-in-out"
+								: "opacity-0 transition-all duration-300 ease-in-out"
+						}
 					>
 						<BlueButton description="Update" handleAction={handleUpdate} />
 					</div>
