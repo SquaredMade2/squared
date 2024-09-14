bf6c35637e71079f6db907d1536b894df888b0f2
THEjacob1000 <jacob@jacobdevelops.com>
2024-09-14 17:06:03 +1000

    style: single column on smaller screens

--- apps/web/src/app/settings/workspace/page.tsx
+++ apps/web/src/app/settings/workspace/page.tsx
@@ -126,7 +126,7 @@ export default function WorkspaceSettings() {
 
 			<Form {...form}>
 				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-					<div className="grid grid-cols-2 gap-4">
+					<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 						<FormField
 							control={form.control}
 							name="name"
