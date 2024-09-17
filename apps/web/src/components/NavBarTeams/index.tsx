115c9e286c78522095a2ebcaa466c4a4bb08a87a
THEjacob1000 <jacob@jacobdevelops.com>
2024-09-18 06:12:14 +1000

    feat: refactored and fixed NavbarTeams component

--- apps/web/src/components/NavBarTeams/index.tsx
+++ apps/web/src/components/NavBarTeams/index.tsx
@@ -3,16 +3,17 @@ import { useEffect } from "react";
 import { Copy, Layers3 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import type { NavBarTeamProps } from "./NavBarTeams.interfaces";
-import { useTeamStore, useWorkspaceStore } from "@/store";
+import { useTaskStore, useTeamStore, useWorkspaceStore } from "@/store";
+import { Button } from "../ui/button";
 
 const NavBarTeams = ({
-	onDropdownClick,
 	teamIdentifier,
 }: NavBarTeamProps): React.ReactElement => {
 	const currentWorkspace = useWorkspaceStore((state) => state.currentWorkspace);
 	const { teams, getAllTeams, getTeam, setCurrentTeam } = useTeamStore(
 		(state) => state,
 	);
+	const { getAllTasks } = useTaskStore((state) => state);
 
 	useEffect(() => {
 		if (!currentWorkspace) return;
@@ -32,79 +33,62 @@ const NavBarTeams = ({
 		const team = teams.find((team) => team.identifier === teamIdentifier);
 		if (team) {
 			const newTeam = await getTeam(team.id);
-			newTeam.team && setCurrentTeam(newTeam.team);
+			if (newTeam.team) {
+				setCurrentTeam(newTeam.team);
+				await getAllTasks(newTeam.team.id);
+			}
 		}
 	};
 
 	const handleViewsButtonClick: () => void = () => {
-		handleActiveParams("views");
 		getTeamOnSelect();
+		handleActiveParams("views");
 	};
 
 	return (
 		<div className="w-full z-10">
-			<div
-				className="w-full flex items-center my-1.5 hover:bg-secondary rounded-md pl-0.5 group"
-				onClick={onDropdownClick}
+			<Button
+				variant={"ghost"}
+				onClick={() => handleActiveParams("all")}
+				className="w-full justify-start h-6"
 			>
-				<button className="flex items-center cursor-pointer" type="button">
-					<div className="mr-2 p-0.5 rounded">
-						<Copy
-							className={"size-4 text-muted-foreground hover:text-accent"}
-						/>
-					</div>
-					<p>Issues</p>
-				</button>
-			</div>
-
+				<div className="mr-2 p-0.5 rounded">
+					<Copy className={"size-4 text-muted-foreground hover:text-accent"} />
+				</div>
+				<p>Issues</p>
+			</Button>
 			<div className="ml-2">
-				<div className="w-full border-y-0 border-r-0 border-l border-l-slate-600 pl-2 ml-1.5 my-0.5">
-					<div
-						className="w-full flex items-center my-1.5 hover:bg-secondary rounded-md pl-0.5"
+				<div className="w-full border-l border-border pl-2 ml-4 my-0.5">
+					{/* NOTE: Functionality broken till @napqueenkaila's PRs are merged */}
+					<Button
+						variant={"ghost"}
 						onClick={() => handleActiveParams("active")}
+						className="w-full justify-start h-6 pl-3"
 					>
-						<div>
-							<span className="pl-2 cursor-pointer">Active</span>
-						</div>
-					</div>
-					<div
-						className="w-full flex items-center my-1.5 hover:bg-secondary rounded-md pl-0.5"
+						Active
+					</Button>
+					{/* NOTE: Functionality broken till @napqueenkaila's PRs are merged */}
+					<Button
+						variant={"ghost"}
 						onClick={() => handleActiveParams("backlog")}
+						className="w-full justify-start h-6 pl-3"
 					>
-						<div>
-							<span className="pl-2 cursor-pointer">Backlog</span>
-						</div>
-					</div>
+						Backlog
+					</Button>
 				</div>
 			</div>
-
-			{/* 
-          Commented out broken links until all bugs are fixed and app is launched. Implementing
-          these links will take a while. ---> Pinak
-          <div className="w-full flex items-center my-1.5 hover:bg-secondary rounded-md pl-0.5 group">
-            <Link href={`/projects`}>
-            <button className="flex items-center cursor-pointer">
-              <div className="mr-2 p-0.5 rounded">
-                {<ProjectSquaresSub className="group-hover:fill-white" />}
-              </div>
-              <p>Projects</p>
-            </button>
-            </Link>
-          </div> 
-      */}
-
-			<button className="w-full" onClick={handleViewsButtonClick} type="button">
-				<div className="w-full flex items-center my-1.5 hover:bg-secondary rounded-md pl-0.5 group hover:text-foreground text-muted-foreground">
-					<div className="flex items-center cursor-pointer">
-						<div className="mr-2 p-0.5 rounded">
-							<Layers3
-								className={"size-4 hover:text-foreground text-muted-foreground"}
-							/>
-						</div>
-						<p>Views</p>
-					</div>
+			<Button
+				variant={"ghost"}
+				onClick={handleViewsButtonClick}
+				className="w-full justify-start h-6"
+			>
+				<div className="mr-2 p-0.5 rounded">
+					<Layers3
+						className={"size-4 hover:text-foreground text-muted-foreground"}
+					/>
 				</div>
-			</button>
+				<p>Views</p>
+			</Button>
 		</div>
 	);
 };
