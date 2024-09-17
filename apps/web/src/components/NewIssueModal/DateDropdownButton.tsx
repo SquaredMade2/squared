1534488992cf91baaf0714005b4cfb63f30713be
THEjacob1000 <jacob@jacobdevelops.com>
2024-09-17 19:42:49 +1000

    style: adjusted date format on new issue modal

--- apps/web/src/components/NewIssueModal/DateDropdownButton.tsx
+++ apps/web/src/components/NewIssueModal/DateDropdownButton.tsx
@@ -34,7 +34,7 @@ export function DateDropdownButton() {
 					)}
 				>
 					<CalendarIcon className="mr-2 h-4 w-4" />
-					{date ? format(date, "PPP") : <span>Pick a date</span>}
+					{date ? format(date, "dd MMM yyyy") : <span>Pick a date</span>}
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent className="w-auto p-0" side="left">
