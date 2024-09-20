28000b2c2d0aad20b30dbf70c14d7ca5c1e52461
NateCummens <78138781+NateCummens@users.noreply.github.com>
2024-09-20 14:14:46 -0400

    added test-db to activity for testing

--- apps/api/api/activity/[taskId]/index.ts
+++ apps/api/api/activity/[taskId]/index.ts
@@ -1,4 +1,4 @@
-import type { Prisma, Task } from "@repo/db";
+import type { Prisma, Task } from "@repo/test-db";
 import { prisma } from "@/api";
 import type { Route, APIResponse } from "@/api/route";
 import { v4 as uuidv4 } from "uuid";
