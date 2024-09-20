ad4cb63fc9c0be9ed89b3102fc38d888119d1a9b
NateCummens <78138781+NateCummens@users.noreply.github.com>
2024-09-20 16:37:29 -0400

    trying to get an error to print

--- apps/api/api/route.ts
+++ apps/api/api/route.ts
@@ -39,7 +39,7 @@ export type Route<P = Record<string, string>> = {
 };
 
 function handleNotSupported(_: unknown, res: Response) {
-	res.status(405).send();
+	res.status(405).send("at least its here");
 }
 
 export function toQueryHandler<P = Record<string, string>>(
