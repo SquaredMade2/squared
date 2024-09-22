d9b3d1f01250eab2575e6113678e3c561bb39463
napqueenkaila <k.m.bullard14@gmail.com>
2024-09-22 14:44:42 -0400

    fix: type error

--- apps/web/src/app/[workspace]/my-issues/page.tsx
+++ apps/web/src/app/[workspace]/my-issues/page.tsx
@@ -114,7 +114,7 @@ export default function MyIssues() {
 			{/* <TopNavBar /> */}
 
 			<ScrollArea className={view === "list" ? "max-h-[calc(100vh-55px)]" : ""}>
-				<ViewAllTasks handleDragEnd={handleDragEnd} tasks={tasks} />
+				<ViewAllTasks handleDragEnd={handleDragEnd} tasks={tasks} page="all" />
 				{view === "grid" && <ScrollBar orientation="horizontal" />}
 			</ScrollArea>
 		</div>
