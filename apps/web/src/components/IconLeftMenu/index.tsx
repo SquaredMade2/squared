3c6cd7c93edc0a4e3fd24abf6553669856dd25db
THEjacob1000 <jacob@jacobdevelops.com>
2024-09-22 15:15:28 +1000

    feat: initial commit

--- apps/web/src/components/IconLeftMenu/index.tsx
+++ apps/web/src/components/IconLeftMenu/index.tsx
@@ -1,29 +1,19 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import { useAuthStore, useModalStore, useWorkspaceStore } from "@/store";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import { useToast } from "@/components/ui/use-toast";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { config, type IconDefinition } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
 import {
-	faHouse,
-	faMagnifyingGlass,
-	faGear,
-	faInbox,
-	faSun,
-	faMoon,
-	faArrowRightFromBracket,
-} from "@fortawesome/free-solid-svg-icons";
-
-// Prevent FontAwesome from adding its CSS since we did it manually above
-config.autoAddCss = false;
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "../ui/tooltip";
+import { Home, Inbox, LogOut, Moon, Search, Settings, Sun } from "lucide-react";
 
 const IconLeftMenu = () => {
-	const [mounted, setMounted] = useState(false);
 	const router = useRouter();
 	const currentRoute = usePathname();
 	const { currentWorkspace: workspace } = useWorkspaceStore((state) => state);
@@ -35,10 +25,6 @@ const IconLeftMenu = () => {
 	const { toast } = useToast();
 	const logout = useAuthStore((state) => state.logout);
 	const currentYear: number = new Date().getFullYear();
-
-	useEffect(() => {
-		setMounted(true);
-	}, []);
 
 	const handleLogout = async (): Promise<void> => {
 		try {
@@ -59,48 +45,105 @@ const IconLeftMenu = () => {
 		homeRoute && !viewsRoute ? "" : router.back();
 	};
 
-	if (!mounted) {
-		return null; // Prevent rendering until client-side
-	}
-
 	return (
-		<div className="flex flex-col h-screen items-center justify-between w-12 py-2 bg-secondary">
-			<div className="flex flex-col items-center space-y-4">
-				<IconButton onClick={toHome} icon={faHouse} />
-				<IconButton
-					onClick={() => setShowCommand(true)}
-					icon={faMagnifyingGlass}
-				/>
-				<IconButton
-					onClick={() => navigateTo("settings/workspace")}
-					icon={faGear}
-				/>
-				<IconButton onClick={() => navigateTo("inbox")} icon={faInbox} />
-				<IconButton
-					onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-					icon={theme === "dark" ? faMoon : faSun}
-				/>
+		<TooltipProvider>
+			<div className="flex flex-col h-screen items-center justify-between w-12 py-2 bg-secondary">
+				<div className="flex flex-col items-center space-y-4">
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<Button variant="ghost" size="icon" onClick={toHome}>
+								<Home className="h-[1.2rem] w-[1.2rem]" />
+								<span className="sr-only">Home</span>
+							</Button>
+						</TooltipTrigger>
+						<TooltipContent side="right" align="center">
+							Home
+						</TooltipContent>
+					</Tooltip>
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<Button
+								variant="ghost"
+								size="icon"
+								onClick={() => setShowCommand(true)}
+							>
+								<Search className="h-[1.2rem] w-[1.2rem]" />
+								<span className="sr-only">Search</span>
+							</Button>
+						</TooltipTrigger>
+						<TooltipContent side="right" align="center">
+							Search
+						</TooltipContent>
+					</Tooltip>
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<Button
+								variant="ghost"
+								size="icon"
+								onClick={() => navigateTo("settings/workspace")}
+							>
+								<Settings className="h-[1.2rem] w-[1.2rem]" />
+								<span className="sr-only">Settings</span>
+							</Button>
+						</TooltipTrigger>
+						<TooltipContent side="right" align="center">
+							Settings
+						</TooltipContent>
+					</Tooltip>
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<Button
+								variant="ghost"
+								size="icon"
+								onClick={() => navigateTo("inbox")}
+							>
+								<Inbox className="h-[1.2rem] w-[1.2rem]" />
+								<span className="sr-only">Inbox</span>
+							</Button>
+						</TooltipTrigger>
+						<TooltipContent side="right" align="center">
+							Inbox
+						</TooltipContent>
+					</Tooltip>
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<Button
+								variant="ghost"
+								size="icon"
+								onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+							>
+								{theme === "dark" ? (
+									<Moon className="h-[1.2rem] w-[1.2rem]" />
+								) : (
+									<Sun className="h-[1.2rem] w-[1.2rem]" />
+								)}
+								<span className="sr-only">Toggle theme</span>
+							</Button>
+						</TooltipTrigger>
+						<TooltipContent side="right" align="center">
+							{theme === "dark"
+								? "Switch to Light Mode"
+								: "Switch to Dark Mode"}
+						</TooltipContent>
+					</Tooltip>
+				</div>
+				<small className="text-muted-foreground -rotate-90 whitespace-nowrap">
+					&copy; {currentYear} Squared. All rights reserved
+				</small>
+
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<Button variant="ghost" size="icon">
+							<LogOut onClick={handleLogout} />
+						</Button>
+					</TooltipTrigger>
+					<TooltipContent side="right" align="center">
+						Logout
+					</TooltipContent>
+				</Tooltip>
 			</div>
-			<small className="text-muted-foreground -rotate-90 whitespace-nowrap">
-				&copy; {currentYear} Squared. All rights reserved
-			</small>
-			<IconButton onClick={handleLogout} icon={faArrowRightFromBracket} />
-		</div>
+		</TooltipProvider>
 	);
 };
-
-const IconButton = ({
-	onClick,
-	icon,
-}: { onClick: () => void; icon: IconDefinition }) => (
-	<Button
-		variant="ghost"
-		size="icon"
-		className="text-muted-foreground hover:text-foreground"
-		onClick={onClick}
-	>
-		<FontAwesomeIcon icon={icon} className="h-5 w-5" />
-	</Button>
-);
 
 export default IconLeftMenu;
