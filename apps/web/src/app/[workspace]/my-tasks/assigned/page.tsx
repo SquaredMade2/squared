adcec609d91ac728f5eb807cbdd599824abcfad3
napqueenkaila <k.m.bullard14@gmail.com>
2024-09-26 19:26:49 -0400

    refactor: my-tasks routes to useTaskPage and TaskPageLayout

--- apps/web/src/app/[workspace]/my-tasks/assigned/page.tsx
+++ apps/web/src/app/[workspace]/my-tasks/assigned/page.tsx
@@ -1,69 +1,37 @@
 // assigned/page.tsx
 "use client";
 
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import ViewAllTasks from "@/components/ViewAllTasks";
-import { useAuthStore, useTaskStore, useViewStore } from "@/store";
-import { DragDropContext, type OnDragEndResponder } from "@hello-pangea/dnd";
-import { Status, type Task } from "@repo/db";
-import { useState } from "react";
+import { TaskPageLayout } from "@/components/ViewAllTasks/PageLayout";
+import { useTaskPage } from "@/hooks/useTaskPage";
+import { useFilterStore } from "@/store";
 
 export default function MyAssignedTasksPage() {
-	const { view } = useViewStore((state) => state);
-	const { user } = useAuthStore((state) => state);
-	const { tasks: initialTasks, updateTask } = useTaskStore((state) => state);
-	const [tasks, setTasks] = useState<Task[]>(
-		initialTasks.filter((t) => t.assigneeId === user?.id),
-	);
+	const { filterTasks } = useFilterStore((state) => state);
+	const {
+		loading,
+		authorized,
+		currentWorkspace,
+		teamIdentifier,
+		handleDragEnd,
+		getFilteredStatuses,
+		getTasksForStatus,
+	} = useTaskPage(filterTasks);
 
-	const handleDragEnd: OnDragEndResponder = async ({
-		destination,
-		source,
-		draggableId,
-	}) => {
-		if (!destination || destination.droppableId === source.droppableId) return;
-
-		const draggedTask = tasks.find((task) => task.id === draggableId);
-		if (!draggedTask) return;
-
-		const updatedTask = {
-			...draggedTask,
-			status: destination.droppableId as Status,
-		};
-		const updatedTasks = tasks.map((task) =>
-			task.id === draggableId ? updatedTask : task,
-		);
-		setTasks(updatedTasks); // directly set updated tasks in local state
-		await updateTask(updatedTask.id, { status: updatedTask.status }); //backend update
-	};
-
-	const titleArr: { value: Status; id: number }[] = [
-		{ value: Status.backlog, id: 1 },
-		{ value: Status.todo, id: 2 },
-		{ value: Status.inProgress, id: 3 },
-		{ value: Status.inReview, id: 4 },
-		{ value: Status.done, id: 5 },
-	];
-
-	const getFilteredStatuses = () => {
-		return titleArr.map((t) => t.value);
-	};
-
-	const getTasksForStatus = (status: Status) => {
-		return tasks.filter((task) => task.status === status);
-	};
+	if (!currentWorkspace) return null;
 
 	return (
-		<div className="w-full flex flex-col h-screen overflow-hidden container">
-			<ScrollArea className={view === "list" ? "max-h-[calc(100vh-55px)]" : ""}>
-				<DragDropContext onDragEnd={handleDragEnd}>
-					<ViewAllTasks
-						getFilteredStatuses={getFilteredStatuses}
-						getTasksForStatus={getTasksForStatus}
-					/>
-					{view === "grid" && <ScrollBar orientation="horizontal" />}
-				</DragDropContext>
-			</ScrollArea>
-		</div>
+		<TaskPageLayout
+			loading={loading}
+			authorized={authorized}
+			currentWorkspace={currentWorkspace}
+			teamIdentifier={teamIdentifier}
+			handleDragEnd={handleDragEnd}
+		>
+			<ViewAllTasks
+				getFilteredStatuses={getFilteredStatuses}
+				getTasksForStatus={getTasksForStatus}
+			/>
+		</TaskPageLayout>
 	);
 }
--- apps/web/src/app/[workspace]/my-tasks/layout.tsx
+++ apps/web/src/app/[workspace]/my-tasks/layout.tsx
@@ -1,16 +1,56 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { useWorkspaceStore } from "@/store";
+import {
+	// useAuthStore,
+	//  useTaskStore,
+	useWorkspaceStore,
+} from "@/store";
+// import type { Task } from "@repo/db";
 import { ArrowLeft } from "lucide-react";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
+import {
+	//  useEffect,
+	useState,
+} from "react";
 
 export default function MyTasksLayout({
 	children,
 }: { children: React.ReactNode }) {
 	const router = useRouter();
-	const path = usePathname();
+	const [activeTab, setActiveTab] = useState<"created" | "assigned">(
+		"assigned",
+	);
+	// const { user } = useAuthStore((state) => state);
 	const { currentWorkspace } = useWorkspaceStore((state) => state);
+	// const { tasks: initialTasks, getAllTasks } = useTaskStore((state) => state);
+	// const [tasks, setTasks] = useState<Task[]>(
+	// 	initialTasks.filter((t) => t.assigneeId === user?.id),
+	// );
+
+	// useEffect(() => {
+	// 	const initiateStore = async () => {
+	// 		if (initialTasks) {
+	// 			setTasks(initialTasks.filter((t) => t.assigneeId === user?.id));
+	// 		} else {
+	// 			await getAllTasks(currentWorkspace?.id ?? "");
+	// 		}
+	// 	};
+	// 	initiateStore();
+	// }, [user, currentWorkspace]);
+
+	// useEffect(() => {
+	// 	switch (activeTab) {
+	// 		case "assigned":
+	// 			setTasks(initialTasks.filter((t) => t.assigneeId === user?.id));
+	// 			break;
+	// 		case "created":
+	// 			setTasks(initialTasks.filter((t) => t.authorId === user?.id));
+	// 			break;
+	// 		default:
+	// 			break;
+	// 	}
+	// }, [activeTab, initialTasks]);
 
 	return (
 		<div className="flex w-full overflow-hidden relative">
@@ -21,19 +61,21 @@ export default function MyTasksLayout({
 					</Button>
 					<p className="hidden xl:block">My Tasks</p>
 					<Button
-						onClick={() =>
-							router.push(`/${currentWorkspace?.url}/my-tasks/assigned`)
-						}
-						variant={path.includes("assigned") ? "secondary" : "ghost"}
+						onClick={() => {
+							setActiveTab("assigned");
+							router.push(`/${currentWorkspace?.url}/my-tasks/assigned`);
+						}}
+						variant={activeTab === "assigned" ? "secondary" : "ghost"}
 						size="sm"
 					>
 						Assigned
 					</Button>
 					<Button
-						onClick={() =>
-							router.push(`/${currentWorkspace?.url}/my-tasks/created`)
-						}
-						variant={path.includes("created") ? "secondary" : "ghost"}
+						onClick={() => {
+							setActiveTab("created");
+							router.push(`/${currentWorkspace?.url}/my-tasks/created`);
+						}}
+						variant={activeTab === "created" ? "secondary" : "ghost"}
 						size="sm"
 					>
 						Created
