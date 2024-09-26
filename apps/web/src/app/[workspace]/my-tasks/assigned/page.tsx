80d0185dc269675dc2f8770d6f5a6aabb80dba35
napqueenkaila <k.m.bullard14@gmail.com>
2024-09-26 18:21:17 -0400

    refactor: move repetitive code to layout.tsx

--- apps/web/src/app/[workspace]/my-tasks/assigned/page.tsx
+++ apps/web/src/app/[workspace]/my-tasks/assigned/page.tsx
@@ -1,28 +1,16 @@
 // assigned/page.tsx
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import ViewAllTasks from "@/components/ViewAllTasks";
-import {
-	useAuthStore,
-	useTaskStore,
-	useViewStore,
-	useWorkspaceStore,
-} from "@/store";
+import { useAuthStore, useTaskStore, useViewStore } from "@/store";
 import { DragDropContext, type OnDragEndResponder } from "@hello-pangea/dnd";
 import { Status, type Task } from "@repo/db";
-import { ArrowLeft } from "lucide-react";
 import { useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
 
 export default function MyAssignedTasksPage() {
-	const router = useRouter();
-	const path = usePathname();
-	const isAssignedPath = path.includes("assigned");
 	const { view } = useViewStore((state) => state);
 	const { user } = useAuthStore((state) => state);
-	const { currentWorkspace } = useWorkspaceStore((state) => state);
 	const { tasks: initialTasks, updateTask } = useTaskStore((state) => state);
 	const [tasks, setTasks] = useState<Task[]>(
 		initialTasks.filter((t) => t.assigneeId === user?.id),
@@ -67,30 +55,6 @@ export default function MyAssignedTasksPage() {
 
 	return (
 		<div className="w-full flex flex-col h-screen overflow-hidden container">
-			<div className="flex justify-start space-x-4 my-4 items-center">
-				<Button size="icon" variant="ghost" onClick={() => router.back()}>
-					<ArrowLeft className="size-4" />
-				</Button>
-				<p className="hidden xl:block">My Tasks</p>
-				<Button
-					onClick={() =>
-						router.push(`/${currentWorkspace?.url}/my-tasks/assigned`)
-					}
-					variant={isAssignedPath ? "secondary" : "ghost"}
-					size="sm"
-				>
-					Assigned
-				</Button>
-				<Button
-					onClick={() =>
-						router.push(`/${currentWorkspace?.url}/my-tasks/created`)
-					}
-					variant={!isAssignedPath ? "secondary" : "ghost"}
-					size="sm"
-				>
-					Created
-				</Button>
-			</div>
 			<ScrollArea className={view === "list" ? "max-h-[calc(100vh-55px)]" : ""}>
 				<DragDropContext onDragEnd={handleDragEnd}>
 					<ViewAllTasks
--- apps/web/src/app/[workspace]/my-tasks/layout.tsx
+++ apps/web/src/app/[workspace]/my-tasks/layout.tsx
@@ -1,11 +1,46 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useWorkspaceStore } from "@/store";
+import { ArrowLeft } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
 export default function MyTasksLayout({
 	children,
 }: { children: React.ReactNode }) {
+	const router = useRouter();
+	const path = usePathname();
+	const { currentWorkspace } = useWorkspaceStore((state) => state);
+
 	return (
 		<div className="flex w-full overflow-hidden relative">
-			<main className="flex flex-grow overflow-hidden">{children}</main>
+			<main className="flex w-full flex-col h-screen flex-grow overflow-hidden container">
+				<div className="flex justify-start space-x-4 my-4 items-center">
+					<Button size="icon" variant="ghost" onClick={() => router.back()}>
+						<ArrowLeft className="size-4" />
+					</Button>
+					<p className="hidden xl:block">My Tasks</p>
+					<Button
+						onClick={() =>
+							router.push(`/${currentWorkspace?.url}/my-tasks/assigned`)
+						}
+						variant={path.includes("assigned") ? "secondary" : "ghost"}
+						size="sm"
+					>
+						Assigned
+					</Button>
+					<Button
+						onClick={() =>
+							router.push(`/${currentWorkspace?.url}/my-tasks/created`)
+						}
+						variant={path.includes("created") ? "secondary" : "ghost"}
+						size="sm"
+					>
+						Created
+					</Button>
+				</div>
+				{children}
+			</main>
 		</div>
 	);
 }
