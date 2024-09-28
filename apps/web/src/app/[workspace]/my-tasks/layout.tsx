84b9b719663165a21571fe3987a552872a25ca6c
napqueenkaila <k.m.bullard14@gmail.com>
2024-09-28 07:30:18 -0400

    fix: address pr comments

--- apps/web/src/app/[workspace]/my-tasks/layout.tsx
+++ apps/web/src/app/[workspace]/my-tasks/layout.tsx
@@ -4,11 +4,9 @@ export default function MyTasksLayout({
 	children,
 }: { children: React.ReactNode }) {
 	return (
-		<div className="flex w-full overflow-hidden relative">
-			<main className="flex w-full flex-col h-screen flex-grow overflow-hidden container">
-				<MyTasksNavbar />
-				{children}
-			</main>
-		</div>
+		<main className="flex w-full flex-col h-screen flex-grow overflow-hidden container">
+			<MyTasksNavbar />
+			{children}
+		</main>
 	);
 }
