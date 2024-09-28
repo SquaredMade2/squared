39a0f0d9580c629c4efa590d90a3f0600fbf2a91
napqueenkaila <k.m.bullard14@gmail.com>
2024-09-28 07:41:27 -0400

    fix: address pr comments

--- apps/web/src/components/MyTasksNavbar/index.tsx
+++ apps/web/src/components/MyTasksNavbar/index.tsx
@@ -1,43 +1,47 @@
 "use client";
 
 import { useWorkspaceStore } from "@/store";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useCallback } from "react";
 import { Button } from "../ui/button";
 import { ArrowLeft } from "lucide-react";
 
+const tabs = [
+	{ id: "assigned", label: "Assigned" },
+	{ id: "created", label: "Created" },
+] as const;
+
+type MyTasksPaths = (typeof tabs)[number]["id"];
+
 const MyTasksNavbar = () => {
 	const router = useRouter();
-	const [activeTab, setActiveTab] = useState<"created" | "assigned">(
-		"assigned",
-	);
+	const pathname = usePathname();
 	const { currentWorkspace } = useWorkspaceStore((state) => state);
+
+	const activeTab = pathname.split("/").pop() as MyTasksPaths;
+
+	const navigate = useCallback(
+		(path: MyTasksPaths) => {
+			router.push(`/${currentWorkspace?.url}/my-tasks/${path}`);
+		},
+		[router, currentWorkspace?.url],
+	);
 	return (
 		<div className="flex justify-start space-x-4 my-4 items-center">
 			<Button size="icon" variant="ghost" onClick={() => router.back()}>
 				<ArrowLeft className="size-4" />
 			</Button>
 			<p className="hidden xl:block">My Tasks</p>
-			<Button
-				onClick={() => {
-					setActiveTab("assigned");
-					router.push(`/${currentWorkspace?.url}/my-tasks/assigned`);
-				}}
-				variant={activeTab === "assigned" ? "secondary" : "ghost"}
-				size="sm"
-			>
-				Assigned
-			</Button>
-			<Button
-				onClick={() => {
-					setActiveTab("created");
-					router.push(`/${currentWorkspace?.url}/my-tasks/created`);
-				}}
-				variant={activeTab === "created" ? "secondary" : "ghost"}
-				size="sm"
-			>
-				Created
-			</Button>
+			{tabs.map((tab) => (
+				<Button
+					key={tab.id}
+					onClick={() => navigate(tab.id)}
+					variant={activeTab === tab.id ? "secondary" : "ghost"}
+					size="sm"
+				>
+					{tab.label}
+				</Button>
+			))}
 		</div>
 	);
 };
