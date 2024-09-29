1d9284286a66694dfc8f342b607a86a2519f351e
napqueenkaila <k.m.bullard14@gmail.com>
2024-09-29 19:41:33 -0400

    feat: filter tasks by time period

--- apps/web/src/components/ViewAllTasks/index.tsx
+++ apps/web/src/components/ViewAllTasks/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 import GroupColumn from "./GroupColumn";
 import RenameModal from "@/components/RenameModal";
-import { Status } from "@repo/db";
+import { Status, type Task } from "@repo/db";
 import type { ViewAllTasksProps } from "./interfaces";
 import { useViewStore } from "@/store";
+import type { CompletedTaskPeriod } from "@/store/views";
 
 const ViewAllTasks = ({
 	getFilteredStatuses,
@@ -14,14 +15,51 @@ const ViewAllTasks = ({
 	);
 	const viewOptions = view === "list" ? listViewOptions : gridViewOptions;
 
+	const filterTasksByPeriod = (tasks: Task[], period: CompletedTaskPeriod) => {
+		const now = new Date();
+		switch (period) {
+			case "Past day":
+				return tasks.filter(
+					(task) =>
+						new Date(task.updatedAt) >=
+						new Date(now.setDate(now.getDate() - 1)),
+				);
+			case "Past week":
+				return tasks.filter(
+					(task) =>
+						new Date(task.updatedAt) >=
+						new Date(now.setDate(now.getDate() - 7)),
+				);
+			case "Past month":
+				return tasks.filter(
+					(task) =>
+						new Date(task.updatedAt) >=
+						new Date(now.setMonth(now.getMonth() - 1)),
+				);
+			case "None":
+				return []; // If period is 'None', return no tasks
+			default:
+				return tasks; // Return all tasks for "All" or unrecognized period
+		}
+	};
+
 	const filteredColumns = () => {
 		const filteredStatuses = getFilteredStatuses();
+
 		return filteredStatuses.map((status) => {
 			if (status === Status.archived) return null;
-			if (status === Status.done && !viewOptions.showCompletedTasks.show)
-				return null;
-			const tasksForStatus = getTasksForStatus(status);
+
+			let tasksForStatus = getTasksForStatus(status);
+
+			if (status === Status.done) {
+				// If status is 'done', filter tasks based on updatedAt and period
+				const { period, show } = viewOptions.showCompletedTasks;
+				if (!show) return null; // Don't show completed tasks if the option is disabled
+				tasksForStatus = filterTasksByPeriod(tasksForStatus, period);
+			}
+
 			if (tasksForStatus.length === 0 && !viewOptions.showEmptyGroups)
+				// Don't display columns with no tasks unless 'showEmptyGroups' is enabled
 				return null;
 
 			return (
