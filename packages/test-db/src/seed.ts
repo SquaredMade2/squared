b25b456784fa232522d20ad7ab4b7474014d0041
THEjacob1000 <jacob@jacobdevelops.com>
2024-09-29 10:02:32 +1000

    fix: fixed type errors

--- packages/test-db/src/seed.ts
+++ packages/test-db/src/seed.ts
@@ -28,44 +28,46 @@ async function seedDB() {
 		addWorkspace(),
 	]);
 
-		const user = await addMainUser();
+	const user = await addMainUser();
 
-		for (const workspace of workspaces) {
-			await addUserToWorkspace(user, workspace);
+	for (const workspace of workspaces) {
+		await addUserToWorkspace(user, workspace);
 
-			const numTeams = faker.number.int({ min: 1, max: 2 });
+		const numTeams = faker.number.int({ min: 1, max: 2 });
 
-			for (let j = 0; j < numTeams; j++) {
-				const team = await addTeam(workspace, user);
-				const numUsers = faker.number.int({ min: 3, max: 6 });
-				const numTasks = faker.number.int({ min: 30, max: 50 });
-				const users = [user];
-				for(let i = 0; i < numUsers; i++){
-					const newUser = await addUser();
-					await addUserToWorkspace(newUser, workspace);
-					users.push(newUser);
-				}
+		for (let j = 0; j < numTeams; j++) {
+			const team = await addTeam(workspace, user);
+			const numUsers = faker.number.int({ min: 3, max: 6 });
+			const numTasks = faker.number.int({ min: 30, max: 50 });
+			const users = [user];
+			for (let i = 0; i < numUsers; i++) {
+				const newUser = await addUser();
+				await addUserToWorkspace(newUser, workspace);
+				users.push(newUser);
+			}
 
-				for (let l = 0; l < numTasks; l++) {
-					const author = users[faker.number.int({ min: 0, max: users.length-1})];
-					const task = await addTask(team, workspace, author);
-					const numComments = faker.number.int({ min: 0, max: 3 });
+			for (let l = 0; l < numTasks; l++) {
+				const author =
+					users[faker.number.int({ min: 0, max: users.length - 1 })];
+				const task = await addTask(team, workspace, author);
+				const numComments = faker.number.int({ min: 0, max: 3 });
 
-					for (let c = 0; c < numComments; c++) {
-						const author = users[faker.number.int({ min: 0, max: users.length-1})];
-						await addComment(author.id, task.id);
-					}
+				for (let c = 0; c < numComments; c++) {
+					const author =
+						users[faker.number.int({ min: 0, max: users.length - 1 })];
+					await addComment(author.id, task.id);
 				}
 			}
 		}
+	}
 	console.log("Database seeding completed");
 }
 
 async function addMainUser() {
 	const name = process.env.SEED_NAME || faker.person.fullName();
-	const email = process.env.SEED_EMAIL || faker.internet.email();;
+	const email = process.env.SEED_EMAIL || faker.internet.email();
 	const password = process.env.SEED_PASSWORD || faker.internet.password();
-	const username = name.replace(' ', '');
+	const username = name.replace(" ", "");
 
 	const hashedPassword = await hashPassword(password);
 
