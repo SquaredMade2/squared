ee499599bf059dcc2994a1042b36be79b94aec13
THEjacob1000 <jacob@jacobdevelops.com>
2024-09-29 16:14:28 +1000

    feat: added background cron job to automatically manage sprints

--- apps/api/api/generate-index.ts
+++ apps/api/api/generate-index.ts
@@ -22,10 +22,12 @@ import type { Router } from "express";
 import { toQueryHandler, toMutationHandler } from "./route";
 import type { Route } from "./route";
 ${process.env.NODE_ENV === "test" ? `import { PrismaClient } from "@repo/test-db";` : `import { PrismaClient } from "@repo/db";`}
-import { setupSwagger } from "../swagger"; // Import Swagger setup
+import { setupSwagger } from "../swagger";
+import { startSprintTransitionJob } from '@/jobs/scheduler';
 import "dotenv/config";
 
 export const prisma = new PrismaClient();
+startSprintTransitionJob();
 
 `);
 
