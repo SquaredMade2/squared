2ca5d7ab220970b99dab7f16a9f0731b63bf7e7b
napqueenkaila <k.m.bullard14@gmail.com>
2024-09-30 19:02:19 -0400

    feat: add tooltip to asc/desc button

--- apps/web/src/components/DisplaySettings/index.tsx
+++ apps/web/src/components/DisplaySettings/index.tsx
@@ -21,6 +21,12 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "../ui/tooltip";
 
 const TopNavBarDisplay = () => {
 	const {
@@ -65,6 +71,22 @@ const TopNavBarDisplay = () => {
 		setView("grid");
 	};
 
+	const tooltipContent = (): string => {
+		return ["Title", "Status", "Assignee"].includes(taskOrder.orderBy)
+			? taskOrder.orderAscending
+				? "A-Z"
+				: "Z-A"
+			: ["Priority", "Effort"].includes(taskOrder.orderBy)
+				? taskOrder.orderAscending
+					? "Ascending"
+					: "Descending"
+				: ["Due Date", "Updated", "Created"].includes(taskOrder.orderBy)
+					? taskOrder.orderAscending
+						? "Oldest first"
+						: "Newest first"
+					: "";
+	};
+
 	const formatCamelCaseString = (str: string): string => {
 		return str
 			.replace(/([A-Z])/g, " $1") // Insert space before each capital letter
@@ -89,42 +111,144 @@ const TopNavBarDisplay = () => {
 	};
 
 	return (
-		<div className="flex flex-col gap-2 items-end relative h-10 ">
-			<Popover>
-				<PopoverTrigger asChild>
-					<Button variant={"ghost"} className="gap-2">
-						<SlidersVertical className="size-4" />
-						<div className="hidden md:flex items-center gap-2">
-							Display
-							<ChevronDown className="size-4" />
-						</div>
-					</Button>
-				</PopoverTrigger>
-				<PopoverContent>
-					<div className="flex flex-col">
-						<div className=" w-full items-center justify-between flex mb-3">
-							<span className="text-foreground text-sm">Layout</span>
-							<div className="flex gap-2 items-center">
-								<Button
-									type="button"
-									onClick={handleListClick}
-									variant={view === "list" ? "outline" : "ghost"}
+		<TooltipProvider delayDuration={0}>
+			<div className="flex flex-col gap-2 items-end relative h-10 ">
+				<Popover>
+					<PopoverTrigger asChild>
+						<Button variant={"ghost"} className="gap-2">
+							<SlidersVertical className="size-4" />
+							<div className="hidden md:flex items-center gap-2">
+								Display
+								<ChevronDown className="size-4" />
+							</div>
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent>
+						<div className="flex flex-col">
+							<div className=" w-full items-center justify-between flex mb-3">
+								<span className="text-foreground text-sm">Layout</span>
+								<div className="flex gap-2 items-center">
+									<Button
+										type="button"
+										onClick={handleListClick}
+										variant={view === "list" ? "outline" : "ghost"}
+									>
+										List
+									</Button>
+									<Button
+										type="button"
+										onClick={handleGridClick}
+										variant={view === "grid" ? "outline" : "ghost"}
+									>
+										Grid
+									</Button>
+								</div>
+							</div>
+							<Separator className="my-4" />
+
+							<div className="flex items-center justify-between">
+								<span className="text-xs text-foreground">Ordering</span>
+								<DropdownMenu>
+									<DropdownMenuTrigger asChild>
+										<Button
+											variant="outline"
+											size="sm"
+											className="w-[120px] justify-between"
+										>
+											<span className="text-xs">{taskOrder.orderBy}</span>
+											<ChevronDown className="size-4" />
+										</Button>
+									</DropdownMenuTrigger>
+									<DropdownMenuContent className="w-[120px]">
+										{orderByOptions.map((option) => (
+											<DropdownMenuItem
+												key={option}
+												className="text-xs"
+												onSelect={() =>
+													setOptions({
+														taskOrder: { ...taskOrder, orderBy: option },
+													})
+												}
+											>
+												{option}
+											</DropdownMenuItem>
+										))}
+									</DropdownMenuContent>
+								</DropdownMenu>
+								<Tooltip>
+									<TooltipTrigger asChild>
+										<Button
+											variant="outline"
+											size="sm"
+											onClick={() =>
+												setOptions({
+													taskOrder: {
+														...taskOrder,
+														orderAscending: !taskOrder.orderAscending,
+													},
+												})
+											}
+										>
+											{taskOrder.orderAscending ? (
+												<ArrowDownWideNarrow className="size-4" />
+											) : (
+												<ArrowUpWideNarrow className="size-4" />
+											)}
+										</Button>
+									</TooltipTrigger>
+									<TooltipContent>{tooltipContent()}</TooltipContent>
+								</Tooltip>
+							</div>
+
+							<div>
+								<Separator className="my-2" />
+								<div>{view === "grid" ? "Grid" : "List"} options</div>
+								<div className="flex items-center justify-between w-full my-3">
+									<p className="text-foreground text-xs py-1">
+										Show Empty Groups
+									</p>
+									<Switch
+										checked={showEmptyGroups}
+										onCheckedChange={(checked) =>
+											setOptions({ showEmptyGroups: checked })
+										}
+									/>
+								</div>
+								<p className="text-foreground text-xs py-1 mb-2">
+									Display Properties
+								</p>
+								<ToggleGroup
+									type="multiple"
+									className="flex flex-wrap justify-start gap-3"
+									onValueChange={handleToggleChange}
 								>
-									List
-								</Button>
-								<Button
-									type="button"
-									onClick={handleGridClick}
-									variant={view === "grid" ? "outline" : "ghost"}
-								>
-									Grid
-								</Button>
+									{Object.keys(displayProperties).map((property) => {
+										const typedKey = property as keyof DisplayProperty;
+										const value = displayProperties[typedKey];
+										return (
+											<ToggleGroupItem
+												key={property}
+												value={property}
+												data-state={value ? "on" : "off"}
+												asChild
+											>
+												<Button
+													variant={value ? "secondary" : "ghost"}
+													size="sm"
+													className="text-xs py-0 px-2 h-6"
+												>
+													{formatCamelCaseString(property)}
+												</Button>
+											</ToggleGroupItem>
+										);
+									})}
+								</ToggleGroup>
 							</div>
 						</div>
 						<Separator className="my-4" />
 
 						<div className="flex items-center justify-between">
-							<span className="text-xs text-foreground">Ordering</span>
+							<span className="text-xs text-foreground">Completed tasks</span>
 							<DropdownMenu>
 								<DropdownMenuTrigger asChild>
 									<Button
@@ -132,122 +256,27 @@ const TopNavBarDisplay = () => {
 										size="sm"
 										className="w-[120px] justify-between"
 									>
-										<span className="text-xs">{taskOrder.orderBy}</span>
+										<span className="text-xs">{showCompletedTasks.period}</span>
 										<ChevronDown className="size-4" />
 									</Button>
 								</DropdownMenuTrigger>
 								<DropdownMenuContent className="w-[120px]">
-									{orderByOptions.map((option) => (
+									{completedPeriodOptions.map((option) => (
 										<DropdownMenuItem
 											key={option}
 											className="text-xs"
-											onSelect={() =>
-												setOptions({
-													taskOrder: { ...taskOrder, orderBy: option },
-												})
-											}
+											onSelect={() => handleDropdownSelection(option)}
 										>
 											{option}
 										</DropdownMenuItem>
 									))}
 								</DropdownMenuContent>
 							</DropdownMenu>
-							<Button
-								variant="outline"
-								size="sm"
-								onClick={() =>
-									setOptions({
-										taskOrder: {
-											...taskOrder,
-											orderAscending: !taskOrder.orderAscending,
-										},
-									})
-								}
-							>
-								{taskOrder.orderAscending ? (
-									<ArrowDownWideNarrow className="size-4" />
-								) : (
-									<ArrowUpWideNarrow className="size-4" />
-								)}
-							</Button>
 						</div>
-
-						<div>
-							<Separator className="my-2" />
-							<div>{view === "grid" ? "Grid" : "List"} options</div>
-							<div className="flex items-center justify-between w-full my-3">
-								<p className="text-foreground text-xs py-1">
-									Show Empty Groups
-								</p>
-								<Switch
-									checked={showEmptyGroups}
-									onCheckedChange={(checked) =>
-										setOptions({ showEmptyGroups: checked })
-									}
-								/>
-							</div>
-							<p className="text-foreground text-xs py-1 mb-2">
-								Display Properties
-							</p>
-							<ToggleGroup
-								type="multiple"
-								className="flex flex-wrap justify-start gap-3"
-								onValueChange={handleToggleChange}
-							>
-								{Object.keys(displayProperties).map((property) => {
-									const typedKey = property as keyof DisplayProperty;
-									const value = displayProperties[typedKey];
-									return (
-										<ToggleGroupItem
-											key={property}
-											value={property}
-											data-state={value ? "on" : "off"}
-											asChild
-										>
-											<Button
-												variant={value ? "secondary" : "ghost"}
-												size="sm"
-												className="text-xs py-0 px-2 h-6"
-											>
-												{formatCamelCaseString(property)}
-											</Button>
-										</ToggleGroupItem>
-									);
-								})}
-							</ToggleGroup>
-						</div>
-					</div>
-					<Separator className="my-4" />
-
-					<div className="flex items-center justify-between">
-						<span className="text-xs text-foreground">Completed tasks</span>
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button
-									variant="outline"
-									size="sm"
-									className="w-[120px] justify-between"
-								>
-									<span className="text-xs">{showCompletedTasks.period}</span>
-									<ChevronDown className="size-4" />
-								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent className="w-[120px]">
-								{completedPeriodOptions.map((option) => (
-									<DropdownMenuItem
-										key={option}
-										className="text-xs"
-										onSelect={() => handleDropdownSelection(option)}
-									>
-										{option}
-									</DropdownMenuItem>
-								))}
-							</DropdownMenuContent>
-						</DropdownMenu>
-					</div>
-				</PopoverContent>
-			</Popover>
-		</div>
+					</PopoverContent>
+				</Popover>
+			</div>
+		</TooltipProvider>
 	);
 };
 
