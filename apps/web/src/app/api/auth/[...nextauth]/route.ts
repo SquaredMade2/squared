2d1f15bef98fac4073f59bf0ffbb3ae2ee1b18ff
THEjacob1000 <jacob@jacobdevelops.com>
2024-09-30 10:27:11 +1000

    fix: hard coded redirect uri

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -15,7 +15,8 @@ const handler = NextAuth({
 			clientSecret: GOOGLE_CLIENT_SECRET,
 			authorization: {
 				params: {
-					redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`,
+					redirect_uri:
+						"https://app-develop.squaredmade.com/api/auth/callback/google",
 				},
 			},
 		}),
