ccfbf413f71b7d765583b5fc0388aeb2c742a0bf
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-10-02 18:33:37 -0400

    fix: refactored helpers

--- apps/api/api/integration/github/webhook/index.ts
+++ apps/api/api/integration/github/webhook/index.ts
@@ -1,4 +1,5 @@
 import type { Route } from "@/api/route";
+import type { Task } from "@repo/db";
 import { prisma } from "@/api";
 import { v4 as uuidv4 } from "uuid";
 
@@ -53,319 +54,296 @@ export function createRoute(): Route<Params> {
 			const payload: GitHubWebhookPayload = body;
 			const eventType = res.req.headers["x-github-event"];
 
-			if (eventType === "installation_repositories") {
-				try {
-					const repositoriesAdded = payload.repositories_added || [];
-					const repositoriesRemoved = payload.repositories_removed || [];
-					const githubUsername = payload.installation?.account?.login;
-
-					if (!githubUsername) {
-						console.error("GitHub username is missing from the payload");
-						res.status(400).json({ message: "Missing GitHub username" });
-						return;
-					}
-
-					// Handle repositories that have been removed
-					for (const repo of repositoriesRemoved) {
-						const repoFullName = repo.full_name;
-						const repoOwner = githubUsername;
-
-						if (!repoFullName || !repoOwner) {
-							console.error(
-								`Missing repository data for removal: ${
-									!repoFullName ? "Repository name " : ""
-								} ${!repoOwner ? "Repository owner" : ""}`,
-							);
-							continue;
-						}
-
-						// Check if the repository exists in the database, remove if exists
-						const existingRepoInfo = await prisma.githubRepoInfo.findFirst({
-							where: { repoName: repoFullName, owner: repoOwner },
-						});
-
-						if (existingRepoInfo) {
-							await prisma.githubRepoInfo.delete({
-								where: { id: existingRepoInfo.id },
-							});
-						}
-					}
-
-					// Process repositories that have been added
-					for (const repo of repositoriesAdded) {
-						const repoFullName = repo.full_name;
-						const repoOwner = githubUsername;
-
-						if (!repoFullName || !repoOwner) {
-							console.error(
-								`Missing repository data for addition: ${
-									!repoFullName ? "Repository name " : ""
-								} ${!repoOwner ? "Repository owner" : ""}`,
-							);
-							continue;
-						}
-
-						const existingRepoInfo = await prisma.githubRepoInfo.findFirst({
-							where: { repoName: repoFullName, owner: repoOwner },
-						});
-
-						if (existingRepoInfo) {
-							console.error(
-								`Repository ${repoFullName} already exists for owner ${repoOwner}`,
-							);
-							continue;
-						}
-
-						await prisma.githubRepoInfo.create({
-							data: {
-								repoName: repoFullName,
-								owner: repoOwner,
-								workspaceId: null,
-							},
-						});
-
-						console.log(`Repository ${repoFullName} created successfully`);
-					}
-
+			try {
+				if (eventType === "installation_repositories") {
+					await handleRepositoryChanges(payload);
 					res
 						.status(200)
 						.json({ message: "Repositories processed successfully" });
-				} catch (error) {
-					console.error(
-						`Error processing repository selection: ${error instanceof Error ? error.message : error}`,
-					);
-					res.status(500).json({
-						message: `Internal Server Error: ${error instanceof Error && `: ${error.message}`}`,
-					});
-				}
-
-				return;
-			}
-
-			// Handle branch creation and push events
-			const branchName = payload.ref?.split("/").pop();
-			const repoFullName = payload.repository?.full_name || "";
-			const repoOwner = payload.repository?.owner?.login || "";
-			const identifierPattern = /([A-Z]{2,}-\d+)/i;
-			const match = branchName?.match(identifierPattern);
-
-			if (match) {
-				const identifier = match[1].toUpperCase();
-
-				try {
-					const task = await prisma.task.findFirst({
-						where: { identifier: { equals: identifier, mode: "insensitive" } },
-					});
-
-					if (task) {
-						const authorName =
-							payload.pusher?.name || payload.sender?.login || "Unknown User";
-
-						const workspaceId = task.workspaceId;
-
-						const workspace = await prisma.workspace.findFirst({
-							where: { id: workspaceId },
-							select: { admins: true },
-						});
-
-						if (
-							!workspace ||
-							!workspace.admins ||
-							workspace.admins.length === 0
-						) {
-							console.error("Workspace or admins not found");
-							res
-								.status(404)
-								.json({ message: "Workspace or admins not found" });
-							return;
-						}
-
-						const workspaceOwnerId = workspace.admins[0];
-
-						const workspaceOwner = await prisma.user.findFirst({
-							where: { id: workspaceOwnerId },
-							select: { githubUsername: true },
-						});
-
-						if (!workspaceOwner || !workspaceOwner.githubUsername) {
-							console.error(
-								`Workspace owner or GitHub username not found: ${
-									!workspaceOwner ? "workspaceOwner" : ""
-								} ${!workspaceOwner?.githubUsername ? "workspaceOwner.githubUsername" : ""}`,
-							);
-							res.status(404).json({
-								message: "Workspace owner or GitHub username not found",
-							});
-							return;
-						}
-
-						const pusherUsername =
-							payload.pusher?.name || payload.sender?.login;
-
-						if (pusherUsername !== workspaceOwner.githubUsername) {
-							console.error("Pusher is not the workspace owner");
-							res.status(403).json({
-								message:
-									"You are not allowed to link this repository to the workspace",
-							});
-							return;
-						}
-
-						// Check if GithubRepoInfo exists for this repository
-						const githubRepoInfo = await prisma.githubRepoInfo.findFirst({
-							where: {
-								repoName: repoFullName,
-								owner: repoOwner,
-							},
-						});
-
-						if (!githubRepoInfo) {
-							console.error("No GithubRepoInfo found for this repository");
-							res.status(404).json({ message: "GithubRepoInfo not found" });
-							return;
-						}
-
-						// Check if this repo is already linked to another workspace
-						if (
-							githubRepoInfo.workspaceId &&
-							githubRepoInfo.workspaceId !== workspaceId
-						) {
-							console.log(
-								`Repository ${repoFullName} is already linked to another workspace`,
-							);
-							res.status(403).json({
-								message: `Repository ${repoFullName} is already linked to another workspace`,
-							});
-							return;
-						}
-
-						// Check if this workspace is already linked to another repository
-						const existingRepoForWorkspace =
-							await prisma.githubRepoInfo.findFirst({
-								where: {
-									workspaceId: workspaceId,
-									repoName: { not: repoFullName },
-								},
-							});
-
-						if (existingRepoForWorkspace) {
-							res.status(403).json({
-								message: `Workspace ${workspaceId} is already linked to another repository`,
-							});
-							return;
-						}
-
-						// Update the workspaceId in GithubRepoInfo if it's null
-						if (!githubRepoInfo.workspaceId) {
-							await prisma.githubRepoInfo.update({
-								where: { id: githubRepoInfo.id },
-								data: { workspaceId },
-							});
-							console.log(
-								`Workspace ID ${workspaceId} added to GithubRepoInfo for repository ${repoFullName}`,
-							);
-						}
-
-						// Upsert branch information linked to the task
-						const branch = await prisma.branch.upsert({
-							where: {
-								taskId: task.id,
-							},
-							update: {
-								name: branchName || "",
-								githubRepoInfoId: githubRepoInfo.id,
-							},
-							create: {
-								id: uuidv4(),
-								name: branchName || "",
-								taskId: task.id,
-								githubRepoInfoId: githubRepoInfo.id,
-							},
-						});
-
-						// Upsert task event log for the task
-						const eventLog = await prisma.taskEventLog.upsert({
-							where: {
-								taskId: task.id,
-							},
-							update: {},
-							create: {
-								id: uuidv4(),
-								taskId: task.id,
-								authorId: task.authorId,
-								authorName: authorName,
-							},
-						});
-
-						// Create a new activity related to the event log
-						const newActivity = await prisma.activity.create({
-							data: {
-								id: uuidv4(),
-								type: "TASK_EVENT",
-								eventLogId: eventLog.id,
-							},
-						});
-
-						// Create task event for the branch creation or push
-						await prisma.taskEvent.create({
-							data: {
-								type: "gitUpdated",
-								authorId: task.authorId,
-								authorName,
-								taskId: eventLog.id,
-								activityId: newActivity.id,
-							},
-						});
-
-						// Handle commits if it's a push event
-						if (eventType === "push" && payload.commits) {
-							for (const commit of payload.commits) {
-								const commitActivity = await prisma.activity.create({
-									data: {
-										id: uuidv4(),
-										type: "COMMIT",
-										eventLogId: eventLog.id,
-									},
-								});
-
-								await prisma.commit.upsert({
-									where: { id: commit.id },
-									update: {
-										message: commit.message,
-										timestamp: commit.timestamp,
-										url: commit.url,
-										authorName: payload.pusher?.name,
-									},
-									create: {
-										id: commit.id,
-										message: commit.message,
-										timestamp: commit.timestamp,
-										url: commit.url,
-										authorName: payload.pusher?.name,
-										treeId: branch.id,
-										repoName: repoFullName,
-										owner: repoOwner,
-										activityId: commitActivity.id,
-									},
-								});
-							}
-						}
-
-						res.status(200).json({ message: "Webhook processed successfully" });
-					} else {
-						console.log(`No task found for identifier ${identifier}`);
-						res.status(404).json({ message: `Task ${identifier} not found` });
-					}
-				} catch (error) {
-					console.error("Error handling webhook:", error);
-					res.status(500).json({ message: "Internal Server Error" });
 					return;
 				}
-			} else {
-				res
-					.status(204)
-					.json({ message: "No task identifier found in branch name" });
+
+				if (eventType === "create" || eventType === "push") {
+					await handleBranchAndCommitEvents(payload, eventType);
+					res.status(200).json({ message: "Webhook processed successfully" });
+					return;
+				}
+
+				res.status(204).json({ message: "No relevant event type" });
+			} catch (error) {
+				console.error(
+					`Error handling webhook: ${error instanceof Error ? error.message : error}`,
+				);
+				res.status(500).json({
+					message: `Error during webhook: ${error instanceof Error && `: ${error.message}`}`,
+				});
 			}
 		},
 	};
+}
+
+// Handle repository addition and removal
+async function handleRepositoryChanges(payload: GitHubWebhookPayload) {
+	const repositoriesAdded = payload.repositories_added || [];
+	const repositoriesRemoved = payload.repositories_removed || [];
+	const githubUsername = payload.installation?.account?.login;
+
+	if (!githubUsername) {
+		throw new Error("GitHub username is missing from the payload");
+	}
+
+	// Handle repository removal
+	for (const repo of repositoriesRemoved) {
+		if (!repo.full_name || !githubUsername) {
+			console.error(
+				`Missing repository data for removal: ${
+					!repo.full_name ? "Repository name " : ""
+				} ${!githubUsername ? "Repository owner" : ""}`,
+			);
+			continue;
+		}
+		await prisma.githubRepoInfo.deleteMany({
+			where: { repoName: repo.full_name, owner: githubUsername },
+		});
+	}
+
+	// Handle repository addition
+	for (const repo of repositoriesAdded) {
+		if (!repo.full_name || !githubUsername) {
+			console.error(
+				`Missing repository data for addition: ${
+					!repo.full_name ? "Repository name " : ""
+				} ${!githubUsername ? "Repository owner" : ""}`,
+			);
+			continue;
+		}
+		const existingRepoInfo = await prisma.githubRepoInfo.findFirst({
+			where: { repoName: repo.full_name, owner: githubUsername },
+		});
+
+		if (existingRepoInfo) {
+			console.error(
+				`Repository ${repo.full_name} already exists for owner ${githubUsername}`,
+			);
+			continue;
+		}
+
+		await prisma.githubRepoInfo.create({
+			data: {
+				repoName: repo.full_name,
+				owner: githubUsername,
+				workspaceId: null,
+			},
+		});
+	}
+}
+
+// Handle branch creation and push events
+async function handleBranchAndCommitEvents(
+	payload: GitHubWebhookPayload,
+	eventType: string,
+) {
+	const branchName = payload.ref?.split("/").pop();
+	const repoFullName = payload.repository?.full_name || "";
+	const repoOwner = payload.repository?.owner?.login || "";
+	const identifierPattern = /([A-Z]{2,}-\d+)/i;
+	const match = branchName?.match(identifierPattern);
+
+	if (!match) {
+		throw new Error("No task identifier found in branch name");
+	}
+
+	const identifier = match[1].toUpperCase();
+	const task = await prisma.task.findFirst({
+		where: { identifier: { equals: identifier, mode: "insensitive" } },
+	});
+
+	if (!task) {
+		throw new Error(`Task ${identifier} not found`);
+	}
+
+	// Check workspace ownership and return early if it fails
+	const isValidOwnership = await validateWorkspaceOwnership(
+		payload,
+		task,
+		repoFullName,
+		repoOwner,
+	);
+	if (!isValidOwnership) {
+		throw new Error(
+			"Invalid workspace ownership, pusher is not owner of workspace",
+		);
+	}
+
+	const githubRepoInfo = await prisma.githubRepoInfo.findFirst({
+		where: { repoName: repoFullName, owner: repoOwner },
+	});
+	if (!githubRepoInfo) {
+		throw new Error("No GithubRepoInfo found for this repository");
+	}
+
+	// Upsert branch information linked to the task
+	const branch = await prisma.branch.upsert({
+		where: { taskId: task.id },
+		update: { name: branchName || "", githubRepoInfoId: githubRepoInfo.id },
+		create: {
+			id: uuidv4(),
+			name: branchName || "",
+			taskId: task.id,
+			githubRepoInfoId: githubRepoInfo.id,
+		},
+	});
+
+	// Upsert task event log for the task
+	const eventLog = await prisma.taskEventLog.upsert({
+		where: { taskId: task.id },
+		update: {},
+		create: {
+			id: uuidv4(),
+			taskId: task.id,
+			authorId: task.authorId,
+			authorName:
+				payload.pusher?.name || payload.sender?.login || "Unknown User",
+		},
+	});
+
+	// Create a new activity related to the event log
+	const newActivity = await prisma.activity.create({
+		data: {
+			id: uuidv4(),
+			type: "TASK_EVENT",
+			eventLogId: eventLog.id,
+		},
+	});
+
+	// Create task event for the branch creation or push
+	await prisma.taskEvent.create({
+		data: {
+			type: "gitUpdated",
+			authorId: task.authorId,
+			authorName:
+				payload.pusher?.name || payload.sender?.login || "Unknown User",
+			taskId: eventLog.id,
+			activityId: newActivity.id,
+		},
+	});
+
+	// Handle commits if it's a push event
+	if (eventType === "push" && payload.commits) {
+		for (const commit of payload.commits) {
+			const commitActivity = await prisma.activity.create({
+				data: {
+					id: uuidv4(),
+					type: "COMMIT",
+					eventLogId: eventLog.id,
+				},
+			});
+
+			await prisma.commit.upsert({
+				where: { id: commit.id },
+				update: {
+					message: commit.message,
+					timestamp: commit.timestamp,
+					url: commit.url,
+					authorName: payload.pusher?.name,
+				},
+				create: {
+					id: commit.id,
+					message: commit.message,
+					timestamp: commit.timestamp,
+					url: commit.url,
+					authorName: payload.pusher?.name,
+					treeId: branch.id,
+					repoName: repoFullName,
+					owner: repoOwner,
+					activityId: commitActivity.id,
+				},
+			});
+		}
+	}
+}
+
+// Validate workspace ownership and restrictions
+async function validateWorkspaceOwnership(
+	payload: GitHubWebhookPayload,
+	task: Task,
+	repoFullName: string,
+	repoOwner: string,
+): Promise<boolean> {
+	const workspaceId = task.workspaceId;
+
+	const workspace = await prisma.workspace.findFirst({
+		where: { id: workspaceId },
+		select: { admins: true },
+	});
+
+	if (!workspace || workspace.admins.length === 0) {
+		throw new Error("Workspace or admins not found");
+	}
+
+	const workspaceOwnerId = workspace.admins[0];
+	const workspaceOwner = await prisma.user.findFirst({
+		where: { id: workspaceOwnerId },
+		select: { githubUsername: true },
+	});
+
+	if (!workspaceOwner || !workspaceOwner.githubUsername) {
+		console.error(
+			`Workspace owner or GitHub username not found: ${
+				!workspaceOwner ? "workspaceOwner" : ""
+			} ${!workspaceOwner?.githubUsername ? "workspaceOwner.githubUsername" : ""}`,
+		);
+		return false;
+	}
+
+	const pusherUsername = payload.pusher?.name || payload.sender?.login;
+
+	if (pusherUsername !== workspaceOwner.githubUsername) {
+		console.error("Pusher is not the workspace owner");
+		return false;
+	}
+
+	// Check if GithubRepoInfo exists for this repository
+	const githubRepoInfo = await prisma.githubRepoInfo.findFirst({
+		where: { repoName: repoFullName, owner: repoOwner },
+	});
+
+	if (!githubRepoInfo) {
+		console.error("No GithubRepoInfo found for this repository");
+		return false;
+	}
+
+	// Check if this repo is already linked to another workspace
+	if (
+		githubRepoInfo?.workspaceId &&
+		githubRepoInfo.workspaceId !== workspaceId
+	) {
+		throw new Error(
+			`Repository ${repoFullName} is already linked to another workspace`,
+		);
+	}
+	// Check if this workspace is already linked to another repository
+	const existingRepoForWorkspace = await prisma.githubRepoInfo.findFirst({
+		where: {
+			workspaceId: workspaceId,
+			repoName: { not: repoFullName },
+		},
+	});
+
+	if (existingRepoForWorkspace) {
+		console.error(
+			`Workspace ${workspaceId} is already linked to another repository`,
+		);
+		return false;
+	}
+
+	// Update the workspaceId in GithubRepoInfo if it's null
+	if (!githubRepoInfo.workspaceId) {
+		await prisma.githubRepoInfo.update({
+			where: { id: githubRepoInfo.id },
+			data: { workspaceId },
+		});
+	}
+
+	return true;
 }
