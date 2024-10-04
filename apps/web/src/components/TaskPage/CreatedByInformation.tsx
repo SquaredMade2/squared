2da588800fd5a12417af2c29b572034cc37ef379
napqueenkaila <k.m.bullard14@gmail.com>
2024-10-04 08:31:54 -0400

    fix: pr comments

--- apps/web/src/components/TaskPage/CreatedByInformation.tsx
+++ apps/web/src/components/TaskPage/CreatedByInformation.tsx
@@ -1,14 +1,16 @@
 import { formatDate } from "date-fns/format";
-import { useActivityStore, useTaskStore } from "@/store";
-import { Avatar, AvatarFallback } from "../ui/avatar";
+import { useActivityStore, useTaskStore, useUserStore } from "@/store";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { getInitials } from "@/utils/formatting";
 
 export const CreatedByInformation = () => {
 	const eventLogs = useActivityStore((state) => state.events);
 
-	const authorName = eventLogs[0]?.taskEvent?.authorName ?? "";
-
 	const currentTask = useTaskStore((state) => state.currentTask);
+	const authorName = eventLogs[0]?.taskEvent?.authorName ?? "";
+	const authorId = currentTask?.authorId;
+	const { users } = useUserStore((state) => state);
+	const foundUser = users.find((user) => user.id === authorId);
 
 	const displayDate = () => {
 		if (currentTask) {
@@ -23,8 +25,7 @@ export const CreatedByInformation = () => {
 		<div className="flex items-center px-8">
 			<div className="mr-4 text-muted-foreground">{displayDate()}</div>
 			<Avatar className="size-6 text-xxs">
-				{/* will need to access users to retrieve avatarUrl */}
-				{/* <AvatarImage src={}/> */}
+				<AvatarImage src={foundUser?.avatarUrl ?? ""} />
 				<AvatarFallback>{getInitials(authorName)}</AvatarFallback>
 			</Avatar>
 			<p className="text-foreground ml-2 mr-4">{authorName}</p>
