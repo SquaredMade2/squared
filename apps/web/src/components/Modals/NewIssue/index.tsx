b17f3fa497acb51f6cdaa2d07ffbba680d7226af
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-10-05 23:39:26 +0100

    fixed font size for description in modal

--- apps/web/src/components/Modals/NewIssue/index.tsx
+++ apps/web/src/components/Modals/NewIssue/index.tsx
@@ -181,12 +181,12 @@ export const NewIssueModal = () => {
 									name="title"
 									render={({ field }) => (
 										<FormItem>
-											<FormLabel className="text-2xl">Title</FormLabel>
+											<FormLabel className="text-xl">Title</FormLabel>
 											<FormControl>
 												<Input
 													{...field}
 													placeholder="Title"
-													className="text-lg"
+													className="text-md"
 												/>
 											</FormControl>
 										</FormItem>
@@ -197,12 +197,12 @@ export const NewIssueModal = () => {
 									name="description"
 									render={({ field }) => (
 										<FormItem>
-											<FormLabel className="text-2xl">Description</FormLabel>
+											<FormLabel className="text-xl">Description</FormLabel>
 											<FormControl>
 												<Textarea
 													{...field}
 													placeholder="Add Description"
-													className="text-lg resize-none"
+													className="text-md resize-none"
 													rows={4}
 												/>
 											</FormControl>
