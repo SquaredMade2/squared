b8f24b71599a207812fcb323803759570f89d77d
agreen254 <aarongreen1997@gmail.com>
2024-10-07 16:58:50 -0400

    make compose files and setup jest name mappers

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,8 +1,12 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
+
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
-	moduleNameMapper: {
-		"^@/(.*)$": "api/$1",
-	},
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+		prefix: "<rootDir>/",
+	}),
+	modulePaths: ["<rootDir>"],
 };
--- apps/api/src/api/index.spec.ts
+++ apps/api/src/api/index.spec.ts
@@ -1,6 +1,9 @@
 import request from "supertest";
 import { app, server, prisma } from ".";
-import { seedTestingDb, seededTestTaskId } from "@repo/test-db";
+import type { PrismaClient } from "@repo/db";
+import { randomUUID } from "node:crypto";
+
+const seededTestTaskId = randomUUID();
 
 /**
  * https://jestjs.io/docs/getting-started
@@ -62,6 +65,58 @@ describe("sample api endpoint test", () => {
  * https://jestjs.io/docs/setup-teardown
  */
 beforeAll(() => {
+	async function seedTestingDb(prisma: PrismaClient) {
+		const user = await prisma.user.create({
+			data: {
+				name: "testy mctestface",
+				username: "tester123",
+				email: "pro_tester@hotmail.com",
+				verified: true,
+				onBoarding: false,
+			},
+		});
+
+		const workspaceName = "test-workspace";
+		const workspaceCompanySize = 10;
+
+		const workspace = await prisma.workspace.create({
+			data: {
+				name: workspaceName,
+				companySize: workspaceCompanySize,
+				url: workspaceName,
+			},
+		});
+
+		const team = await prisma.team.create({
+			data: {
+				name: "testing-team",
+				identifier: "ABC",
+				workspaceId: workspace.id,
+				Users: {
+					create: {
+						userId: user.id,
+					},
+				},
+			},
+		});
+
+		const _task = await prisma.task.create({
+			data: {
+				id: seededTestTaskId,
+				authorId: user.id,
+				title: "test task",
+				description: "test description",
+				status: "inProgress",
+				priority: "low",
+				dueDate: new Date(),
+				effortEstimate: 1,
+				identifier: `${workspace.name}-01`,
+				teamId: team.id,
+				workspaceId: workspace.id,
+			},
+		});
+	}
+
 	return seedTestingDb(prisma);
 });
 
