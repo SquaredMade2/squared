83dc480edcb49a1ee91915119fd4bb65aeae82b7
THEjacob1000 <jacob@jacobdevelops.com>
2024-10-08 21:00:09 +1100

    fix: fixed bug where task search would scroll to bottom of scroll

--- apps/web/src/components/Modals/TaskSelectorModal.tsx
+++ apps/web/src/components/Modals/TaskSelectorModal.tsx
@@ -9,7 +9,7 @@ import {
 	CommandItem,
 	CommandList,
 } from "../ui/command";
-import { useTaskStore, useModalStore, useTeamStore } from "@/store";
+import { useTaskStore, useModalStore, useWorkspaceStore } from "@/store";
 import { ScrollArea } from "../ui/scroll-area";
 import { StatusIcon } from "../Icons";
 import { formatUrl } from "@/utils/formatting";
@@ -20,41 +20,38 @@ export function TaskSelector() {
 	const { showTaskSelector: open, setShowTaskSelector: setOpen } =
 		useModalStore((state) => state);
 	const { tasks, setCurrentTask } = useTaskStore((state) => state);
-	const { currentTeam } = useTeamStore((state) => state);
+	const { currentWorkspace } = useWorkspaceStore((state) => state);
 
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogContent className="p-0">
-				<div className="h-[300px] overflow-scroll">
-					<Command className="rounded-lg border shadow-md">
-						<CommandInput placeholder="Open task..." />
-						<CommandList>
-							<ScrollArea className="h-[300px]">
-								<CommandEmpty>No tasks found.</CommandEmpty>
-								<CommandGroup>
-									{tasks.map((task) => (
-										<CommandItem
-											key={task.id}
-											onSelect={() => {
-												setCurrentTask(task);
-												router.push(
-													`/${currentTeam?.name}/task/${task?.identifier}/${formatUrl(task.title)}`,
-												);
-												setOpen(false);
-											}}
-											className="grid grid-cols-[auto_1fr_11fr] gap-x-4 p-2"
-										>
-											<StatusIcon status={task.status} />
-
-											<span>{task.identifier}</span>
-											<span className="">{task.title}</span>
-										</CommandItem>
-									))}
-								</CommandGroup>
-							</ScrollArea>
-						</CommandList>
-					</Command>
-				</div>
+			<DialogContent className="p-0 max-h-[80vh] overflow-hidden">
+				<Command className="rounded-lg border shadow-md h-full">
+					<CommandInput placeholder="Open task..." />
+					<CommandList>
+						<ScrollArea className="h-[calc(80vh-53px)]">
+							<CommandEmpty>No tasks found.</CommandEmpty>
+							<CommandGroup>
+								{tasks.map((task) => (
+									<CommandItem
+										key={task.id}
+										onSelect={() => {
+											setCurrentTask(task);
+											router.push(
+												`/${currentWorkspace?.url}/task/${task?.identifier}/${formatUrl(task.title)}`,
+											);
+											setOpen(false);
+										}}
+										className="grid grid-cols-[auto_1fr_11fr] gap-x-4 p-2"
+									>
+										<StatusIcon status={task.status} />
+										<span>{task.identifier}</span>
+										<span className="">{task.title}</span>
+									</CommandItem>
+								))}
+							</CommandGroup>
+						</ScrollArea>
+					</CommandList>
+				</Command>
 			</DialogContent>
 		</Dialog>
 	);
