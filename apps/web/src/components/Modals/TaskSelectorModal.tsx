959fa305cf3fa071b478ba5b527c9ee630485407
THEjacob1000 <jacob@jacobdevelops.com>
2024-10-08 21:03:26 +1100

    refactor: refactroed taskSelectorModal to use CommandDialog

--- apps/web/src/components/Modals/TaskSelectorModal.tsx
+++ apps/web/src/components/Modals/TaskSelectorModal.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Dialog, DialogContent } from "../ui/dialog";
+import { DialogTitle } from "../ui/dialog";
 import {
-	Command,
+	CommandDialog,
 	CommandEmpty,
 	CommandGroup,
 	CommandInput,
@@ -14,6 +14,7 @@ import { ScrollArea } from "../ui/scroll-area";
 import { StatusIcon } from "../Icons";
 import { formatUrl } from "@/utils/formatting";
 import { useRouter } from "next/navigation";
+import { VisuallyHidden } from "@repo/ui/visually-hidden";
 
 export function TaskSelector() {
 	const router = useRouter();
@@ -23,36 +24,35 @@ export function TaskSelector() {
 	const { currentWorkspace } = useWorkspaceStore((state) => state);
 
 	return (
-		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogContent className="p-0 max-h-[80vh] overflow-hidden">
-				<Command className="rounded-lg border shadow-md h-full">
-					<CommandInput placeholder="Open task..." />
-					<CommandList>
-						<ScrollArea className="h-[calc(80vh-53px)]">
-							<CommandEmpty>No tasks found.</CommandEmpty>
-							<CommandGroup>
-								{tasks.map((task) => (
-									<CommandItem
-										key={task.id}
-										onSelect={() => {
-											setCurrentTask(task);
-											router.push(
-												`/${currentWorkspace?.url}/task/${task?.identifier}/${formatUrl(task.title)}`,
-											);
-											setOpen(false);
-										}}
-										className="grid grid-cols-[auto_1fr_11fr] gap-x-4 p-2"
-									>
-										<StatusIcon status={task.status} />
-										<span>{task.identifier}</span>
-										<span className="">{task.title}</span>
-									</CommandItem>
-								))}
-							</CommandGroup>
-						</ScrollArea>
-					</CommandList>
-				</Command>
-			</DialogContent>
-		</Dialog>
+		<CommandDialog open={open} onOpenChange={setOpen}>
+			<VisuallyHidden>
+				<DialogTitle>Open Task</DialogTitle>
+			</VisuallyHidden>
+			<CommandInput placeholder="Open task..." />
+			<CommandList>
+				<ScrollArea className="h-[calc(80vh-53px)]">
+					<CommandEmpty>No tasks found.</CommandEmpty>
+					<CommandGroup>
+						{tasks.map((task) => (
+							<CommandItem
+								key={task.id}
+								onSelect={() => {
+									setCurrentTask(task);
+									router.push(
+										`/${currentWorkspace?.url}/task/${task?.identifier}/${formatUrl(task.title)}`,
+									);
+									setOpen(false);
+								}}
+								className="grid grid-cols-[auto_1fr_11fr] gap-x-4 p-2"
+							>
+								<StatusIcon status={task.status} />
+								<span>{task.identifier}</span>
+								<span className="">{task.title}</span>
+							</CommandItem>
+						))}
+					</CommandGroup>
+				</ScrollArea>
+			</CommandList>
+		</CommandDialog>
 	);
 }
