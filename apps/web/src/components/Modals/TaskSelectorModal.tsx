092e54b2c0db8cd0658f52cad2637615c12c7f1e
THEjacob1000 <jacob@jacobdevelops.com>
2024-10-09 09:12:38 +1100

    fix: fixed type errors

--- apps/web/src/components/Modals/TaskSelectorModal.tsx
+++ apps/web/src/components/Modals/TaskSelectorModal.tsx
@@ -10,7 +10,6 @@ import {
 	CommandList,
 } from "../ui/command";
 import { useTaskStore, useModalStore, useWorkspaceStore } from "@/store";
-import { useTaskStore, useModalStore, useWorkspaceStore } from "@/store";
 import { ScrollArea } from "../ui/scroll-area";
 import { StatusIcon } from "../Icons";
 import { formatUrl } from "@/utils/formatting";
@@ -22,7 +21,6 @@ export function TaskSelector() {
 	const { showTaskSelector: open, setShowTaskSelector: setOpen } =
 		useModalStore((state) => state);
 	const { tasks, setCurrentTask } = useTaskStore((state) => state);
-	const { currentWorkspace } = useWorkspaceStore((state) => state);
 	const { currentWorkspace } = useWorkspaceStore((state) => state);
 
 	return (
