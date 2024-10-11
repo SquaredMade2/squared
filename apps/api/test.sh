3ba59f788fc11a3f20fa0bb60b5168bdda275ad9
agreen254 <aarongreen1997@gmail.com>
2024-10-11 17:10:06 -0500

    echo script initialization

--- apps/api/test.sh
+++ apps/api/test.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-POSTGRES_PRISMA_URL=postgresql://squared:squared@localhost:5433/squared-test?schema=public
-TEST_POSTGRES_PRISMA_URL=postgresql://squared:squared@localhost:5433/squared-test?schema=public
+POSTGRES_PRISMA_URL="postgresql://squared:squared@localhost:5433/squared-test?schema=public"
+TEST_POSTGRES_PRISMA_URL="postgresql://squared:squared@localhost:5433/squared-test?schema=public"
 
-pnpm --filter=@repo/seed docker:db;
+pnpm --filter=@repo/seed docker:db
 
 until pg_isready -h localhost -p 5433; do
     echo "waiting for db..."
     sleep 1
 done
 
-pnpm --filter=@repo/db db:push;
-pnpm jest --ci --runInBand --forceExit;
-pnpm --filter=@repo/seed docker:db:down; 
+pnpm --filter=@repo/db db:push
+echo ">> DB PUSHED"
+pnpm jest --ci --runInBand --forceExit
+pnpm --filter=@repo/seed docker:db:down
