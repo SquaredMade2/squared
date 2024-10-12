e78b245f64be927345e220212be4c385c1718f05
THEjacob1000 <jacob@jacobdevelops.com>
2024-10-12 20:23:38 +1100

    chore: removed excess whitespace

--- apps/web/src/components/Icons/index.tsx
+++ apps/web/src/components/Icons/index.tsx
@@ -35,19 +35,19 @@ export const PriorityIcon = ({ priority }: { priority: Priority }) => {
 export const StatusIcon = ({ status }: { status: Status }) => {
 	switch (status) {
 		case "backlog":
-			return <Inbox className="size-4 shrink-0 text-gray-500 " />;
+			return <Inbox className="size-4 shrink-0 text-gray-500" />;
 		case "todo":
-			return <Circle className="size-4 shrink-0 text-blue-500 " />;
+			return <Circle className="size-4 shrink-0 text-blue-500" />;
 		case "inProgress":
-			return <HourglassIcon className="size-4 shrink-0 text-yellow-500 " />;
+			return <HourglassIcon className="size-4 shrink-0 text-yellow-500" />;
 		case "inReview":
-			return <Eye className="size-4 shrink-0 text-purple-500 " />;
+			return <Eye className="size-4 shrink-0 text-purple-500" />;
 		case "done":
-			return <CheckCircle2 className="size-4 shrink-0 text-green-500 " />;
+			return <CheckCircle2 className="size-4 shrink-0 text-green-500" />;
 		case "canceled":
-			return <XCircle className="size-4 shrink-0 text-red-500 " />;
+			return <XCircle className="size-4 shrink-0 text-red-500" />;
 		case "archived":
-			return <Archive className="size-4 shrink-0 text-gray-400 " />;
+			return <Archive className="size-4 shrink-0 text-gray-400" />;
 		default:
 			return null;
 	}
