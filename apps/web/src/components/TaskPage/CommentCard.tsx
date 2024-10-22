e9afe7ccbf07a23575817995414e594f76cf49b2
yoloOnTheBattlefield <cristianfloreadev@gmail.com>
2024-10-22 00:18:57 -0700

    feat: added styled components to mdx remote

--- apps/web/src/components/TaskPage/CommentCard.tsx
+++ apps/web/src/components/TaskPage/CommentCard.tsx
@@ -130,11 +130,30 @@ const CommentCard = ({ comment }: { comment: Comment }) => {
 		}
 	};
 
+	const customStyledComponents = {
+		h2: (props: { children: React.ReactNode[] } | { children: string }) => {
+			if (typeof props.children === "string") {
+				return;
+			}
+			return <h2 className="text-2xl" {...props} />;
+		},
+		code: (props: { children: React.ReactNode[] } | { children: string }) => {
+			if (typeof props.children === "string") {
+				return;
+			}
+			return (
+				<code className="text-red-200 bg-gray-800 p-1 rounded-md" {...props} />
+			);
+		},
+	};
+
 	const setRemoteMDX = async () => {
 		try {
 			const { MDXRemote } = await import("next-mdx-remote");
 			if ("compiledSource" in commentData) {
-				setRemote(<MDXRemote {...commentData} />);
+				setRemote(
+					<MDXRemote {...commentData} components={customStyledComponents} />,
+				);
 			}
 		} catch (err) {
 			toast({
