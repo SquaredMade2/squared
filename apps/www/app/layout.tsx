52079446dcd92cdb32d4219ccdc4434deb79c0db
THEjacob1000 <jacob@jacobdevelops.com>
2024-10-22 17:54:00 +1100

    feat: initial commit

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { GeistSans } from "geist/font/sans";
-import { cn } from "@/lib/utils";
-import { ViewTransitions } from "next-view-transitions";
+import "@squared/fonts/src/styles.css";
 import { ThemeProvider } from "@/context/theme-provider";
+import { ViewTransitions } from "next-view-transitions";
 
 export const metadata: Metadata = {
 	title: "Squared",
@@ -25,10 +24,9 @@ export default function RootLayout({
 		<ViewTransitions>
 			<html lang="en">
 				<body
-					className={cn(
-						GeistSans.className,
-						"bg-white dark:bg-background-darkAccent antialiased h-full w-full",
-					)}
+					className={
+						"bg-white dark:bg-background-darkAccent antialiased h-full w-full"
+					}
 				>
 					<ThemeProvider
 						attribute="class"
