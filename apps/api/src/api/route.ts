8d77382198c09e4f44067121d2878caedd9bc9c4
NateCummens <78138781+NateCummens@users.noreply.github.com>
2024-10-28 18:31:25 -0400

    test: uncommented deploy

--- apps/api/src/api/route.ts
+++ apps/api/src/api/route.ts
@@ -6,7 +6,7 @@ export type APIResponse<Type> = {
 	message?: string;
 	variant: "default" | "destructive";
 };
-
+//this is also a change
 export type Route<P = Record<string, string>> = {
 	GET?: (
 		res: Response,
