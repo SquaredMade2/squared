e37d4796c591f53c364f6ad3b465393fc96b0e08
Chase-T-White <chase.t.white@hotmail.com>
2024-10-29 17:36:10 -0600

    docs: workspace join and filter docs

---
+++ apps/api/src/api/workspace/[workspaceId]/filter/index.docs.ts
@@ -0,0 +1,58 @@
+export default {
+	"/api/workspace/{workspaceId}/filter": {
+		get: {
+			tags: ["Workspace"],
+			summary: "Retrieve a list of filters by workspace ID",
+			description: "Find and return a list of filters by workspace ID.",
+			parameters: [
+				{
+					in: "path",
+					name: "workspaceId",
+					schema: {
+						type: "string",
+					},
+					required: true,
+					description: "The ID of the workspace associated with saved filters.",
+				},
+			],
+			responses: {
+				200: {
+					description: "A list of retrieved filters.",
+					content: {
+						"application/json": {
+							schema: {
+								type: "object",
+								properties: {
+									data: {
+										type: "array",
+										items: "#/components/schemas/SavedFilter",
+									},
+								},
+							},
+						},
+					},
+				},
+				404: {
+					description: "Filters not found",
+					content: {
+						"application/json": {
+							schema: {
+								$ref: "#/components/schemas/InvalidError",
+							},
+						},
+					},
+				},
+				500: {
+					description: "Internal server error",
+					content: {
+						"application/json": {
+							schema: {
+								$ref: "#/components/schemas/InternalServerError",
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+};
