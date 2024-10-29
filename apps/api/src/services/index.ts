2c09d9c53cb23fdb2ff3fa7e050f99ff2f7bc7a1
THEjacob1000 <jacob@jacobdevelops.com>
2024-10-29 18:42:48 +1100

    fix: fix prisma initialization error

--- apps/api/src/services/index.ts
+++ apps/api/src/services/index.ts
@@ -1,6 +1,14 @@
-import { prisma } from "../api";
+import "dotenv/config";
+import { PrismaClient } from "@squared/db";
 import { SprintService, createSprintRpcHandler } from "./sprints";
 
+const prisma = new PrismaClient({
+	datasources: {
+		db: {
+			url: process.env.TEST_POSTGRES_PRISMA_URL,
+		},
+	},
+});
 export const services = {
 	sprint: new SprintService(prisma),
 };
