52264f6fc1b419867b2f3be6fd06391a6eb1a328
THEjacob1000 <jacob@jacobdevelops.com>
2024-11-01 23:18:20 +1100

    style: put error boundary in scroll area

--- apps/web/src/components/ErrorBoundary.tsx
+++ apps/web/src/components/ErrorBoundary.tsx
@@ -1,5 +1,6 @@
 import React, { type ErrorInfo, type ReactNode } from "react";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "./ui/scroll-area";
 
 interface ErrorBoundaryProps {
 	children: ReactNode;
@@ -44,40 +45,42 @@ class ErrorBoundary extends React.Component<
 					<h1 className="text-2xl font-bold mb-4">
 						Oops! Something went wrong.
 					</h1>
-					<div className="bg-muted p-4 rounded-md mb-4 w-full max-w-3xl overflow-auto">
-						<h2 className="text-xl font-semibold mb-2">Error Details:</h2>
-						<pre className="text-sm whitespace-pre-wrap break-words">
-							{this.state.error?.toString()}
-						</pre>
-						{this.state.error && (
-							<>
-								<h3 className="text-lg font-semibold mt-4 mb-2">
-									Error Stack:
-								</h3>
-								<pre className="text-sm whitespace-pre-wrap break-words">
-									{this.state.error.stack}
-								</pre>
-							</>
-						)}
-						{this.state.errorInfo && (
-							<>
-								<h3 className="text-lg font-semibold mt-4 mb-2">
-									Component Stack:
-								</h3>
-								<pre className="text-sm whitespace-pre-wrap break-words">
-									{this.state.errorInfo.componentStack}
-								</pre>
-							</>
-						)}
-					</div>
-					<div className="flex space-x-4">
-						<Button onClick={() => window.location.reload()}>
-							Refresh Page
-						</Button>
-						<Button onClick={() => window.history.back()} variant="outline">
-							Go Back
-						</Button>
-					</div>
+					<ScrollArea className="bg-muted p-4 rounded-md mb-4 w-full max-w-3xl h-[70vh]">
+						<div>
+							<h2 className="text-xl font-semibold mb-2">Error Details:</h2>
+							<pre className="text-sm whitespace-pre-wrap break-words">
+								{this.state.error?.toString()}
+							</pre>
+							{this.state.error && (
+								<>
+									<h3 className="text-lg font-semibold mt-4 mb-2">
+										Error Stack:
+									</h3>
+									<pre className="text-sm whitespace-pre-wrap break-words">
+										{this.state.error.stack}
+									</pre>
+								</>
+							)}
+							{this.state.errorInfo && (
+								<>
+									<h3 className="text-lg font-semibold mt-4 mb-2">
+										Component Stack:
+									</h3>
+									<pre className="text-sm whitespace-pre-wrap break-words">
+										{this.state.errorInfo.componentStack}
+									</pre>
+								</>
+							)}
+						</div>
+						<div className="flex space-x-4">
+							<Button onClick={() => window.location.reload()}>
+								Refresh Page
+							</Button>
+							<Button onClick={() => window.history.back()} variant="outline">
+								Go Back
+							</Button>
+						</div>
+					</ScrollArea>
 				</div>
 			);
 		}
