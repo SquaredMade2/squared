0555839073f2820cffb085482ee0c2be0200092f
THEjacob1000 <jacob@jacobdevelops.com>
2024-11-02 18:48:46 +1100

    feat: readded error boundary

--- apps/web/src/app/clientLayout-wrapper.tsx
+++ apps/web/src/app/clientLayout-wrapper.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/Modals";
 import SearchCommand from "@/components/SearchCommand";
 import MobileMenuSheet from "@/components/MobileNav";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default function ClientLayoutWrapper({
 	children,
@@ -20,22 +21,26 @@ export default function ClientLayoutWrapper({
 }) {
 	return (
 		<SessionProvider>
-			<SquaredStoreProvider>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="system"
-					enableSystem
-					disableTransitionOnChange
-				>
-					<WorkspaceInviteModal />
-					<MobileMenuSheet />
-					<SearchCommand />
-					<WorkspaceSwitcher />
-					<TaskSelector />
-					<div className="h-full flex flex-row overflow-hidden">{children}</div>
-				</ThemeProvider>
-				<Toaster />
-			</SquaredStoreProvider>
+			<ErrorBoundary>
+				<SquaredStoreProvider>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<WorkspaceInviteModal />
+						<MobileMenuSheet />
+						<SearchCommand />
+						<WorkspaceSwitcher />
+						<TaskSelector />
+						<div className="h-full flex flex-row overflow-hidden">
+							{children}
+						</div>
+					</ThemeProvider>
+					<Toaster />
+				</SquaredStoreProvider>
+			</ErrorBoundary>
 		</SessionProvider>
 	);
 }
