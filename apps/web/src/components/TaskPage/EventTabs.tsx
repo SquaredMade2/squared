56810e201b1ab14db0c5d646d0e0a5fd8d58405d
THEjacob1000 <jacob@jacobdevelops.com>
2024-11-08 11:22:29 +1200

    feat: set up comment service integration in the frontend

--- apps/web/src/components/TaskPage/EventTabs.tsx
+++ apps/web/src/components/TaskPage/EventTabs.tsx
@@ -1,35 +1,15 @@
 "use client";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useTaskPage } from "@/hooks/useTaskPage";
-import { useCommentStore } from "@/store";
-import { useEffect } from "react";
+import { useCommentStore, useTaskStore } from "@/store";
 import { CreatedByInformation } from ".";
 import TextEditor from "../TextEditor";
-import { toast } from "../ui/use-toast";
 import CommentCard from "./CommentCard";
 
 export const EventTabs = () => {
 	const comments = useCommentStore((state) => state.comments);
-	const getComments = useCommentStore((state) => state.getAllComments);
-	const { task } = useTaskPage();
+	const currentTask = useTaskStore((state) => state.currentTask);
 
-	useEffect(() => {
-		const fetchTaskComments = async () => {
-			try {
-				if (task) {
-					await getComments(task.id);
-				}
-			} catch (err) {
-				toast({
-					title: "Error getting comments",
-					description: err instanceof Error ? err.message : "",
-					variant: "destructive",
-				});
-			}
-		};
-		fetchTaskComments();
-	}, [task]);
 	return (
 		<Tabs defaultValue="activity" className="w-full mt-8">
 			<TabsList className="grid w-1/2 grid-cols-2 bg-transparent">
@@ -45,7 +25,7 @@ export const EventTabs = () => {
 				{comments.map((comment) => {
 					return <CommentCard key={comment.id} comment={comment} />;
 				})}
-				{task && <TextEditor task={task} />}
+				{currentTask && <TextEditor task={currentTask} />}
 			</TabsContent>
 		</Tabs>
 	);
