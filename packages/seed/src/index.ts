248020f09ff8cc8b3bd4f8dbb31a9fe6923a9f63
jamhdev <jameshensley37@gmail.com>
2024-11-07 11:43:37 -0500

    refactor: added default profile pics to users in seed script

--- packages/seed/src/index.ts
+++ packages/seed/src/index.ts
@@ -90,6 +90,7 @@ async function addMainUser() {
 			password: hashedPassword,
 			verified: true,
 			onBoarding: false,
+			avatarUrl: `https://api.dicebear.com/9.x/thumbs/svg?seed=${Math.floor(Math.random() * 100000)}`,
 		},
 	});
 	return user;
@@ -112,6 +113,7 @@ async function addUser() {
 			password: hashedPassword,
 			verified: true,
 			onBoarding: false,
+			avatarUrl: `https://api.dicebear.com/9.x/thumbs/svg?seed=${Math.floor(Math.random() * 100000)}`,
 		},
 	});
 	return user;
