2ad1a5c4c3c637fe18b8dddb712c2d0b6d1070ec
THEjacob1000 <jacob@jacobdevelops.com>
2024-11-10 09:24:40 +1200

    feat: update schema and fix cascading type issues

--- apps/web/src/components/ViewAllTasks/TaskCard/index.tsx
+++ apps/web/src/components/ViewAllTasks/TaskCard/index.tsx
@@ -2,16 +2,22 @@
 import { ContextMenu, ContextMenuTrigger } from "@/components/ui/context-menu";
 import { useUserStore, useViewStore, useWorkspaceStore } from "@/store";
 import { Draggable } from "@hello-pangea/dnd";
-import type { Task } from "@squared/db";
+import type { Task, User } from "@squared/db";
+import { useEffect, useState } from "react";
 import TaskContextMenu from "./TaskContextMenu";
 import TaskGrid from "./TaskGrid";
 import TaskList from "./TaskList";
 import type { TaskCardProps } from "./interfaces";
 
 const TaskCard = ({ task, index, highlightText, location }: TaskCardProps) => {
+	const [assignee, setAssignee] = useState<User | null>(null);
 	const { view } = useViewStore((state) => state);
 	const { currentWorkspace } = useWorkspaceStore((state) => state);
 	const { users } = useUserStore((state) => state);
+	useEffect(() => {
+		const foundUser = users.find((user) => user.id === task.assigneeId);
+		setAssignee(foundUser ?? null);
+	}, []);
 
 	const taskLabels =
 		currentWorkspace?.Labels.filter((label) =>
@@ -23,14 +29,14 @@ const TaskCard = ({ task, index, highlightText, location }: TaskCardProps) => {
 			{view === "grid" && location !== "search" ? (
 				<TaskGrid
 					task={taskToRender}
-					user={users.filter((u) => u.id === task.assigneeId)[0]}
+					user={assignee}
 					taskLabels={taskLabels}
 					currentWorkspaceUrl={currentWorkspace?.url}
 				/>
 			) : (
 				<TaskList
 					task={taskToRender}
-					user={users.filter((u) => u.id === task.assigneeId)[0]}
+					user={assignee}
 					location={location}
 					highlightText={highlightText}
 					currentWorkspaceUrl={currentWorkspace?.url}
