1fc14bf75d6421d05aeef7c00c032d127336b585
THEjacob1000 <jacob@jacobdevelops.com>
2024-11-09 21:05:52 +1200

    feat: initial commit pre-join-workspace

---
+++ apps/api/src/services/auth/types.ts
@@ -0,0 +1,45 @@
+import type { User } from "@squared/db";
+
+export type Login = {
+	email: string;
+	password: string;
+};
+
+export type JoinWorkspaceParams = {
+	email: string;
+	password: string;
+	joinWorkspaceToken: string;
+};
+
+export type Register = {
+	email: string;
+	password: string;
+	name: string;
+	username: string;
+};
+
+export type OauthLogin = {
+	email: string;
+	oauthId: string;
+	name: string;
+	username: string;
+	avatarUrl: string;
+};
+
+export type UserToken = {
+	user: User;
+	token: string;
+};
+
+export interface AuthRpc {
+	login: (login: Login) => Promise<UserToken | null>;
+	googleLogin: (login: OauthLogin) => Promise<UserToken | null>;
+	register: (login: Register) => Promise<User | null>;
+	verifyUser: ({ token }: { token: string }) => Promise<User | null>;
+	resetPasswordEmail: ({ email }: { email: string }) => Promise<void>;
+	resetPassword: ({
+		token,
+		newPassword,
+	}: { token: string; newPassword: string }) => Promise<void>;
+	checkTokenValid: ({ token }: { token: string }) => Promise<void>;
+}
