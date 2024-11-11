babccb9325b4a4b165d92528d9e6c930268e912d
napqueenkaila <k.m.bullard14@gmail.com>
2024-11-11 17:49:11 -0500

    fix: undo schema changes

--- packages/db/prisma/migrations/20241111011958_make_team_identifier_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[identifier]` on the table `Team` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Team_identifier_key" ON "Team"("identifier");
