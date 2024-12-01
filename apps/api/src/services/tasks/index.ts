6e401dcb23a22ff2dc9dab13fe43e5b4ff12128a
duncanwhyte <duncan_whyte@hotmail.co.uk>
2024-12-01 16:28:25 +0000

    feat: Added template logic for task service validation of incoming params and validation of output of task services using zod. Still (Work in progress) WIP.

--- apps/api/src/services/tasks/index.ts
+++ apps/api/src/services/tasks/index.ts
@@ -127,16 +127,76 @@ export type TaskRpcSchema = typeof taskRpcSchema;
 
 export const createTaskRpcHandler = (taskService: TaskRpc) =>
 	createRpcHandler("task", taskRpcSchema, {
-		createTask: (input) => taskService.createTask(input),
-		updateTask: (input) => taskService.updateTask(input),
-		deleteTask: (input) => taskService.deleteTask(input),
-		getTask: (input) => taskService.getTask(input),
-		getTaskByIdentifier: (input) => taskService.getTaskByIdentifier(input),
-		getTeamTasks: (input) => taskService.getTeamTasks(input),
-		addActiveSprintTasks: (input) => taskService.addActiveSprintTasks(input),
-		addSprintTasks: (input) => taskService.addSprintTasks(input),
-		reorderSubtasks: (input) => taskService.reorderSubtasks(input),
-		getSubtasks: (input) => taskService.getSubtasks(input),
+		createTask: async (input) => {
+			const {input: inputSchema, output: outputSchema} = taskRpcSchema.createTask;
+			inputSchema.parse(input);
+			const newTask = await taskService.createTask(input);
+			outputSchema.parse(newTask);
+			return newTask
+		},
+		updateTask: async (input) => {
+			const {input: inputSchema, output: outputSchema} = taskRpcSchema.updateTask;
+			inputSchema.parse(input);
+			const updatedTask = await taskService.updateTask(input);
+			outputSchema.parse(updatedTask);
+			return updatedTask
+		},
+		deleteTask: async (input) => {
+			const {input: inputSchema, output: outputSchema} = taskRpcSchema.deleteTask;
+			inputSchema.parse(input);
+			const deleteTask = await taskService.deleteTask(input);
+			outputSchema.parse(deleteTask);
+			return deleteTask
+		},
+		getTask: async (input) => {
+			const {input: inputSchema, output: outputSchema} = taskRpcSchema.getTask;
+			inputSchema.parse(input);
+			const task = await taskService.getTask(input);
+			outputSchema.parse(task);
+			return task
+		},
+		getTaskByIdentifier: async (input) => {
+			const {input: inputSchema, output: outputSchema} = taskRpcSchema.getTaskByIdentifier;
+			inputSchema.parse(input);
+			const task = await taskService.getTaskByIdentifier(input);
+			outputSchema.parse(task);
+			return task
+		},
+		getTeamTasks: async (input) => {
+			const {input: inputSchema, output: outputSchema} = taskRpcSchema.getTeamTasks;
+			inputSchema.parse(input);
+			const teamTasks = await taskService.getTeamTasks(input);
+			outputSchema.parse(teamTasks);
+			return teamTasks
+		},
+		addActiveSprintTasks: async (input) => {
+			const {input: inputSchema, output: outputSchema} = taskRpcSchema.addActiveSprintTasks;
+			inputSchema.parse(input);
+			const sprintTaskNumber = await taskService.addActiveSprintTasks(input);
+			outputSchema.parse(sprintTaskNumber);
+			return sprintTaskNumber
+		},
+		addSprintTasks: async (input) => {
+			const {input: inputSchema, output: outputSchema} = taskRpcSchema.addSprintTasks;
+			inputSchema.parse(input);
+			const sprintTasks = await taskService.addSprintTasks(input);
+			outputSchema.parse(sprintTasks);
+			return sprintTasks
+		},
+		reorderSubtasks: async (input) => {
+			const {input: inputSchema, output: outputSchema} = taskRpcSchema.reorderSubtasks;
+			inputSchema.parse(input);
+			const reorderedSubtasks = await taskService.reorderSubtasks(input);
+			outputSchema.parse(reorderedSubtasks);
+			return reorderedSubtasks
+		},
+		getSubtasks: async (input) => {
+			const {input: inputSchema, output: outputSchema} = taskRpcSchema.getSubtasks;
+			inputSchema.parse(input);
+			const subTasks = await taskService.getSubtasks(input);
+			outputSchema.parse(subTasks);
+			return subTasks
+		},
 	});
 
 export { TaskService } from "./task-service";
