0ad4024491273e377164ae4bf1ac32ac48d25a56
napqueenkaila <k.m.bullard14@gmail.com>
2024-12-05 15:27:16 -0500

    fix: import errors

--- apps/web/src/components/Settings/SettingsNavBar/index.tsx
+++ apps/web/src/components/Settings/SettingsNavBar/index.tsx
@@ -12,6 +12,7 @@ import {
 import { useTheme } from "next-themes";
 import { useRouter } from "next/navigation";
 
+import { WorkspaceDropdown } from "@/components/Sidebar/WorkspaceDropdown";
 import {
 	Accordion,
 	AccordionContent,
@@ -29,10 +30,9 @@ import {
 	SidebarTrigger,
 	useSidebar,
 } from "@/components/ui/sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { useTeamStore, useWorkspaceStore } from "@/store";
 import type { Team } from "@squared/db";
-import { WorkspaceDropdown } from "../Sidebar/WorkspaceDropdown";
-import { TooltipProvider } from "../ui/tooltip";
 
 function SettingsNavbarContent() {
 	const router = useRouter();
