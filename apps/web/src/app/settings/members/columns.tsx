7007651b9b049401139dfcde177a65b53fbd7e80
Chase-T-White <chase.t.white@hotmail.com>
2024-12-06 13:52:32 -0700

    chore: quick update

--- apps/web/src/app/settings/members/columns.tsx
+++ apps/web/src/app/settings/members/columns.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import type { ColumnDef } from "@tanstack/react-table";
 import RemoveMemberButton from "./RemoveMemberButton";
