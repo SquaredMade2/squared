09aca9c40881e9c73eed9f2901f255fd0a4926a9
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2024-12-08 16:51:58 +0000

    fixed drop down padding and sizing for priority component

--- apps/web/src/components/ui/select.tsx
+++ apps/web/src/components/ui/select.tsx
@@ -118,7 +118,7 @@ const SelectItem = React.forwardRef<
 	<SelectPrimitive.Item
 		ref={ref}
 		className={cn(
-			"relative flex w-full cursor-default select-none items-center rounded-sm p-2 pr-24 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+			"relative flex w-full cursor-default select-none items-center rounded-sm p-2 pr-[calc(96px-4px)] text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
 			className,
 		)}
 		{...props}
