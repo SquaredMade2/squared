2a58131ae05ad2b820453a64cac957547424fef1
napqueenkaila <k.m.bullard14@gmail.com>
2024-12-14 15:11:40 -0500

    fix: edit Modal store

--- apps/web/src/store/modals/index.ts
+++ apps/web/src/store/modals/index.ts
@@ -6,15 +6,16 @@ export * from "./store";
 export const createModalStore = (
 	initState: ModalState = {
 		showNewTask: false,
-		showCommand: false,
+		newTaskData: {},
 		showRename: false,
 		renameData: null,
+		showLabelModal: false,
+		labelData: null,
+		showCommand: false,
 		showWorkspaceInvite: false,
 		showSwitchWorkspace: false,
 		showTaskSelector: false,
 		showLinkForm: false,
-		showLabelModal: false,
-		newTaskData: {},
 	},
 ) => {
 	return createStore<ModalStore>()((set) => ({
@@ -22,14 +23,20 @@ export const createModalStore = (
 		setShowNewTask: (input) => {
 			set({ showNewTask: input });
 		},
+		setNewTaskData: (task) => {
+			set({ newTaskData: task });
+		},
 		setShowRename: (input) => {
 			set({ showRename: input });
 		},
 		setRenameData: (input) => {
 			set({ renameData: input });
 		},
-		setNewTaskData: (task) => {
-			set({ newTaskData: task });
+		setShowLabelModal: (input) => {
+			set({ showLabelModal: input });
+		},
+		setLabelData: (input) => {
+			set({ labelData: input });
 		},
 		setShowCommand: (input) => {
 			set({ showCommand: input });
@@ -40,14 +47,11 @@ export const createModalStore = (
 		setShowSwitchWorkspace: (input) => {
 			set({ showSwitchWorkspace: input });
 		},
-		setShowLinkForm: (input) => {
-			set({ showLinkForm: input });
-		},
 		setShowTaskSelector: (input) => {
 			set({ showTaskSelector: input });
 		},
-		setShowLabelModal: (input) => {
-			set({ showLabelModal: input });
+		setShowLinkForm: (input) => {
+			set({ showLinkForm: input });
 		},
 	}));
 };
--- apps/web/src/store/modals/interfaces.ts
+++ apps/web/src/store/modals/interfaces.ts
@@ -1,29 +1,31 @@
-import type { Task } from "@squared/db";
+import type { Label, Task } from "@squared/db";
 
 export type ModalState = {
 	showNewTask: boolean;
 	newTaskData: Partial<Task>;
-	showCommand: boolean;
 	showRename: boolean;
 	renameData: Task | null;
+	showLabelModal: boolean;
+	labelData: Label | null;
+	showCommand: boolean;
 	showWorkspaceInvite: boolean;
 	showSwitchWorkspace: boolean;
-	showLinkForm: boolean;
 	showTaskSelector: boolean;
-	showLabelModal: boolean;
+	showLinkForm: boolean;
 };
 
 type ModalActions = {
 	setShowNewTask: (input: boolean) => void;
+	setNewTaskData: (task: Partial<Task>) => void;
 	setShowRename: (input: boolean) => void;
 	setRenameData: (input: Task) => void;
-	setNewTaskData: (task: Partial<Task>) => void;
+	setShowLabelModal: (input: boolean) => void;
+	setLabelData: (input: Label) => void;
 	setShowCommand: (input: boolean) => void;
 	setShowWorkspaceInvite: (input: boolean) => void;
 	setShowSwitchWorkspace: (input: boolean) => void;
 	setShowTaskSelector: (input: boolean) => void;
 	setShowLinkForm: (input: boolean) => void;
-	setShowLabelModal: (input: boolean) => void;
 };
 
 export type ModalStore = ModalState & ModalActions;
