8da57aeceb9e486a4e84d5189660a096377807ac
Chase-T-White <chase.t.white@hotmail.com>
2024-12-17 17:09:12 -0700

    feat: adding new team button to main nav for admins

---
+++ apps/web/src/components/Buttons/AddTeamButton.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const AddTeamButton = () => {
+	const router = useRouter();
+
+	return (
+		<Button
+			variant="ghost"
+			className="w-full justify-start mt-2 ml-6"
+			onClick={() => router.replace("/settings/new-team")}
+		>
+			<Plus className="mr-2 h-4 w-4" />
+			Add team
+		</Button>
+	);
+};
+
+export default AddTeamButton;
--- apps/web/src/components/Sidebar/TeamAccordion.tsx
+++ apps/web/src/components/Sidebar/TeamAccordion.tsx
@@ -6,6 +6,7 @@ import type { Team } from "@squared/db";
 import { LayoutGrid } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import AddTeamButton from "../Buttons/AddTeamButton";
 import {
 	Accordion,
 	AccordionContent,
@@ -50,35 +51,42 @@ export function TeamAccordion({ teams, currentTeam }: TeamAccordionProps) {
 	};
 
 	return (
-		<ScrollArea className="h-[calc(100vh-16rem)]">
-			<Accordion
-				type="multiple"
-				value={openItems}
-				onValueChange={handleAccordionChange}
-			>
-				{teams?.map((team: Team) => (
-					<AccordionItem value={team.id} key={team.id} className="pb-2">
-						<AccordionTrigger
-							className={cn(
-								buttonVariants({ variant: "ghost" }),
-								"justify-between",
-							)}
-						>
-							<div className="flex items-center gap-2">
-								<LayoutGrid className="text-primary h-4 w-4" />
-								<span className="text-sm font-medium">{team.name}</span>
-							</div>
-						</AccordionTrigger>
-						<AccordionContent className="pl-6 pt-1">
-							<NavBarTeams
-								teamIdentifier={team.identifier}
-								currentPage={currentPage}
-								active={currentTeam?.id === team.id}
-							/>
-						</AccordionContent>
-					</AccordionItem>
-				))}
-			</Accordion>
-		</ScrollArea>
+		<>
+			<ScrollArea className="h-[calc(100vh-16rem)]">
+				<Accordion
+					type="multiple"
+					value={openItems}
+					onValueChange={handleAccordionChange}
+				>
+					{teams?.map((team: Team) => (
+						<AccordionItem value={team.id} key={team.id} className="pb-2">
+							<AccordionTrigger
+								className={cn(
+									buttonVariants({ variant: "ghost" }),
+									"justify-between",
+								)}
+							>
+								<div className="flex items-center gap-2">
+									<LayoutGrid className="text-primary h-4 w-4" />
+									<span className="text-sm font-medium">{team.name}</span>
+								</div>
+							</AccordionTrigger>
+							<AccordionContent className="pl-6 pt-1">
+								<NavBarTeams
+									teamIdentifier={team.identifier}
+									currentPage={currentPage}
+									active={currentTeam?.id === team.id}
+								/>
+							</AccordionContent>
+						</AccordionItem>
+					))}
+				</Accordion>
+				{/* NOTE
+				Add in check to show button only if user is an Admin of the workspace once Admin privileges are implemented
+				isUserAdmin && <AddTeamButton />
+				*/}
+				<AddTeamButton />
+			</ScrollArea>
+		</>
 	);
 }
