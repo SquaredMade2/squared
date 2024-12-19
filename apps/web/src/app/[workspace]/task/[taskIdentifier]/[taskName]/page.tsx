3fa38810ee8f1cdd7b46f6fd577b6d904f4255fd
Ernest Cheung <ErnestCheung02@gmail.com>
2024-12-19 01:00:55 -0800

    revert: revert to exist task page

--- apps/web/src/app/[workspace]/task/[taskIdentifier]/[taskName]/page.tsx
+++ apps/web/src/app/[workspace]/task/[taskIdentifier]/[taskName]/page.tsx
@@ -1,63 +1,63 @@
 "use client";
-// import { NewTaskCollapsible } from "@/components/Modals";
-// import {
-// 	EventTabs,
-// 	MobileTaskSettings,
-// 	TaskBreadcrumbs,
-// 	TaskDesignationsContainer,
-// 	TaskPageForm,
-// 	TaskSidebarTopRow,
-// } from "@/components/TaskPage";
-// import { LoadingTask } from "@/components/TaskPage/LoadingTask";
-// import { ScrollArea } from "@/components/ui/scroll-area";
-// import { useToast } from "@/components/ui/use-toast";
-// import { userService } from "@/lib/services";
-// import { useUserStore } from "@/store";
-// import { TODO } from "@squared/context";
+import { NewTaskCollapsible } from "@/components/Modals";
+import {
+	EventTabs,
+	MobileTaskSettings,
+	TaskBreadcrumbs,
+	TaskDesignationsContainer,
+	TaskPageForm,
+	TaskSidebarTopRow,
+} from "@/components/TaskPage";
+import { LoadingTask } from "@/components/TaskPage/LoadingTask";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useToast } from "@/components/ui/use-toast";
+import { userService } from "@/lib/services";
+import { useUserStore } from "@/store";
+import { TODO } from "@squared/context";
 
-// import { useTaskPage } from "@/hooks/useTaskPage";
-// import { useTaskStore } from "@/store";
-// import { useEffect } from "react";
-// import Subtasks from "./Subtasks";
+import { useTaskPage } from "@/hooks/useTaskPage";
+import { useTaskStore } from "@/store";
+import { useEffect } from "react";
+import Subtasks from "./Subtasks";
 
 const TaskPage = () => {
-	// const {
-	//isLoading,
-	// error,
-	//subtasks
-	// } = useTaskPage();
-	// const { currentTask } = useTaskStore((state) => state);
-	// const { toast } = useToast();
-	// const user = useUserStore((state) => state.user);
+	const { isLoading, error, subtasks } = useTaskPage();
+	const { currentTask } = useTaskStore((state) => state);
+	const { toast } = useToast();
+	const user = useUserStore((state) => state.user);
 
-	// useEffect(() => {
-	// 	if (currentTask && user?.id) {
-	// 		userService
-	// 			.setLastViewedTask(TODO, {
-	// 				userId: user.id,
-	// 				taskId: currentTask.id,
-	// 			})
-	// 			.catch((error) => {
-	// 				toast({
-	// 					title: error,
-	// 					variant: "destructive",
-	// 				});
-	// 			});
-	// 	}
-	// }, [currentTask, user]);
+	useEffect(() => {
+		console.log("currentTask", currentTask);
+		console.log("userId", user?.id);
+		if (currentTask && user?.id) {
+			console.log("currentTask and userId exists");
+			console.log(TODO);
+			userService
+				.setLastViewedTask(TODO, {
+					userId: user.id,
+					taskId: currentTask.id,
+				})
+				.catch((error) => {
+					toast({
+						title: error,
+						variant: "destructive",
+					});
+				});
+		}
+	}, [currentTask, user]);
 
-	// useEffect(() => {
-	// 	if (error) {
-	// 		toast({
-	// 			title: error,
-	// 			variant: "destructive",
-	// 		});
-	// 	}
-	// }, [error]);
+	useEffect(() => {
+		if (error) {
+			toast({
+				title: error,
+				variant: "destructive",
+			});
+		}
+	}, [error]);
 
 	return (
 		<div className="w-full h-screen flex bg-background overflow-hidden">
-			{/* {isLoading || !currentTask ? (
+			{isLoading || !currentTask ? (
 				<LoadingTask />
 			) : (
 				<div className="w-full mdlg:w-full flex space-around scrollbar-thin-transparent overflow-auto max850:overflow-x-hidden">
@@ -86,7 +86,7 @@ const TaskPage = () => {
 						</div>
 					</div>
 				</div>
-			)} */}
+			)}
 		</div>
 	);
 };
