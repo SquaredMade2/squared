8b6bc201ad608d068c0bd523d188d61fd06bf0b1
Chase-T-White <chase.t.white@hotmail.com>
2024-12-20 15:34:29 -0700

    fix: test without root tags

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -26,25 +26,25 @@ export const metadata: Metadata = {
 		default: "Squared",
 		template: "%s | Squared",
 	},
-	description:
-		"Squared is a platform to help organize software development projects. Create new tasks, plan product goals, and setup milestones using Squared",
-	openGraph: {
-		title: "Squared",
-		description:
-			"An invitation to join Squared. Help organize software development projects. Create new tasks, plan product goals, and setup milestones using Squred!",
-		url: "https://www.squared.com",
-		siteName: "Squared",
-		images: [
-			{
-				url: "https://www.squared.com/logo.png",
-				width: 800,
-				height: 600,
-				alt: "Squared logo",
-			},
-		],
-		locale: "en_US",
-		type: "website",
-	},
+	// description:
+	// 	"Squared is a platform to help organize software development projects. Create new tasks, plan product goals, and setup milestones using Squared",
+	// openGraph: {
+	// 	title: "Squared",
+	// 	description:
+	// 		"An invitation to join Squared. Help organize software development projects. Create new tasks, plan product goals, and setup milestones using Squred!",
+	// 	url: "https://www.squared.com",
+	// 	siteName: "Squared",
+	// 	images: [
+	// 		{
+	// 			url: "https://www.squared.com/logo.png",
+	// 			width: 800,
+	// 			height: 600,
+	// 			alt: "Squared logo",
+	// 		},
+	// 	],
+	// 	locale: "en_US",
+	// 	type: "website",
+	// },
 };
 
 export default function RootLayout({
