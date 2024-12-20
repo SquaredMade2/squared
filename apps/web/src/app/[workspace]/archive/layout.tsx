0fbf9af5a27869f1481aa298212cbd39164b550b
Chase-T-White <chase.t.white@hotmail.com>
2024-12-19 17:55:11 -0700

    feat: og meta workspace

--- apps/web/src/app/[workspace]/archive/layout.tsx
+++ apps/web/src/app/[workspace]/archive/layout.tsx
@@ -5,6 +5,22 @@ export const metadata: Metadata = {
 	title: "Archive",
 	description:
 		"Access your completed and archived tasks in one place. Review past milestones, reference previous work, and keep a record of your accomplishments.",
+	openGraph: {
+		title: "workspace name",
+		description: "An invitation to join a workspace",
+		url: "https://nextjs.org",
+		siteName: "Squared",
+		images: [
+			{
+				url: "https://nextjs.org/logo.png",
+				width: 800,
+				height: 600,
+				alt: "Squared logo",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
 };
 
 export default function MyTasksLayout({
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
 		siteName: "Squared",
 		images: [
 			{
-				url: "https://nextjs.org/logo.png", // Must be an absolute URL
+				url: "https://nextjs.org/logo.png",
 				width: 800,
 				height: 600,
 				alt: "Squared logo",
