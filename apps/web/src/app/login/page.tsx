f924c36f4ef6cb6b3076ad18675eef5430009c7f
Pinak Nayak <pinak.nayak@gmail.com>
2024-12-20 17:22:20 -0800

    fix: disabled google sign in

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -148,14 +148,14 @@ function LoginForm() {
 						</Button>
 					</form>
 					<div className="relative">
-						<div className="absolute inset-0 flex items-center">
+						{/* <div className="absolute inset-0 flex items-center">
 							<Separator />
-						</div>
-						<div className="relative flex justify-center text-xs uppercase">
+						</div> */}
+						{/* <div className="relative flex justify-center text-xs uppercase">
 							<span className="bg-card px-2 text-muted-foreground">
 								Or continue with
 							</span>
-						</div>
+						</div> */}
 					</div>
 					{process.env.NODE_ENV === "production" && (
 						<Button
