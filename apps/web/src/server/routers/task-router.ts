8c3b84bca861cb2d5976cd45897d413e2a8de245
Jacob <jacob@jacobdevelops.com>
2024-12-22 15:05:16 +1100

    refactor: updated useTaskDashboard hook

--- apps/web/src/server/routers/task-router.ts
+++ apps/web/src/server/routers/task-router.ts
@@ -3,6 +3,16 @@ import { z } from "zod";
 import { router } from "../__internals/router";
 import { privateProcedure } from "../procedures";
 
+const statusEnum = z.enum([
+	"backlog",
+	"todo",
+	"inProgress",
+	"inReview",
+	"done",
+	"canceled",
+	"archived",
+]);
+
 export const taskRouter = router({
 	getTaskByIdentifier: privateProcedure
 		.input(z.object({ identifier: z.string(), workspaceId: z.string() }))
@@ -22,5 +32,35 @@ export const taskRouter = router({
 			const { taskService } = ctx;
 			const { parentId } = input;
 			return c.superjson(await taskService.getSubtasks(TODO, { parentId }));
+		}),
+	getAllTasks: privateProcedure
+		.input(
+			z.object({
+				teamId: z.string(),
+			}),
+		)
+		.query(async ({ c, ctx, input }) => {
+			const { taskService } = ctx;
+			const { teamId } = input;
+			return c.superjson(await taskService.getTeamTasks(TODO, { teamId }));
+		}),
+	updateStatus: privateProcedure
+		.input(
+			z.object({
+				taskId: z.string(),
+				status: statusEnum,
+				updaterId: z.string(),
+			}),
+		)
+		.mutation(async ({ c, ctx, input }) => {
+			const { taskService } = ctx;
+			const { taskId, status, updaterId } = input;
+			return c.superjson(
+				await taskService.updateTask(TODO, {
+					id: taskId,
+					updaterId,
+					status,
+				}),
+			);
 		}),
 });
