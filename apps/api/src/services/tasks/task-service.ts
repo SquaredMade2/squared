b2077bffa5dbfe90a820b2def02b47a12eedff5f
Chase-T-White <chase.t.white@hotmail.com>
2025-01-06 15:41:00 -0700

    fix: task deletion bug

--- apps/api/src/services/tasks/task-service.ts
+++ apps/api/src/services/tasks/task-service.ts
@@ -173,7 +173,9 @@ export class TaskService implements TaskRpc {
 		return task;
 	}
 
-	async deleteTask({ taskId }: { taskId: string }): Promise<void> {
+	async deleteTask({
+		taskId,
+	}: { taskId: string }): Promise<{ success: boolean }> {
 		this.logger.info("Deleting task by ID: %s", taskId);
 		const task = await this.db.task.delete({
 			where: { id: taskId },
@@ -181,7 +183,7 @@ export class TaskService implements TaskRpc {
 		if (!task) {
 			this.throwError("There was an issue deleting the task");
 		}
-		return;
+		return { success: true };
 	}
 
 	async getTask({ taskId }: { taskId: string }): Promise<Task> {
