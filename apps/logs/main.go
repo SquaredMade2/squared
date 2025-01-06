b2ca5732ce0fdf8b530f24fb6cf1c36b71182333
THEjacob1000 <jacob@jacobdevelops.com>
2025-01-06 11:45:35 +1100

    refactor: remove requestId

--- apps/logs/main.go
+++ apps/logs/main.go
@@ -157,6 +157,9 @@ func formatLog(log VercelLog) string {
 		message = strings.TrimSpace(message[startIndex:endIndex])
 	}
 
+	parts := strings.Split(message, "\n")
+	message = strings.Join(parts[1:], "\n")
+
 	return fmt.Sprintf("%s %s %s",
 		timestamp.Format("Jan 02 15:04:05"),
 		coloredLogLevel,
