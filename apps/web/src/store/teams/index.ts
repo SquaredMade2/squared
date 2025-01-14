f5554dc1128e26fe3e15a22b22c02bf39145bffd
Chase-T-White <chase.t.white@hotmail.com>
2025-01-14 16:34:52 -0700

    fix: sessionStorage to teamStore

--- apps/web/src/store/teams/index.ts
+++ apps/web/src/store/teams/index.ts
@@ -1,3 +1,4 @@
+import { persist } from "zustand/middleware";
 import { createStore } from "zustand/vanilla";
 import type { TeamState, TeamStore } from "./interfaces";
 export * from "./interfaces";
@@ -9,18 +10,38 @@ export const createTeamStore = (
 		team: null,
 	},
 ) => {
-	return createStore<TeamStore>()((set) => ({
-		...initState,
-		setTeam: (team) => {
-			set({ team });
-		},
-		setTeams: (teams) => set({ teams }),
-		createTeam: (team) => set((state) => ({ teams: [...state.teams, team] })),
-		updateTeam: (team) =>
-			set((state) => ({
-				teams: state.teams.map((t) => (t.id === team.id ? team : t)),
-			})),
-		deleteTeam: (team) =>
-			set((state) => ({ teams: state.teams.filter((t) => t.id === team) })),
-	}));
+	return createStore<TeamStore>()(
+		persist(
+			(set) => ({
+				...initState,
+				setTeam: (team) => {
+					set({ team });
+				},
+				setTeams: (teams) => set({ teams }),
+				createTeam: (team) =>
+					set((state) => ({ teams: [...state.teams, team] })),
+				updateTeam: (team) =>
+					set((state) => ({
+						teams: state.teams.map((t) => (t.id === team.id ? team : t)),
+					})),
+				deleteTeam: (team) =>
+					set((state) => ({ teams: state.teams.filter((t) => t.id === team) })),
+			}),
+			{
+				name: "team-store",
+				storage: {
+					getItem: (name) => {
+						const storedValue = sessionStorage.getItem(name);
+						return storedValue ? JSON.parse(storedValue) : null;
+					},
+					setItem: (name, value) => {
+						sessionStorage.setItem(name, JSON.stringify(value));
+					},
+					removeItem: (name) => {
+						sessionStorage.removeItem(name);
+					},
+				},
+			},
+		),
+	);
 };
