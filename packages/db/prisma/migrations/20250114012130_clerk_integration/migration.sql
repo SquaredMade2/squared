41a2cd0299a211ad58aec9f0aca897a168110c6b
Jacob <jacob@jacobdevelops.com>
2025-01-14 12:32:38 +1100

    feat(migration): enforce externalId uniqueness and update foreign key constraints across related tables

--- packages/db/prisma/migrations/20250114012130_clerk_integration/migration.sql
+++ packages/db/prisma/migrations/20250114012130_clerk_integration/migration.sql
@@ -1,113 +1,34 @@
--- Start a transaction
-BEGIN;
+-- Make externalId unique and not nullable
+ALTER TABLE "User" ALTER COLUMN "externalId" SET NOT NULL;
+ALTER TABLE "User" ADD CONSTRAINT "User_externalId_unique" UNIQUE ("externalId");
 
--- 1. Update all users with a temporary externalId if it's null
-UPDATE "User"
-SET "externalId" = gen_random_uuid()::text
-WHERE "externalId" IS NULL;
+-- Update foreign key references
+-- TaskEvent.authorId references User.externalId
+ALTER TABLE "TaskEvent" DROP CONSTRAINT "TaskEvent_authorId_fkey";
+ALTER TABLE "TaskEvent" ADD CONSTRAINT "TaskEvent_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- 2. Update all related tables to use externalId instead of id
+-- Comment.authorId references User.externalId
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_authorId_fkey";
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- Update TaskEvent
-UPDATE "TaskEvent" te
-SET "authorId" = u."externalId"
-FROM "User" u
-WHERE te."authorId" = u.id;
+-- Notification.userId references User.externalId
+ALTER TABLE "Notification" DROP CONSTRAINT "Notification_userId_fkey";
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- Update Comment
-UPDATE "Comment" c
-SET "authorId" = u."externalId"
-FROM "User" u
-WHERE c."authorId" = u.id;
+-- Task.authorId and assigneeId reference User.externalId
+ALTER TABLE "Task" DROP CONSTRAINT "Task_authorId_fkey";
+ALTER TABLE "Task" DROP CONSTRAINT "Task_assigneeId_fkey";
+ALTER TABLE "Task" ADD CONSTRAINT "Task_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Task" ADD CONSTRAINT "Task_assigneeId_fkey" FOREIGN KEY ("assigneeId") REFERENCES "User"("externalId") ON DELETE SET NULL ON UPDATE CASCADE;
 
--- Update Notification
-UPDATE "Notification" n
-SET "userId" = u."externalId"
-FROM "User" u
-WHERE n."userId" = u.id;
+-- UserWorkspace.userId references User.externalId
+ALTER TABLE "UserWorkspace" DROP CONSTRAINT "UserWorkspace_userId_fkey";
+ALTER TABLE "UserWorkspace" ADD CONSTRAINT "UserWorkspace_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- Update Task (authorId)
-UPDATE "Task" t
-SET "authorId" = u."externalId"
-FROM "User" u
-WHERE t."authorId" = u.id;
+-- UserTeam.userId references User.externalId
+ALTER TABLE "UserTeam" DROP CONSTRAINT "UserTeam_userId_fkey";
+ALTER TABLE "UserTeam" ADD CONSTRAINT "UserTeam_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- Update Task (assigneeId)
-UPDATE "Task" t
-SET "assigneeId" = u."externalId"
-FROM "User" u
-WHERE t."assigneeId" = u.id;
-
--- Update SavedFilter
-UPDATE "SavedFilter" sf
-SET "authorId" = u."externalId"
-FROM "User" u
-WHERE sf."authorId" = u.id;
-
--- Update UserWorkspace
-UPDATE "UserWorkspace" uw
-SET "userId" = u."externalId"
-FROM "User" u
-WHERE uw."userId" = u.id;
-
--- Update UserTeam
-UPDATE "UserTeam" ut
-SET "userId" = u."externalId"
-FROM "User" u
-WHERE ut."userId" = u.id;
-
--- Update RetrospectiveItem
-UPDATE "RetrospectiveItem" ri
-SET "authorId" = u."externalId"
-FROM "User" u
-WHERE ri."authorId" = u.id;
-
--- 3. Drop foreign key constraints
-ALTER TABLE "Comment" DROP CONSTRAINT IF EXISTS "Comment_authorId_fkey";
-ALTER TABLE "Notification" DROP CONSTRAINT IF EXISTS "Notification_userId_fkey";
-ALTER TABLE "RetrospectiveItem" DROP CONSTRAINT IF EXISTS "RetrospectiveItem_authorId_fkey";
-ALTER TABLE "SavedFilter" DROP CONSTRAINT IF EXISTS "SavedFilter_authorId_fkey";
-ALTER TABLE "Task" DROP CONSTRAINT IF EXISTS "Task_assigneeId_fkey";
-ALTER TABLE "Task" DROP CONSTRAINT IF EXISTS "Task_authorId_fkey";
-ALTER TABLE "TaskEvent" DROP CONSTRAINT IF EXISTS "TaskEvent_authorId_fkey";
-ALTER TABLE "UserTeam" DROP CONSTRAINT IF EXISTS "UserTeam_userId_fkey";
-ALTER TABLE "UserWorkspace" DROP CONSTRAINT IF EXISTS "UserWorkspace_userId_fkey";
-
--- 4. Alter User table
-ALTER TABLE "User" 
-  DROP COLUMN IF EXISTS "githubId",
-  DROP COLUMN IF EXISTS "googleId",
-  DROP COLUMN IF EXISTS "password",
-  DROP COLUMN IF EXISTS "verified",
-  ALTER COLUMN "externalId" SET NOT NULL;
-
--- 5. Add new foreign key constraints
-ALTER TABLE "TaskEvent" ADD CONSTRAINT "TaskEvent_authorId_fkey" 
-  FOREIGN KEY ("authorId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE "Comment" ADD CONSTRAINT "Comment_authorId_fkey" 
-  FOREIGN KEY ("authorId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_fkey" 
-  FOREIGN KEY ("userId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE "Task" ADD CONSTRAINT "Task_authorId_fkey" 
-  FOREIGN KEY ("authorId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE "Task" ADD CONSTRAINT "Task_assigneeId_fkey" 
-  FOREIGN KEY ("assigneeId") REFERENCES "User"("externalId") ON DELETE SET NULL ON UPDATE CASCADE;
-
-ALTER TABLE "SavedFilter" ADD CONSTRAINT "SavedFilter_authorId_fkey" 
-  FOREIGN KEY ("authorId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE "UserWorkspace" ADD CONSTRAINT "UserWorkspace_userId_fkey" 
-  FOREIGN KEY ("userId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE "UserTeam" ADD CONSTRAINT "UserTeam_userId_fkey" 
-  FOREIGN KEY ("userId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE "RetrospectiveItem" ADD CONSTRAINT "RetrospectiveItem_authorId_fkey" 
-  FOREIGN KEY ("authorId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- Commit the transaction
-COMMIT;
+-- RetrospectiveItem.authorId references User.externalId
+ALTER TABLE "RetrospectiveItem" DROP CONSTRAINT "RetrospectiveItem_authorId_fkey";
+ALTER TABLE "RetrospectiveItem" ADD CONSTRAINT "RetrospectiveItem_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("externalId") ON DELETE CASCADE ON UPDATE CASCADE;
