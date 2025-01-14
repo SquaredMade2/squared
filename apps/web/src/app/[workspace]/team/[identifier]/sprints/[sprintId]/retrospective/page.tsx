26b8e8a932f8ecb50504197e91e383e93bc20835
napqueenkaila <k.m.bullard14@gmail.com>
2025-01-14 10:15:04 -0500

    fix: type error

--- apps/web/src/app/[workspace]/team/[identifier]/sprints/[sprintId]/retrospective/page.tsx
+++ apps/web/src/app/[workspace]/team/[identifier]/sprints/[sprintId]/retrospective/page.tsx
@@ -159,7 +159,7 @@ export default function SprintRetrospectivePage() {
 			const itemId = result.draggableId;
 
 			try {
-				const response = await sprintService.updateRetrospectiveItem(TODO, {
+				const response = await sprintService.updateRetrospectiveType(TODO, {
 					sprintId,
 					retrospectiveItemId: itemId,
 					type: destinationType,
