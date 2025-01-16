d81498dfe763a87d0b36fcdfc49138068106c284
THEjacob1000 <jacob@jacobdevelops.com>
2025-01-16 16:08:29 +1100

    refactor: simplify navigation logic in SettingsNavBar component

--- apps/web/src/components/Settings/SettingsNavBar/index.tsx
+++ apps/web/src/components/Settings/SettingsNavBar/index.tsx
@@ -41,7 +41,7 @@ function SettingsNavbarContent() {
 	const { workspace } = useWorkspaceStore((state) => state);
 
 	const navigateTo = (targetRoute: string) => {
-		router.replace(`/settings/${targetRoute}`);
+		router.replace(`/${workspace?.url}/settings/${targetRoute}`);
 	};
 	const handleTeamClick = (team: Team, path?: string) => {
 		setTeam(team);
@@ -78,21 +78,21 @@ function SettingsNavbarContent() {
 									<Button
 										variant="ghost"
 										className="w-full justify-start"
-										onClick={() => navigateTo(`/${workspace?.url}/settings`)}
+										onClick={() => navigateTo("")}
 									>
 										General
 									</Button>
 									<Button
 										variant="ghost"
 										className="w-full justify-start"
-										onClick={() => navigateTo(`/${workspace?.url}/members`)}
+										onClick={() => navigateTo("members")}
 									>
 										Members
 									</Button>
 									<Button
 										variant="ghost"
 										className="w-full justify-start"
-										onClick={() => navigateTo(`${workspace?.url}/integrations`)}
+										onClick={() => navigateTo("integrations")}
 									>
 										Integrations
 									</Button>
@@ -110,7 +110,7 @@ function SettingsNavbarContent() {
 									<Button
 										variant="ghost"
 										className="w-full justify-start"
-										onClick={() => navigateTo("account/profile")}
+										onClick={() => navigateTo("profile")}
 									>
 										Profile
 									</Button>
