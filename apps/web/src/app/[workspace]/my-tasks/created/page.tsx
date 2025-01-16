a1e13192034f104e23644c45a61c30b81a58d243
THEjacob1000 <jacob@jacobdevelops.com>
2025-01-16 11:31:02 +1100

    refactor: update user retrieval to use Clerk's useUser hook in MyCreatedTasksPage

--- apps/web/src/app/[workspace]/my-tasks/created/page.tsx
+++ apps/web/src/app/[workspace]/my-tasks/created/page.tsx
@@ -4,17 +4,18 @@ import ViewAllTasks from "@/components/ViewAllTasks";
 import { TaskPageLayout } from "@/components/ViewAllTasks/PageLayout";
 import { useGroups } from "@/hooks/useGroups";
 import { useTaskDashboard } from "@/hooks/useTaskDashboard";
-import { useFilterStore, useUserStore } from "@/store";
+import { useFilterStore } from "@/store";
+import { useUser } from "@clerk/nextjs";
 
 export default function MyCreatedTasksPage() {
-	const user = useUserStore((state) => state.user);
+	const { user } = useUser();
 	const { filterTasks } = useFilterStore((state) => state);
 
 	const { loading, authorized, workspace, teamIdentifier, handleDragEnd } =
 		useTaskDashboard();
 
 	const { getGroupedColumns } = useGroups((tasks) =>
-		filterTasks(tasks.filter((t) => t.authorId === user?.externalId)),
+		filterTasks(tasks.filter((t) => t.authorId === user?.id)),
 	);
 
 	if (!workspace) return null;
