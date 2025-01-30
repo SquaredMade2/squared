7153a29c5d76b5bc3cae5f02b29b6037c081b8cd
BIBruington <psychological_chemist@hotmail.com>
2025-01-29 19:53:47 -0500

    imports

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -1,6 +1,6 @@
-// import rehypePrism from "@mapbox/rehype-prism";
+import rehypePrism from "@mapbox/rehype-prism";
 import nextMDX from "@next/mdx";
-// import remarkGfm from "remark-gfm";
+import remarkGfm from "remark-gfm";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
