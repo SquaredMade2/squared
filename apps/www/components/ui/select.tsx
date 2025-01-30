96ed93c9c25ba731be444468b6d6e6e3dc606264
BIBruington <psychological_chemist@hotmail.com>
2025-01-30 00:31:44 -0500

    update squaredmade path

--- apps/www/components/ui/select.tsx
+++ apps/www/components/ui/select.tsx
@@ -3,7 +3,6 @@
 import * as SelectPrimitive from "@squaredmade/ui/select";
 import { Check, ChevronDown, ChevronUp } from "lucide-react";
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
 const Select = SelectPrimitive.Root;
