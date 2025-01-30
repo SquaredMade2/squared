77f59875533f887f8160e75e9523d8f108921047
THEjacob1000 <jacob@jacobdevelops.com>
2025-01-30 12:38:54 +1100

    fix: update grouping in workspace query to include workspacesTable.id

--- apps/api/src/services/workspaces/workspace-service.ts
+++ apps/api/src/services/workspaces/workspace-service.ts
@@ -169,7 +169,7 @@ export class WorkspaceService implements WorkspaceRpc {
 				eq(userWorkspacesTable.workspaceId, workspacesTable.id),
 			)
 			.where(eq(userWorkspacesTable.userId, userId))
-			.groupBy(userWorkspacesTable.workspaceId);
+			.groupBy(userWorkspacesTable.workspaceId, workspacesTable.id);
 
 		return workspaces.map((workspace) => workspace.Workspace);
 	}
