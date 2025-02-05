b5eb5b82c530bdf7b80148e17e39fd7039aff358
Bret Miller <bret.miller2015@gmail.com>
2025-02-05 08:55:34 -0600

    fix(web): Corrected visual bugs for adding a new task page

--- apps/web/src/app/[workspace]/(main)/task/[taskIdentifier]/[taskName]/page.tsx
+++ apps/web/src/app/[workspace]/(main)/task/[taskIdentifier]/[taskName]/page.tsx
@@ -57,7 +57,7 @@ const TaskPage = () => {
 				<LoadingTask />
 			) : (
 				<div className="space-around scrollbar-thin-transparent flex w-full overflow-auto max850:overflow-x-hidden mdlg:w-full">
-					<div className="h-full w-full p-1 md:pb-5 xl:px-10 ">
+					<div className="h-full w-full p-1 md:pb-5 xl:px-10">
 						<div className="relative flex w-full flex-col">
 							<div className="z-0 w-full snap-start overflow-x-hidden">
 								<div className="mb-4 flex w-full items-center gap-4 border-border border-b py-4">
@@ -67,7 +67,7 @@ const TaskPage = () => {
 							<MobileTaskSettings />
 							<div className="relative flex w-full">
 								<ScrollArea className="h-[calc(100vh-5rem)] w-full">
-									<div className="mr-1 max850:mr-1 md:mr-5 xl:mr-10">
+									<div className="mr-1 px-1 max850:mr-1 md:mr-5 xl:mr-10">
 										<TaskPageForm />
 										{currentTaskBlockedBy.length > 0 && <BlockedByTasks />}
 										{subtasks.length > 0 && <Subtasks />}
