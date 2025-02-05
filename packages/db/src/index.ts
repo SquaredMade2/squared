324120ca1089cfd3a75838a727e467ec2fe85c25
BIBruington <psychological_chemist@hotmail.com>
2025-02-04 19:48:01 -0500

    temp

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,8 +1,9 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
+import { Pool, neonConfig, neon } from "@neondatabase/serverless";
 import type { ExtractTablesWithRelations } from "drizzle-orm";
 import { type NeonQueryResultHKT, drizzle } from "drizzle-orm/neon-serverless";
 import type { PgTransaction } from "drizzle-orm/pg-core";
 import ws from "ws";
+import { drizzle as driz } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 export * from "drizzle-orm";
 export * from "./schema";
@@ -17,14 +18,20 @@ declare global {
 	var cachedDb: DBClient;
 }
 
-export const createDb = ({ databaseUrl }: { databaseUrl?: string }) => {
+export const createRemoteDb = ({databaseUrl}: {databaseUrl: string}) => {
+	console.log('DATABASE URL: ', databaseUrl)
+	const sql = neon(databaseUrl);
+	return driz({client: sql, schema});
+}
+
+export const createDb = ({ databaseUrl, isLocal }: { databaseUrl?: string, isLocal?: boolean }) => {
 	// Function to create the database connection
 	const config = {
 		databaseUrl:
 			databaseUrl ||
 			process.env.DATABASE_URL ||
 			"postgres://squared:squared@localhost:5432/squared-test?sslmode=disable",
-		localDb: process.env.LOCAL_DB || false,
+		localDb: isLocal !== undefined ? isLocal : ( process.env.LOCAL_DB || false),
 		nodeEnv: process.env.NODE_ENV || "test",
 	};
 
@@ -40,7 +47,6 @@ export const createDb = ({ databaseUrl }: { databaseUrl?: string }) => {
 		parsedDatabaseURL.host = "db.localtest.me"; // Magic string here 🤷
 		config.databaseUrl = parsedDatabaseURL.toString();
 	}
-
 	const pool = new Pool({ connectionString: config.databaseUrl });
 	return drizzle({ client: pool, schema });
 };
