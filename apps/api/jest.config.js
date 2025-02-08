b40f56fe17c27d60f48f96d2f3b1da3d3c284a28
Jacob <jacob@jacobdevelops.com>
2025-02-08 14:29:10 +1100

    refactor: update jest configuration to support ESM and improve module resolution

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,13 +1,21 @@
-module.exports = {
-	preset: "ts-jest",
+/** @type {import('jest').Config} */
+export default {
+	preset: "ts-jest/presets/default-esm",
 	testEnvironment: "node",
 	roots: ["<rootDir>/src"],
 	testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
 	moduleNameMapper: {
 		"^@/(.*)": "<rootDir>/src/$1",
+		"^(\\.{1,2}/.*)\\.js$": "$1",
 	},
+	extensionsToTreatAsEsm: [".ts"],
 	transform: {
-		"^.+\\.ts$": "ts-jest",
+		"^.+\\.tsx?$": [
+			"ts-jest",
+			{
+				useESM: true,
+			},
+		],
 	},
 	setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
