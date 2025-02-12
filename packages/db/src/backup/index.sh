5ed8e425d929d9912b8b889969ddcce60f97f474
BIBruington <psychological_chemist@hotmail.com>
2025-02-12 17:39:45 -0500

    feat: Update backup workflows to run monthly and adjust AWS region for staging

--- packages/db/src/backup/index.sh
+++ packages/db/src/backup/index.sh
@@ -1,9 +1,9 @@
 export $(grep -v '^#' .env | xargs)
 
-BACKUP_FILE_NAME="$(date +%Y-%m-%d | tr -d '-')"
+BACKUP_FILE_NAME="$(date +%Y%m%d | tr -d '-')"
 
 # Remove `pgbouncer=true` from the database URL
-STAGING_DB_URL=$(echo $REMOTE_DATABASE_URL | sed "s/[?&]pgbouncer=true//")
+STAGING_DB_URL=$(echo $STAGING_READONLY_STRING | sed "s/[?&]pgbouncer=true//")
 PRODUCTION_DB_URL=$(echo $PRODUCTION_READONLY_STRING | sed "s/[?&]pgbouncer=true//")
 
 
@@ -14,7 +14,7 @@ if ! command -v aws &> /dev/null; then
 fi
 
 # Run pg_dump to create the backup
-echo "Using database URL: $REMOTE_DATABASE_URL"
-pg_dump --dbname="$STAGING_DB_URL" | aws s3 cp - s3://squared-staging-backup/$BACKUP_FILE_NAME.bak --region us-east-1
+echo "Using database URL: $STAGING_READONLY_STRING"
+pg_dump --dbname="$STAGING_DB_URL" | aws s3 cp - s3://squared-staging-backup/$BACKUP_FILE_NAME.bak --region ap-southeast-2
 echo "Using database URL: $PRODUCTION_READONLY_STRING"
 pg_dump --dbname="$PRODUCTION_DB_URL" | aws s3 cp - s3://squared-prod-backup/$BACKUP_FILE_NAME.bak --region us-east-1
