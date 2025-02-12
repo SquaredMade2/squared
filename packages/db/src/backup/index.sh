f9f9c4cd2b6afe7a428d0b84d23183fb7041a894
BIBruington <psychological_chemist@hotmail.com>
2025-02-11 21:09:06 -0500

    feat: Update backup workflows to run every 7 days and enhance backup script for staging and production databases

--- packages/db/src/backup/index.sh
+++ packages/db/src/backup/index.sh
@@ -3,18 +3,18 @@ export $(grep -v '^#' .env | xargs)
 BACKUP_FILE_NAME="$(date +%Y-%m-%d | tr -d '-')"
 
 # Remove `pgbouncer=true` from the database URL
-CLEAN_DB_URL=$(echo $REMOTE_DATABASE_URL | sed "s/[?&]pgbouncer=true//")
+STAGING_DB_URL=$(echo $REMOTE_DATABASE_URL | sed "s/[?&]pgbouncer=true//")
+PRODUCTION_DB_URL=$(echo $PRODUCTION_READONLY_STRING | sed "s/[?&]pgbouncer=true//")
 
 
-echo "Using database URL: $REMOTE_DATABASE_URL"
-
-# Run pg_dump to create the backup
-
-pg_dump --dbname="$CLEAN_DB_URL" | aws s3 cp - s3://squared-staging-backup/$BACKUP_FILE_NAME.bak --region us-east-1
 
 if ! command -v aws &> /dev/null; then
   echo "❌ AWS CLI not installed. Install it and configure credentials."
   exit 1
 fi
 
-echo "🗑️ Emptying S3 bucket before upload..."
+# Run pg_dump to create the backup
+echo "Using database URL: $REMOTE_DATABASE_URL"
+pg_dump --dbname="$STAGING_DB_URL" | aws s3 cp - s3://squared-staging-backup/$BACKUP_FILE_NAME.bak --region us-east-1
+echo "Using database URL: $PRODUCTION_READONLY_STRING"
+pg_dump --dbname="$PRODUCTION_DB_URL" | aws s3 cp - s3://squared-prod-backup/$BACKUP_FILE_NAME.bak --region us-east-1
