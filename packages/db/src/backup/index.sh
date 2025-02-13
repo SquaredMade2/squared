765415c31c104f1818edbeb68bafda7413521351
BIBruington <psychological_chemist@hotmail.com>
2025-02-13 12:07:14 -0500

    feat: Refactor backup scripts to support staging and production environments with separate commands

--- packages/db/src/backup/index.sh
+++ packages/db/src/backup/index.sh
@@ -1,10 +1,12 @@
-export $(grep -v '^#' .env | xargs)
+# export $(grep -v '^#' .env | xargs)
 
 BACKUP_FILE_NAME="$(date +%Y%m%d | tr -d '-')"
 
+DATABASE_URL=$1
+AWS_BUCKET_NAME=$2
+
 # Remove `pgbouncer=true` from the database URL
-STAGING_DB_URL=$(echo $STAGING_READONLY_STRING | sed "s/[?&]pgbouncer=true//")
-PRODUCTION_DB_URL=$(echo $PRODUCTION_READONLY_STRING | sed "s/[?&]pgbouncer=true//")
+CLEAN_DB_URL=$(echo $DATABASE_URL | sed "s/[?&]pgbouncer=true//")
 
 
 
@@ -14,7 +16,6 @@ if ! command -v aws &> /dev/null; then
 fi
 
 # Run pg_dump to create the backup
-echo "Using database URL: $STAGING_READONLY_STRING"
-pg_dump --dbname="$STAGING_DB_URL" | aws s3 cp - s3://squared-staging-backup/$BACKUP_FILE_NAME.bak --region us-east-1
-echo "Using database URL: $PRODUCTION_READONLY_STRING"
-pg_dump --dbname="$PRODUCTION_DB_URL" | aws s3 cp - s3://squared-prod-backup/$BACKUP_FILE_NAME.bak --region us-east-1
+echo "Using database URL: $DATABASE_URL"
+echo "Using AWS bucket: $AWS_BUCKET_NAME"
+pg_dump --dbname="$CLEAN_DB_URL" | aws s3 cp - s3://$AWS_BUCKET_NAME/$BACKUP_FILE_NAME.bak --region us-east-1
