569083ede19ec6fbea67cad585444ff830176ddd
BIBruington <psychological_chemist@hotmail.com>
2025-02-13 12:13:25 -0500

    feat: Update backup scripts to accept AWS region as a parameter for staging and production backups

--- packages/db/src/backup/index.sh
+++ packages/db/src/backup/index.sh
@@ -4,6 +4,7 @@ BACKUP_FILE_NAME="$(date +%Y%m%d | tr -d '-')"
 
 DATABASE_URL=$1
 AWS_BUCKET_NAME=$2
+AWS_REGION=$3
 
 # Remove `pgbouncer=true` from the database URL
 CLEAN_DB_URL=$(echo $DATABASE_URL | sed "s/[?&]pgbouncer=true//")
@@ -18,4 +19,4 @@ fi
 # Run pg_dump to create the backup
 echo "Using database URL: $DATABASE_URL"
 echo "Using AWS bucket: $AWS_BUCKET_NAME"
-pg_dump --dbname="$CLEAN_DB_URL" | aws s3 cp - s3://$AWS_BUCKET_NAME/$BACKUP_FILE_NAME.bak --region us-east-1
+pg_dump --dbname="$CLEAN_DB_URL" | aws s3 cp - s3://$AWS_BUCKET_NAME/$BACKUP_FILE_NAME.bak --region $AWS_REGION
