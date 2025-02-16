d105fd0c93e50ca58d00d275e838115b41eab947
duncanwhyte <duncan_whyte@hotmail.co.uk>
2025-02-16 15:30:56 +0000

    feat: built icons

---
+++ packages/icons/components/Check.tsx
@@ -0,0 +1,41 @@
+import type { FC } from "react";
+
+interface CheckProps {
+	className?: string;
+	size?: number;
+	color?: string;
+	strokeWidth?: number;
+	absoluteStrokeWidth?: boolean;
+}
+
+export const Check: FC<CheckProps> = ({
+	className = "",
+	size = 24,
+	color = "currentColor",
+	strokeWidth = 2,
+	absoluteStrokeWidth = false,
+}) => {
+	const scale = size / 24;
+	const scaledStrokeWidth = absoluteStrokeWidth
+		? strokeWidth
+		: strokeWidth / scale;
+
+	return (
+		<svg
+			className={className}
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			stroke={color}
+			strokeWidth={scaledStrokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<title>Check</title>
+
+			<path d="M20 6 9 17l-5-5" />
+		</svg>
+	);
+};
