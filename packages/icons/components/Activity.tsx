d242e3bef5632aaa74a2ab3ef354d48dc0d3cac5
duncanwhyte <duncan_whyte@hotmail.co.uk>
2025-02-16 15:25:33 +0000

    fix: removed gitignore file as its not needed as we use our builder

---
+++ packages/icons/components/Activity.tsx
@@ -0,0 +1,41 @@
+import type { FC } from "react";
+
+interface ActivityProps {
+	className?: string;
+	size?: number;
+	color?: string;
+	strokeWidth?: number;
+	absoluteStrokeWidth?: boolean;
+}
+
+export const Activity: FC<ActivityProps> = ({
+	className = "",
+	size = 24,
+	color = "currentColor",
+	strokeWidth = 2,
+	absoluteStrokeWidth = false,
+}) => {
+	const scale = size / 24;
+	const scaledStrokeWidth = absoluteStrokeWidth
+		? strokeWidth
+		: strokeWidth / scale;
+
+	return (
+		<svg
+			className={className}
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			stroke={color}
+			strokeWidth={scaledStrokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<title>Activity</title>
+
+			<path d="M22 12h-2.48a2 2 0 0 0-1.93 1.46l-2.35 8.36a.25.25 0 0 1-.48 0L9.24 2.18a.25.25 0 0 0-.48 0l-2.35 8.36A2 2 0 0 1 4.49 12H2" />
+		</svg>
+	);
+};
---
+++ packages/icons/components/ArrowDownWideNarrow.tsx
@@ -0,0 +1,45 @@
+import type { FC } from "react";
+
+interface ArrowDownWideNarrowProps {
+	className?: string;
+	size?: number;
+	color?: string;
+	strokeWidth?: number;
+	absoluteStrokeWidth?: boolean;
+}
+
+export const ArrowDownWideNarrow: FC<ArrowDownWideNarrowProps> = ({
+	className = "",
+	size = 24,
+	color = "currentColor",
+	strokeWidth = 2,
+	absoluteStrokeWidth = false,
+}) => {
+	const scale = size / 24;
+	const scaledStrokeWidth = absoluteStrokeWidth
+		? strokeWidth
+		: strokeWidth / scale;
+
+	return (
+		<svg
+			className={className}
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			stroke={color}
+			strokeWidth={scaledStrokeWidth}
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<title>ArrowDownWideNarrow</title>
+
+			<path d="m3 16 4 4 4-4" />
+			<path d="M7 20V4" />
+			<path d="M11 4h10" />
+			<path d="M11 8h7" />
+			<path d="M11 12h4" />
+		</svg>
+	);
+};
