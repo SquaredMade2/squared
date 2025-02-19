245bb92516706ef187dce7c750703a407ed2b97d
THEjacob1000 <jacob@jacobdevelops.com>
2025-02-20 09:14:11 +1100

    refactor(formatting): remove unused formatName function

--- apps/web/src/utils/formatting.ts
+++ apps/web/src/utils/formatting.ts
@@ -111,11 +111,6 @@ export const formatPriority = (priority: Priority) => {
 	}
 };
 
-export const formatName = (user?: PublicUserData) => {
-	if (!user) return "Unknown User";
-	return user.firstName ? `${user.firstName} ${user.lastName}` : "Unknown User";
-};
-
 // TODO: Implement formatting link
 // export const handleFormatLink = (url: string) => {
 // // if is in url link format [nameOfLink]LinkUrl
