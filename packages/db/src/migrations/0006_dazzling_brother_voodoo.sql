e37522ccbd62f6f84fad12959a5d6586f336aa2e
Serhii <serhiiderkach1995@gmail.com>
2025-02-20 17:36:39 -0600

    chore: removed my migration

--- packages/db/src/migrations/0006_dazzling_brother_voodoo.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "DeletedUsers" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"deleted_at" timestamp DEFAULT now() NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE "User" ADD COLUMN "deleted" boolean DEFAULT false;--> statement-breakpoint
-ALTER TABLE "DeletedUsers" ADD CONSTRAINT "DeletedUsers_id_User_id_fk" FOREIGN KEY ("id") REFERENCES "public"."User"("id") ON DELETE cascade ON UPDATE no action;
