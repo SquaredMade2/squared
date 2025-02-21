79dcd05385e788139d3f20f59f8f31be8a0e2019
Serhii <serhiiderkach1995@gmail.com>
2025-02-20 20:03:28 -0600

    chore: added mark-as-deleted service

--- apps/api/src/services/users/user-service.ts
+++ apps/api/src/services/users/user-service.ts
@@ -3,6 +3,7 @@ import {
 	type Team,
 	type Workspace,
 	and,
+	deletedUsers,
 	desc,
 	eq,
 	githubRepoInfoTable,
@@ -22,6 +23,36 @@ export class UserService implements UserRpc {
 	constructor(db: DBClient) {
 		this.db = db;
 		this.logger = createCustomLogger("users");
+	}
+
+	async markAsDeleted({ userId }: { userId: string }) {
+		this.logger.info("Marking user as deleted", userId);
+
+		await this.db.transaction(async (tx) => {
+			const userRecord = await tx
+				.select()
+				.from(usersTable)
+				.where(eq(usersTable.externalId, userId))
+				.limit(1)
+				.then((results) => results[0]);
+
+			if (!userRecord) {
+				throw new Error(`User with id ${userId} not found`);
+			}
+			if (userRecord.deleted) {
+				throw new Error(`User with id ${userId} is already deleted`);
+			}
+
+			await tx
+				.update(usersTable)
+				.set({ deleted: true })
+				.where(eq(usersTable.externalId, userId));
+
+			await tx.insert(deletedUsers).values({
+				id: userRecord.id,
+				deletedAt: new Date(),
+			});
+		});
 	}
 
 	async onBoardUser({ userId }: { userId: string }) {
