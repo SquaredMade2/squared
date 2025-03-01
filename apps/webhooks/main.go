8082e210b56e6e6d7e5e608c9a5b421a6936a774
THEjacob1000 <jacob@jacobdevelops.com>
2025-03-01 17:08:20 +1100

    fix: swap webhook handler routes for correct functionality

--- apps/webhooks/main.go
+++ apps/webhooks/main.go
@@ -11,8 +11,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/bob", handleRequest)
-	http.HandleFunc("/", github.WebhookHandler)
+	http.HandleFunc("/", handleRequest)
+	http.HandleFunc("/github", github.WebhookHandler)
 	http.HandleFunc("/vercel", vercel.WebhookHandler)
 
 	if os.Getenv("PORT") == "" {
