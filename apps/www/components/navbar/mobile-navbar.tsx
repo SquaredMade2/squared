b44610edf26e436acf819cf2b5fb511f7fc10d35
Chase <chase.t.white@hotmail.com>
2025-03-30 16:14:31 -0600

    refactor(www): wrap svgs with divs to make them clickable

--- apps/www/components/navbar/mobile-navbar.tsx
+++ apps/www/components/navbar/mobile-navbar.tsx
@@ -36,20 +36,18 @@ export const MobileNavbar = ({
 			)}
 		>
 			<Logo />
-			<Menu
-				className="h-6 w-6 text-black dark:text-white"
-				onClick={() => setOpen(!open)}
-			/>
+			<div onClick={() => setOpen(!open)}>
+				<Menu className="h-6 w-6 text-black dark:text-white" />
+			</div>
 			{open && (
 				<div className="fixed inset-0 z-50 flex flex-col items-start justify-start space-y-10 bg-white pt-5 text-xl text-zinc-600 transition duration-200 hover:text-zinc-800 dark:bg-black">
 					<div className="flex w-full items-center justify-between px-5">
 						<Logo />
 						<div className="flex items-center space-x-2">
 							<ModeToggle />
-							<X
-								className="h-8 w-8 text-black dark:text-white"
-								onClick={() => setOpen(!open)}
-							/>
+							<div onClick={() => setOpen(!open)}>
+								<X className="h-8 w-8 text-black dark:text-white" />
+							</div>
 						</div>
 					</div>
 					<div className="flex flex-col items-start justify-start gap-[14px] px-8">
