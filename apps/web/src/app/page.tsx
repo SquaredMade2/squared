0f4a2f6f21af060ad701f633c0a61febfe182836
Bret Miller <bret.miller2015@gmail.com>
2025-03-31 17:06:47 -0500

    refactor(web): updated code to include a check if the user doesn't have an active organization set

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import SquaredLoader from "@/components/Loaders/SquaredLoader";
-import { useClerk, useUser } from "@clerk/nextjs";
+import { useAuth, useClerk, useOrganizationList, useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const HomePage = () => {
 	const router = useRouter();
-	const { user, isLoaded } = useUser();
+	const { user } = useUser();
 	const { organization } = useClerk();
+	const { setActive, isLoaded, userMemberships } = useOrganizationList({
+		userMemberships: true,
+	});
 	const [error, setError] = useState<string | null>(null);
+	const { orgId } = useAuth();
 
 	useEffect(() => {
 		if (!isLoaded) return;
@@ -22,11 +26,21 @@ const HomePage = () => {
 		if (!user) {
 			router.push("/sign-in");
 		}
-		if (organization) {
+
+		if (userMemberships.data.length > 0 && !orgId && isLoaded) {
+			setActive?.({
+				organization: userMemberships.data[0].organization.id,
+			}).then(() => {
+				router.push(`/${userMemberships.data[0].organization.slug}`);
+			});
+		}
+
+		if (organization && isLoaded) {
 			router.push(`/${organization.slug}`);
 		} else {
 			router.push("/create");
 		}
+		//check for if the user has organizations but doesn't have an active organization set
 	}, [isLoaded]);
 
 	if (!isLoaded) {
