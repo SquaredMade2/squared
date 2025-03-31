dc82eb2238d7428ae8e3355a2105cc51a4bfa73c
Chase <chase.t.white@hotmail.com>
2025-03-30 18:27:26 -0600

    build(www): refactor and remove react-icons deb

--- apps/www/src/components/navbar/mobile-navbar.tsx
+++ apps/www/src/components/navbar/mobile-navbar.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { cn } from "@/lib/utils";
+import { Menu, X } from "@squaredmade/icons";
 import { useMotionValueEvent, useScroll } from "framer-motion";
 import { Link } from "next-view-transitions";
 import { useState } from "react";
-import { IoIosMenu } from "react-icons/io";
-import { IoIosClose } from "react-icons/io";
 import { Logo } from "../Logo";
 import { Button } from "../button";
 import { ModeToggle } from "../mode-toggle";
@@ -37,20 +36,21 @@ export const MobileNavbar = ({
 			)}
 		>
 			<Logo />
-			<IoIosMenu
-				className="h-6 w-6 text-black dark:text-white"
-				onClick={() => setOpen(!open)}
-			/>
+			<button type="button" onClick={() => setOpen(!open)}>
+				<Menu className="h-6 w-6 text-black dark:text-white" />
+			</button>
 			{open && (
 				<div className="fixed inset-0 z-50 flex flex-col items-start justify-start space-y-10 bg-white pt-5 text-xl text-zinc-600 transition duration-200 hover:text-zinc-800 dark:bg-black">
 					<div className="flex w-full items-center justify-between px-5">
 						<Logo />
 						<div className="flex items-center space-x-2">
 							<ModeToggle />
-							<IoIosClose
-								className="h-8 w-8 text-black dark:text-white"
-								onClick={() => setOpen(!open)}
-							/>
+							<button type="button" onClick={() => setOpen(!open)}>
+								<X
+									className="h-8 w-8 text-black dark:text-white"
+									onClick={() => setOpen(!open)}
+								/>
+							</button>
 						</div>
 					</div>
 					<div className="flex flex-col items-start justify-start gap-[14px] px-8">
