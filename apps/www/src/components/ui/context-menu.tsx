252b603fdca1d3cf86adcaa9a1e0a01171c98df6
THEjacob1000 <jacob@jacobdevelops.com>
2025-04-06 14:03:11 +1000

    feat: add @tailwindcss/typography and @radix-ui/react-context-menu dependencies, update package versions, and adjust imports in context menu component

--- apps/www/src/components/ui/context-menu.tsx
+++ apps/www/src/components/ui/context-menu.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import * as ContextMenuPrimitive from "@radix-ui/react-context-menu";
 import { Check, ChevronRight, Circle } from "@squaredmade/icons";
-import * as ContextMenuPrimitive from "@squaredmade/ui/context-menu";
 import * as React from "react";
 
 const ContextMenu = ContextMenuPrimitive.Root;
