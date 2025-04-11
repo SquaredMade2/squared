741829cdee7e2ff69d08ff229dca906cc70c0b4b
Mioara Cenusa <128102876+Mioara82@users.noreply.github.com>
2025-04-11 18:52:38 +0100

    Update checkOverdueDate.ts
    removed the { } wrapping the return statement of if (!date) conditional statement

--- apps/web/src/utils/checkOverdueDate.ts
+++ apps/web/src/utils/checkOverdueDate.ts
@@ -1,7 +1,5 @@
 export function checkOverdueDate(date: Date | null): boolean {
-	if (!date) {
-		return false;
-	}
+	if (!date) return false;
 	const currentDate = new Date();
 	//use Number.isNaN() instead of isNaN() due to latter being type unsafe
 	return !Number.isNaN(date.getTime()) && date < currentDate;
