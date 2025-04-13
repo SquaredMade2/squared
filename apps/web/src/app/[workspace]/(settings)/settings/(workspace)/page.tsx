bf14aecd47ad8fae1b14760230b22e9ad53f5f17
duncanwhyte <duncan_whyte@hotmail.co.uk>
2025-04-13 20:28:03 +0100

    refactor: added condition to useEffect. user must have domain permissions before populating updating form values

--- apps/web/src/app/[workspace]/(settings)/settings/(workspace)/page.tsx
+++ apps/web/src/app/[workspace]/(settings)/settings/(workspace)/page.tsx
@@ -115,8 +115,9 @@ export default function WorkspaceSettings() {
 		//on a page refresh the form values are blank. This is a quick fix for it to reupdate the values.
 		if (
 			form.getValues("name") !== workspace.name ||
-			form.getValues("url") !==
-				workspace.url.replace("https://app.squaredmade.com/", "")
+			(form.getValues("url") !==
+				workspace.url.replace("https://app.squaredmade.com/", "") &&
+				hasDomainManagePermission)
 		) {
 			updateValues();
 		}
