8eebe5029d79f1c92a526ea48d19db566bfbfadc
Mioara Cenusa <mioaracenusa82@gmail.com>
2025-05-03 01:56:56 +0100

    fix(web): update the TaskPageLayout props

--- apps/web/src/app/[workspace]/(main)/archive/recently-deleted-tasks/page.tsx
+++ apps/web/src/app/[workspace]/(main)/archive/recently-deleted-tasks/page.tsx
@@ -24,7 +24,7 @@ const Page = () => {
 			authorized={authorized}
 			currentWorkspace={workspace}
 			teamIdentifier={teamIdentifier}
-			handleDragEnd={handleDragEnd}
+			handleDragEndAction={handleDragEnd}
 			pageTitle="Recently Deleted Tasks"
 		>
 			<ViewAllTasks getGroupedColumns={getGroupedColumns} />
